fc9da537cf6ed5eac8a6824a64a377243c831585
Justin Bambrick <jbambrick@tsilhqotin.ca>
2022-01-07 14:13:06 -0800

    build(tng-dictionary): add home and credits components

--- apps/tng-dictionary/src/app/app.tsx
+++ apps/tng-dictionary/src/app/app.tsx
@@ -3,12 +3,16 @@ import styles from './app.module.css';
 import { Routes } from "react-router-dom";
 import { Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
+import Home from '../components/Home/Home';
 import VocabularyListIndex from '../components/VocabularyListIndex/VocabularyListIndex';
+import Credits from '../components/Credits/Credits';
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Credits" element={<Credits />} />
         <Route path="/vocabularyLists" element={<VocabularyListIndex />} />
         <Route path="/vocabularyLists/:id" element={<VocabularyListDetail />} />
       </Routes>
@@ -19,3 +23,6 @@ export function App() {
 }
 
 export default App;
+
+// TODO ADD HOME, CREDITS, NAVIGATION FROM HOME TO CREDITS & VOCAB INDEX
+// 
