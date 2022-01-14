ac622d8af78cf712a815bdeb81fbf52a5ddffb4b
Justin Bambrick <jbambrick@tsilhqotin.ca>
2022-01-14 11:15:43 -0800

    refactor(tng-dictionary): fix issues after rebasing

--- apps/tng-dictionary/src/components/VocabularyListIndex/VocabularyListIndex.tsx
+++ apps/tng-dictionary/src/components/VocabularyListIndex/VocabularyListIndex.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Loading from '../Loading/Loading';
-import Table, { TableProps } from '../Table/Table';
 import './VocabularyListIndex.module.css';
 
 type HasIdAndName = {
