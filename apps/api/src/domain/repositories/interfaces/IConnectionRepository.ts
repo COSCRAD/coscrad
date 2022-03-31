a3ad8a56dc29392e83609b7f47204f9f66699910
Aaron Plahn <aplahn85@gmail.com>
2022-03-30 17:47:49 -0700

    WIP: update draft Connection Repository Interface

--- apps/api/src/domain/repositories/interfaces/IConnectionRepository.ts
+++ apps/api/src/domain/repositories/interfaces/IConnectionRepository.ts
@@ -1,6 +1,8 @@
+import { Maybe } from 'apps/api/src/lib/types/maybe';
 import { EntityCompositeIdentifier } from '../../models/types/entityCompositeIdentifier';
 import { EntityId } from '../../types/entity-id';
-import { EntityType } from '../../types/entityType';
+import { EntityType, entityTypes } from '../../types/entityType';
+import { ISpecification } from './ISpecification';
 
 type ContextType = string;
 
@@ -9,7 +11,7 @@ type ContextData = Record<string, unknown>;
 type Context = {
     contextType: ContextType;
 
-    contextData: ContextData;
+    data: ContextData;
 };
 
 type ContextMetadata = {
@@ -55,6 +57,90 @@ type RelatedEntities = {
     [K in keyof EntityType]: RelatedEntity[];
 };
 
+const sampleConnection: EdgeConnection = {
+    // id?
+    to: {
+        type: entityTypes.term, // Pretend I am a video
+        id: '1234',
+    },
+    // Note we would never actually do this. we need more entity types!
+    from: {
+        type: entityTypes.vocabularyList, // Pretend I am a book
+        id: '34',
+    },
+    context: [
+        {
+            to: {
+                contextType: 'timestamp',
+                data: {
+                    timestamp: {
+                        in: '00:23:32',
+                        out: '00:45:13',
+                    },
+                },
+            },
+            from: {
+                contextType: 'pageRange', //ContextType.pageRange
+                data: {
+                    pageRange: ['x', '1', '12', '123', 'V', '124', '125', '126'], // New PageRange(new Range(123,126))
+                },
+            },
+            tags: [],
+            note: 'The historical account of how the trail was built is portrayed in the video.',
+        },
+    ],
+};
+
+const sampleSelfConnection: EdgeConnection = {
+    // id?
+    to: {
+        type: entityTypes.term, // Pretend I am a book
+        id: '1234',
+    },
+    // Note do we want a different type for a self connection? e.g. a single `self` field?
+    from: {
+        type: entityTypes.term, // Pretend I am a book
+        id: '1234',
+    },
+    context: [
+        {
+            // We should just have a single 'self' prop instead of dupulicating data
+            to: {
+                contextType: 'pageRange', //ContextType.pageRange
+                data: {
+                    pageRange: ['33'], // New PageRange(new Range(123,126))
+                },
+            },
+            from: {
+                contextType: 'pageRange', //ContextType.pageRange
+                data: {
+                    pageRange: ['33'], // New PageRange(new Range(123,126))
+                },
+            },
+            tags: ['11', '17'], // tagId: 11 <-> 'animals', '17' ,-> 'bears'
+            note: 'This page is about bears',
+        },
+        {
+            // We should just have a single 'self' prop instead of dupulicating data
+            to: {
+                contextType: 'pageRange', //ContextType.pageRange
+                data: {
+                    pageRange: ['44', '45', '46', '47'], // New PageRange(new Range(123,126))
+                },
+            },
+            // We should just have a single 'self' prop instead of dupulicating data
+            from: {
+                contextType: 'pageRange', //ContextType.pageRange
+                data: {
+                    pageRange: ['44', '45', '46', '47'], // New PageRange(new Range(123,126))
+                },
+            },
+            tags: ['12'], // tagId: 12 <-> 'legeds'
+            note: 'This one is about how the sun was made',
+        },
+    ],
+};
+
 export interface IConnectionRepository {
     // "Queries"
     getConnectionsToEntity(
@@ -62,13 +148,18 @@ export interface IConnectionRepository {
         options: ConnectionQueryOptions
     ): Promise<RelatedEntities>;
 
-    fetchMany(): Promise<EdgeConnection[]>;
+    fetchMany(specification?: ISpecification<EdgeConnection>): Promise<EdgeConnection[]>;
+
+    // Maybe this should take in the composite Identifier(s) in case you are using that as an id
+    fetchOne(specification?: ISpecification<EdgeConnection>): Promise<Maybe<EdgeConnection>>;
 
     fetchById(
+        // or should this be the doc ID???
         edgeConnectionCompositeID: Promise<EdgeConnectionCompositeIdentifier>
-    ): EdgeConnection;
+    ): Promise<Maybe<EdgeConnection>>;
 
-    getCount(): Promise<number>;
+    // I prefer count to `getCount` let's change the entity repos too
+    count(specification?: ISpecification<EdgeConnection>): Promise<number>;
 
     // "Commands"
     createConnection(
