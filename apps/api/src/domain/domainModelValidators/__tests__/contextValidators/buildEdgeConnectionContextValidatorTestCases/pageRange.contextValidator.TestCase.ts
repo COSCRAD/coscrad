d814bceff3eb916f7d1cd003c2dff4c2cd685747
Aaron Plahn <aplahn85@gmail.com>
2022-04-12 16:10:10 -0700

    Update page range validation to prevent duplicate identifiers

--- apps/api/src/domain/domainModelValidators/__tests__/contextValidators/buildEdgeConnectionContextValidatorTestCases/pageRange.contextValidator.TestCase.ts
+++ apps/api/src/domain/domainModelValidators/__tests__/contextValidators/buildEdgeConnectionContextValidatorTestCases/pageRange.contextValidator.TestCase.ts
@@ -1,8 +1,10 @@
+import { PageIdentifier } from 'apps/api/src/domain/models/book/entities/types/PageIdentifier';
 import { PartialDTO } from '../../../../../types/partial-dto';
 import EmptyPageRangeError from '../../../../domainModelValidators/errors/context/EmptyPageRangeError';
 import { PageRangeContext } from '../../../../models/context/page-range/page-range.context.entity';
 import { EdgeConnectionContextType } from '../../../../models/context/types/EdgeConnectionContextType';
 import { pageRangeContextValidator } from '../../../contextValidators/pageRangeContext.validator';
+import DuplicatePageIdentifierError from '../../../errors/context/DuplicatePageIdentifierError';
 import NullOrUndefinedEdgeConnectionContextDTOError from '../../../errors/context/NullOrUndefinedEdgeConnectionContextDTOError';
 import { ContextModelValidatorTestCase } from '../types/ContextModelValidatorTestCase';
 import createInvalidContextErrorFactory from './utilities/createInvalidContextErrorFactory';
@@ -37,6 +39,21 @@ export const buildPageRangeTestCase = (): ContextModelValidatorTestCase<PageRang
                 pages: [],
             },
             expectedError: topLevelErrorFactory([new EmptyPageRangeError()]),
+        },
+        {
+            description: 'the pages include duplicates',
+            invalidDTO: {
+                ...validDTO,
+                // TODO remove cast
+                pages: [
+                    ...(validDTO.pages as PageIdentifier[]),
+                    validDTO.pages[0],
+                    validDTO.pages[3],
+                ],
+            },
+            expectedError: topLevelErrorFactory([
+                new DuplicatePageIdentifierError([validDTO.pages[0], validDTO.pages[3]]),
+            ]),
         },
     ],
 });
--- apps/api/src/domain/domainModelValidators/contextValidators/pageRangeContext.validator.ts
+++ apps/api/src/domain/domainModelValidators/contextValidators/pageRangeContext.validator.ts
@@ -2,10 +2,50 @@ import { InternalError } from 'apps/api/src/lib/errors/InternalError';
 import { PageRangeContext } from '../../models/context/page-range/page-range.context.entity';
 import { EdgeConnectionContextType } from '../../models/context/types/EdgeConnectionContextType';
 import { isNullOrUndefined } from '../../utilities/validation/is-null-or-undefined';
+import DuplicatePageIdentifierError from '../errors/context/DuplicatePageIdentifierError';
 import EmptyPageRangeError from '../errors/context/EmptyPageRangeError';
 import InvalidEdgeConnectionContextError from '../errors/context/InvalidEdgeConnectionContextError';
 import NullOrUndefinedEdgeConnectionContextDTOError from '../errors/context/NullOrUndefinedEdgeConnectionContextDTOError';
 import { Valid } from '../Valid';
+
+type UniqueAndDuplicates<T> = {
+    unique: T[];
+
+    duplicates: T[];
+};
+
+/**
+ * TODO: Generalize this and extract to a higher level.
+ */
+const buildEmptyUniqueAndDuplicatesInstance = <T>(): UniqueAndDuplicates<T> => ({
+    unique: [],
+    duplicates: [],
+});
+
+const findDuplicatesInArray = (input: string[]): string[] =>
+    input.reduce(({ unique, duplicates }: UniqueAndDuplicates<string>, element) => {
+        // Repeat duplicate- don't add to the list
+        if (duplicates.includes(element))
+            return {
+                unique,
+                duplicates,
+            };
+
+        // Newly identified duplicate- add to list
+        if (unique.includes(element))
+            return {
+                unique,
+                // TODO use deep clone if taking in non-primitive type of data
+                duplicates: [...duplicates, element],
+            };
+
+        // the element is unique
+        return {
+            // TODO use deep clone if taking in non-primitive type of data
+            unique: [...unique, element],
+            duplicates,
+        };
+    }, buildEmptyUniqueAndDuplicatesInstance<string>()).duplicates;
 
 export const pageRangeContextValidator = (input: unknown): Valid | InternalError => {
     if (isNullOrUndefined(input))
@@ -21,6 +61,11 @@ export const pageRangeContextValidator = (input: unknown): Valid | InternalError
         allErrors.push(new InternalError(`Invalid type for pages property: ${typeof pages}`));
 
     if (pages.length === 0) allErrors.push(new EmptyPageRangeError());
+
+    const duplicateIdentifiers = findDuplicatesInArray(pages);
+
+    if (duplicateIdentifiers.length > 0)
+        allErrors.push(new DuplicatePageIdentifierError(duplicateIdentifiers));
 
     return allErrors.length > 0
         ? new InvalidEdgeConnectionContextError(EdgeConnectionContextType.pageRange, allErrors)
