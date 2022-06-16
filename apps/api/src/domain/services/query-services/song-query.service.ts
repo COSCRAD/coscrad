c9293f7f374b5dd22ede4b8dbec9f2b144915737
Aaron Plahn <aplahn85@gmail.com>
2022-06-15 18:29:28 -0700

    refactor(api): break out Transcribed Audio controller

--- apps/api/src/domain/services/query-services/song-query.service.ts
+++ apps/api/src/domain/services/query-services/song-query.service.ts
@@ -1,28 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CommandInfoService } from '../../../app/controllers/command/services/command-info-service';
-import { isInternalError } from '../../../lib/errors/InternalError';
 import { RepositoryProvider } from '../../../persistence/repositories/repository.provider';
 import { SongViewModel } from '../../../view-models/buildViewModelForResource/viewModels/song.view-model';
 import { Song } from '../../models/song/song.entity';
-import { Tag } from '../../models/tag/tag.entity';
 import { InMemorySnapshot, ResourceType } from '../../types/ResourceType';
-import buildInMemorySnapshot from '../../utilities/buildInMemorySnapshot';
 import { BaseQueryService } from './base-query.service';
 
 @Injectable()
 export class SongQueryService extends BaseQueryService<Song, SongViewModel> {
     constructor(repositoryProvider: RepositoryProvider, commandInfoService: CommandInfoService) {
         super(ResourceType.song, repositoryProvider, commandInfoService);
-    }
-
-    async fetchRequiredExternalState(): Promise<InMemorySnapshot> {
-        const tags = (await this.repositoryProvider.getTagRepository().fetchMany()).filter(
-            (result): result is Tag => !isInternalError(result)
-        );
-
-        return buildInMemorySnapshot({
-            tags,
-        });
     }
 
     buildViewModel(song: Song, _: InMemorySnapshot) {
