09421beb57108c42838dc38df0684e75dd9fff5b
Aaron Plahn <aplahn85@gmail.com>
2022-06-16 17:30:15 -0700

    refactor(api): break out Bibliographic Reference controller (#111)
    * refactor(api): break out Bibliographic Reference controller
    
    * refactor(api): refactor controller and query services based on PR reviews (#103-111)

--- apps/api/src/app/controllers/resources/spatial-feature.controller.ts
+++ apps/api/src/app/controllers/resources/spatial-feature.controller.ts
@@ -4,9 +4,10 @@ import IsPublished from '../../../domain/repositories/specifications/isPublished
 import { SpatialFeatureQueryService } from '../../../domain/services/query-services/spatial-feature-query.service';
 import { ResourceType } from '../../../domain/types/ResourceType';
 import { SpatialFeatureViewModel } from '../../../view-models/buildViewModelForResource/viewModels/spatial-data/spatial-feature.view-model';
-import { buildByIdApiParamMetadata, RESOURCES_ROUTE_PREFIX } from '../resourceViewModel.controller';
 import buildViewModelPathForResourceType from '../utilities/buildViewModelPathForResourceType';
-import sendInternalResultAsHttpResponse from './sendInternalResultAsHttpResponse';
+import buildByIdApiParamMetadata from './common/buildByIdApiParamMetadata';
+import sendInternalResultAsHttpResponse from './common/sendInternalResultAsHttpResponse';
+import { RESOURCES_ROUTE_PREFIX } from './constants';
 
 @Injectable()
 @Controller(
