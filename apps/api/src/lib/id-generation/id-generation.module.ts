3c40fb4be6afa6d2e2702d149b5f9dfb37cb7442
Aaron Plahn <aplahn85@gmail.com>
2022-06-22 13:17:20 -0700

    fix(api): fix Id Manager dependency injection

--- apps/api/src/lib/id-generation/id-generation.module.ts
+++ apps/api/src/lib/id-generation/id-generation.module.ts
@@ -1,29 +1,20 @@
 import { Module } from '@nestjs/common';
 import { IdGenerationController } from '../../app/controllers/id-generation/id-generation.controller';
-import { AggregateId } from '../../domain/types/AggregateId';
-import { ArangoConnectionProvider } from '../../persistence/database/arango-connection.provider';
-import { DatabaseProvider } from '../../persistence/database/database.provider';
 import { PersistenceModule } from '../../persistence/persistence.module';
-import { ArangoIdRepository } from '../../persistence/repositories/arango-id-repository';
+import { RepositoryProvider } from '../../persistence/repositories/repository.provider';
 import { IdManagementService } from './id-management.service';
-import { IIdRepository } from './interfaces/id-repository.interface';
 
 @Module({
     imports: [PersistenceModule],
     providers: [
         {
-            provide: 'ID_REPOSITORY',
-            useFactory: (arangoConnectionProvider: ArangoConnectionProvider) =>
-                new ArangoIdRepository(new DatabaseProvider(arangoConnectionProvider)),
-            inject: [ArangoConnectionProvider],
-        },
-        {
             provide: 'ID_MANAGER',
-            useFactory: (idRepository: IIdRepository<AggregateId>) =>
-                new IdManagementService(idRepository),
-            inject: ['ID_REPOSITORY'],
+            useFactory: (repositoryProvider: RepositoryProvider) =>
+                new IdManagementService(repositoryProvider.getIdRepository()),
+            inject: [RepositoryProvider],
         },
     ],
     controllers: [IdGenerationController],
+    exports: ['ID_MANAGER'],
 })
 export class IdGenerationModule {}
