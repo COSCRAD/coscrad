808ff8a846e8315e1d04f1de4e2272af528e1ae5
Aaron Plahn <aplahn85@gmail.com>
2022-07-14 13:45:31 -0700

    feat(api): introduce REGISTER_USER command

--- apps/api/src/domain/models/__tests__/command-helpers/assert-command-payload-type-error.ts
+++ apps/api/src/domain/models/__tests__/command-helpers/assert-command-payload-type-error.ts
@@ -1,7 +1,11 @@
 import { InternalError } from '../../../../lib/errors/InternalError';
 import InvalidCommandPayloadTypeError from '../../../models/shared/common-command-errors/InvalidCommandPayloadTypeError';
 
-export const assertCommandPayloadTypeError = (error: InternalError, propertyKey: string) => {
+export const assertCommandPayloadTypeError = (result: unknown, propertyKey: string) => {
+    expect(result).toBeInstanceOf(InternalError);
+
+    const error = result as InternalError;
+
     expect(error).toBeInstanceOf(InvalidCommandPayloadTypeError);
 
     expect(error.toString().includes(propertyKey)).toBe(true);
--- apps/api/src/domain/models/user-management/group/commands/create-group.command.integration.spec.ts
+++ apps/api/src/domain/models/user-management/group/commands/create-group.command.integration.spec.ts
@@ -1,5 +1,8 @@
 import { CommandHandlerService, FluxStandardAction } from '@coscrad/commands';
+import { FuzzGenerator, getCoscradDataSchema } from '@coscrad/data-types';
 import setUpIntegrationTest from '../../../../../app/controllers/__tests__/setUpIntegrationTest';
+import { assertExternalStateError } from '../../../../../domain/models/__tests__/command-helpers/assert-external-state-error';
+import { InternalError } from '../../../../../lib/errors/InternalError';
 import { NotAvailable } from '../../../../../lib/types/not-available';
 import { ArangoConnectionProvider } from '../../../../../persistence/database/arango-connection.provider';
 import generateRandomTestDatabaseName from '../../../../../persistence/repositories/__tests__/generateRandomTestDatabaseName';
@@ -7,10 +10,18 @@ import TestRepositoryProvider from '../../../../../persistence/repositories/__te
 import { DTO } from '../../../../../types/DTO';
 import { IIdManager } from '../../../../interfaces/id-manager.interface';
 import { AggregateId } from '../../../../types/AggregateId';
+import { AggregateType } from '../../../../types/AggregateType';
 import buildEmptyInMemorySnapshot from '../../../../utilities/buildEmptyInMemorySnapshot';
+import buildInMemorySnapshot from '../../../../utilities/buildInMemorySnapshot';
+import { assertCommandPayloadTypeError } from '../../../__tests__/command-helpers/assert-command-payload-type-error';
+import { assertCreateCommandError } from '../../../__tests__/command-helpers/assert-create-command-error';
 import { assertCreateCommandSuccess } from '../../../__tests__/command-helpers/assert-create-command-success';
 import { CommandAssertionDependencies } from '../../../__tests__/command-helpers/types/CommandAssertionDependencies';
+import { InvalidFSAFactoryFunction } from '../../../__tests__/command-helpers/types/InvalidFSAFactoryFunction';
+import buildDummyUuid from '../../../__tests__/utilities/buildDummyUuid';
 import { CoscradUserGroup } from '../entities/coscrad-user-group.entity';
+import { UserGroupIdAlreadyInUseError } from '../errors/external-state-errors/UserGroupIdAlreadyInUseError';
+import { UserGroupLabelAlreadyInUseError } from '../errors/external-state-errors/UserGroupLabelAlreadyInUseError';
 import { CreateGroup } from './create-group.command';
 import { CreateGroupCommandHandler } from './create-group.command-handler';
 
@@ -28,6 +39,27 @@ const buildValidCommandFSA = (id: AggregateId): FluxStandardAction<DTO<CreateGro
 
 const initialState = buildEmptyInMemorySnapshot();
 
+// TODO share this as a util- it's time!
+const buildInvalidFSA: InvalidFSAFactoryFunction<CreateGroup> = (
+    id: AggregateId,
+    payloadOverrides: Partial<Record<keyof CreateGroup, unknown>> = {}
+): FluxStandardAction<DTO<CreateGroup>> => ({
+    type: commandType,
+    payload: {
+        ...buildValidCommandFSA(id).payload,
+        ...(payloadOverrides as Partial<CreateGroup>),
+    },
+});
+
+const existingUserGroupDto: DTO<CoscradUserGroup> = {
+    type: AggregateType.userGroup,
+    id: buildDummyUuid(),
+    label: 'existing users',
+    description: 'this group already exists before running the command',
+    userIds: [],
+};
+
+const existingUserGroup = new CoscradUserGroup(existingUserGroupDto);
 describe('CreateGroup', () => {
     let testRepositoryProvider: TestRepositoryProvider;
 
@@ -41,9 +73,12 @@ describe('CreateGroup', () => {
 
     beforeAll(async () => {
         ({ testRepositoryProvider, commandHandlerService, idManager, arangoConnectionProvider } =
-            await setUpIntegrationTest({
-                ARANGO_DB_NAME: generateRandomTestDatabaseName(),
-            }));
+            await setUpIntegrationTest(
+                {
+                    ARANGO_DB_NAME: generateRandomTestDatabaseName(),
+                },
+                { shouldMockIdGenerator: true }
+            ));
 
         commandHandlerService.registerHandler(
             commandType,
@@ -55,6 +90,8 @@ describe('CreateGroup', () => {
             idManager,
             commandHandlerService,
         };
+
+        jest.useFakeTimers().setSystemTime(new Date('2020-04-05'));
     });
 
     afterAll(async () => {
@@ -88,6 +125,77 @@ describe('CreateGroup', () => {
 
                     expect(idStatus).toBe(NotAvailable);
                 },
+            });
+        });
+    });
+
+    describe('when the command is invalid', () => {
+        describe('when the new group ID was not generated with our system', () => {
+            it('should return the expected error', async () => {
+                await assertCreateCommandError(commandAssertionDependencies, {
+                    buildCommandFSA: (id: AggregateId) =>
+                        buildInvalidFSA(id, { id: buildDummyUuid() }),
+                    initialState,
+                });
+            });
+        });
+
+        describe('when there is already a user group with the given ID', () => {
+            it('should fail with the expected error', async () => {
+                const newId = await idManager.generate();
+
+                await testRepositoryProvider.addFullSnapshot(
+                    buildInMemorySnapshot({
+                        userGroups: [existingUserGroup.clone({ id: newId })],
+                    })
+                );
+
+                const commandFSAThatReusesId = buildValidCommandFSA(newId);
+
+                const executionResult = await commandHandlerService.execute(commandFSAThatReusesId);
+
+                assertExternalStateError(executionResult, new UserGroupIdAlreadyInUseError(newId));
+            });
+        });
+
+        describe('when there is already a user group with the given label', () => {
+            it('should fail with the expected error', async () => {
+                await assertCreateCommandError(commandAssertionDependencies, {
+                    buildCommandFSA: (id: AggregateId) =>
+                        buildInvalidFSA(id, { label: existingUserGroup.label }),
+                    initialState: buildInMemorySnapshot({
+                        userGroups: [existingUserGroup],
+                    }),
+                    checkError: (error: InternalError) =>
+                        assertExternalStateError(
+                            error,
+                            new UserGroupLabelAlreadyInUseError(existingUserGroup.label)
+                        ),
+                });
+            });
+        });
+
+        describe('when the payload has a property with an invalid type', () => {
+            const commandPayloadDataSchema = getCoscradDataSchema(CreateGroup);
+
+            Object.entries(commandPayloadDataSchema).forEach(([propertyName, propertySchema]) => {
+                const invalidValues = new FuzzGenerator(propertySchema).generateInvalidValues();
+
+                invalidValues.forEach(({ value, description }) => {
+                    describe(`when the property: ${propertyName} has an invalid value: ${value} (${description})`, () => {
+                        it('should return the appropriate type error', async () => {
+                            const validId = await idManager.generate();
+
+                            const result = await commandHandlerService.execute(
+                                buildInvalidFSA(validId, {
+                                    [propertyName]: value,
+                                })
+                            );
+
+                            assertCommandPayloadTypeError(result, propertyName);
+                        });
+                    });
+                });
             });
         });
     });
