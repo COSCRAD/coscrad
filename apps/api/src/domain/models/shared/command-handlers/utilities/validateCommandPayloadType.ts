d2e2d792c8eecc76d8d988b3ac9c9ab755f1ac87
Aaron Plahn <aplahn85@gmail.com>
2022-07-26 17:47:57 -0700

    fix(api): prevent superfluous props in command validation

--- apps/api/src/domain/models/shared/command-handlers/utilities/validateCommandPayloadType.ts
+++ apps/api/src/domain/models/shared/command-handlers/utilities/validateCommandPayloadType.ts
@@ -5,9 +5,13 @@ import { Valid } from '../../../../domainModelValidators/Valid';
 import InvalidCommandPayloadTypeError from '../../common-command-errors/InvalidCommandPayloadTypeError';
 
 export default (command: ICommand, commandType: string): Valid | InternalError => {
+    // TODO Remove this hack that has to do with the 'whitelist' failing for `CompositeIdentifier` payload prop
+    const forbidUnknownValues = commandType === 'GRANT_RESOURCE_READ_ACCESS_TO_USER' ? false : true;
+
     // Validate command type
     const payloadTypeErrors = buildSimpleValidationFunction(
-        Object.getPrototypeOf(command).constructor
+        Object.getPrototypeOf(command).constructor,
+        { forbidUnknownValues }
     )(command).map(
         (simpleError) => new InternalError(`invalid payload type: ${simpleError.toString()}`)
     );
--- libs/validation/src/lib/buildSimpleValidationFunction.ts
+++ libs/validation/src/lib/buildSimpleValidationFunction.ts
@@ -6,14 +6,31 @@ interface Ctor<T> {
     new (...args): T;
 }
 
+type Options = {
+    forbidUnknownValues: boolean;
+};
+
 /* eslint-disable @typescript-eslint/ban-types */
-export default (ModelCtor: Ctor<Object>): SimpleValidationFunction =>
+export default (
+        ModelCtor: Ctor<Object>,
+        { forbidUnknownValues }: Options = { forbidUnknownValues: false }
+    ): SimpleValidationFunction =>
     (input: unknown) => {
         const instanceToValidate = plainToInstance(ModelCtor, input, {
             enableImplicitConversion: false,
         });
 
+        const additionalOptions = forbidUnknownValues
+            ? {
+                  forbidUnknownValues,
+                  // TODO alias the following props in favor of more inclusive identifiers
+                  forbidNonWhitelisted: true,
+                  whitelist: true,
+              }
+            : {};
+
         return validateSync(instanceToValidate, {
             skipMissingProperties: false,
+            ...additionalOptions,
         });
     };
