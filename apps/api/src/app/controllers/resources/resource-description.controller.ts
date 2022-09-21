264bb850189acdd7d53a8a7e845a90dd30526331
Aaron Plahn <aplahn85@gmail.com>
2022-09-21 14:03:05 -0700

    WIP refactor edge and resource description controllers

--- apps/api/src/app/controllers/resources/resource-description.controller.ts
+++ apps/api/src/app/controllers/resources/resource-description.controller.ts
@@ -2,33 +2,24 @@ import { Controller, Get } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ApiTags } from '@nestjs/swagger';
 import { isResourceType } from '../../../domain/types/ResourceType';
-import { RepositoryProvider } from '../../../persistence/repositories/repository.provider';
-import { buildAllResourceDescriptions } from '../../../view-models/resourceDescriptions/buildAllResourceDescriptions';
+import { buildAllAggregateDescriptions } from '../../../view-models/resourceDescriptions/buildAllAggregateDescriptions';
 import { AggregateInfo } from '../../../view-models/resourceDescriptions/types/AggregateInfo';
-import { buildIndexPathForAggregate } from '../utilities/buildIndexPathForAggregate';
+import { mixLinkIntoViewModelDescription } from '../utilities';
 import { RESOURCES_ROUTE_PREFIX } from './constants';
 
 @ApiTags(RESOURCES_ROUTE_PREFIX)
 @Controller(RESOURCES_ROUTE_PREFIX)
 export class ResourceDescriptionController {
-    constructor(
-        private readonly repositoryProvider: RepositoryProvider,
-        private readonly configService: ConfigService
-    ) {}
+    constructor(private readonly configService: ConfigService) {}
 
     @Get('')
     getAllResourceDescriptions(): (AggregateInfo & { link: string })[] {
         const APP_GLOBAL_PREFIX = this.configService.get<string>('GLOBAL_PREFIX');
 
-        const buildLink = (route: string) => `/${APP_GLOBAL_PREFIX}/${route}`;
-
-        const resourceInfos = buildAllResourceDescriptions();
+        const resourceInfos = buildAllAggregateDescriptions();
 
         return resourceInfos
             .filter(({ type }) => isResourceType(type))
-            .map((resourceInfo) => ({
-                ...resourceInfo,
-                link: buildLink(buildIndexPathForAggregate(resourceInfo.type)),
-            }));
+            .map(mixLinkIntoViewModelDescription(APP_GLOBAL_PREFIX));
     }
 }
