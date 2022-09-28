4cf1a7c911c8aa0de17dd72d4703c07d5d1857d5
Aaron Plahn <aplahn85@gmail.com>
2022-09-28 09:26:54 -0700

    build(coscrad-frontend): migrate cypress to 10

--- apps/coscrad-frontend-e2e/src/integration/app.spec.ts
+++
@@ -1,8 +0,0 @@
-
-describe('coscrad-frontend', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    cy.contains('coscrad',{matchCase:false})
-  });
-});
