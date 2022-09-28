035958adcd33df7e06d48b27a53146f1cf495952
Aaron Plahn <aplahn85@gmail.com>
2022-09-28 15:44:25 -0700

    build(coscrad-frontend): migrate cypress to 10 (#191)
    * build(coscrad-frontend): migrate cypress to 10
    
    * test(coscrad-frontend): remove skipped test

---
+++ apps/coscrad-frontend-e2e/src/e2e/app.cy.ts
@@ -0,0 +1,7 @@
+describe('coscrad-frontend', () => {
+    beforeEach(() => cy.visit('/'));
+
+    it('should display welcome message', () => {
+        cy.contains('coscrad', { matchCase: false });
+    });
+});
