8447eb4d89ba0de5ed94fe579d00c09158c88345
Blake Sellars <bsellars@domain.tsilhqotin.ca>
2022-10-12 09:53:09 -0700

    added mui button

--- apps/tng-radio-hub/src/Components/widgets/player/player.tsx
+++ apps/tng-radio-hub/src/Components/widgets/player/player.tsx
@@ -1,14 +1,32 @@
+import { Button, createTheme, ThemeProvider } from '@mui/material';
 import './player.module.css';
 
 /* eslint-disable-next-line */
 export interface PlayerProps {}
 
 export function Player(props: PlayerProps) {
+    const audio = new Audio(`${'http://datsan.openbroadcaster.pro:8000/datsan'}`);
+
+    const start = () => {
+        audio.play().catch(console.log);
+    };
     return (
         <div>
-            <h1>Welcome to Player!</h1>
+            <ThemeProvider theme={Theme}>
+                <Button variant="contained" color="primary" onClick={start}>
+                    hey
+                </Button>
+            </ThemeProvider>
         </div>
     );
 }
 
 export default Player;
+
+const Theme = createTheme({
+    palette: {
+        primary: {
+            main: '#f12029',
+        },
+    },
+});
