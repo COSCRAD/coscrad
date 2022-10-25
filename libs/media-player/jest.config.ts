7b02a52776348c835f2ebf6e85d8ba480a60eed5
Justin Bambrick <jbambrick@tsilhqotin.ca>
2022-10-25 14:41:54 -0700

    build(coscrad): create react front-end library

---
+++ libs/media-player/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+    displayName: 'media-player',
+    preset: '../../jest.preset.js',
+    transform: {
+        '^.+\\.[tj]sx?$': 'babel-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    coverageDirectory: '../../coverage/libs/media-player',
+};
