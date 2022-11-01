d18580b0711348caa357634bb2e1b85daad30735
Aaron Plahn <aplahn85@gmail.com>
2022-11-01 14:20:08 -0700

    feat(coscrad-frontend): expose photographs on front-end (#216)
    feat(coscrad-frontend): expose photographs on front-end

--- apps/coscrad-frontend/src/components/AllResources/ResourceInfos.presenter.tsx
+++ apps/coscrad-frontend/src/components/AllResources/ResourceInfos.presenter.tsx
@@ -1,4 +1,4 @@
-import { IAggregateInfo } from '@coscrad/api-interfaces';
+import { IAggregateInfo, ResourceType } from '@coscrad/api-interfaces';
 import { FunctionalComponent } from '../../utils/types/functional-component';
 import { HasData } from '../higher-order-components';
 import { ResourceInfoPresenter } from './ResourceInfoPresenter';
@@ -7,9 +7,9 @@ import { ResourceInfoPresenter } from './ResourceInfoPresenter';
  * Note the plural in the name. This presents all of the resource infos. It
  * maps through the singular `ResourceInfoPresenter` to do so.
  */
-export const ResourceInfosPresenter: FunctionalComponent<HasData<IAggregateInfo[]>> = ({
-    data: resourceInfos,
-}: HasData<IAggregateInfo[]>) => (
+export const ResourceInfosPresenter: FunctionalComponent<
+    HasData<IAggregateInfo<ResourceType>[]>
+> = ({ data: resourceInfos }: HasData<IAggregateInfo<ResourceType>[]>) => (
     <div>
         <h2>Available Resources</h2>
         <div>
