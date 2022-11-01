cf92548afa8de7c846314d91e2d42d7e969b68dc
yaanahuu2 <63014783+yaanahuu2@users.noreply.github.com>
2022-11-01 11:32:52 -0700

    test(coscrad-frontend): test configurable front matter content flows (#214)
    * WIP added footer and logo
    
    * WIP troubleshoot testing components error
    
    * WIP fixed breaking tests with MemoryRouter
    
    * WIP about page test troubleshooting
    
    * WIP fixed about.spec
    
    * WIP added configurable content tests
    
    * WIP refactored tests to separate properties out
    
    Co-authored-by: yaanahuu2 <gahorner@gmail.com>

--- apps/coscrad-frontend/src/app/App.spec.tsx
+++ apps/coscrad-frontend/src/app/App.spec.tsx
@@ -1,25 +1,12 @@
 import { MemoryRouter } from 'react-router-dom';
-import { ConfigurableContent } from '../configurable-front-matter/data/configurableContentSchema';
 import { renderWithProviders } from '../utils/test-utils/renderWithProviders';
 import App from './App';
-
-export const getDummyConfigurableContent = (): ConfigurableContent => ({
-    siteTitle: 'My Site',
-
-    subTitle: 'Where it all Happens',
-
-    about: 'Just a Test',
-
-    siteDescription: 'This is my testing site',
-
-    copyrightHolder: 'ME',
-});
 
 describe('App', () => {
     it('should render successfully', () => {
         const { baseElement } = renderWithProviders(
             <MemoryRouter>
-                <App content={getDummyConfigurableContent()} />
+                <App />
             </MemoryRouter>
         );
 
