f36e5ef24845358a98640e6ede4deacaa4db5b11
Justin Bambrick <55550194+jbambrick@users.noreply.github.com>
2022-11-03 14:22:32 -0700

    feat(tng-language-hub): create radio component (#218)
    * WIP - radio player
    
    * feat(tng-language-hub): create radio component and config
    
    * refactor(tng-language-hub): refactor based on PR (#218)

---
+++ apps/tsilhqotin-language-hub/src/Components/Pages/Radio/Radio.tsx
@@ -0,0 +1,31 @@
+import RadioIcon from '@mui/icons-material/Radio';
+import { Card } from '@mui/material';
+import { MediaPlayer } from '../../../../../../libs/media-player/src';
+import { getRadioConfig } from '../../../Configs/Radio/radio.config';
+import ScrollToTop from '../../Widgets/ScrollButton/ScrollToTop';
+
+export const Radio = (): JSX.Element => {
+    return (
+        <div className="page">
+            <ScrollToTop />
+            <div id="heading">
+                <div id="container">
+                    <h1 id="pageTitle">Radio</h1>
+                </div>
+            </div>
+            <div id="aboutContent" className="pageContent">
+                <h2>
+                    Tŝilhqot’in Radio <RadioIcon />
+                </h2>
+                <Card className="tsilhqotinRadio">
+                    <img width={120} alt="radioLogo" src={getRadioConfig().radioLogoUrl} />
+                    <div className="radioPlayer">
+                        <MediaPlayer audioUrl={getRadioConfig().radioAudioUrl} />
+                    </div>
+                </Card>
+
+                <p>{getRadioConfig().radioMissionStatement}</p>
+            </div>
+        </div>
+    );
+};
--- libs/media-player/src/lib/MediaPlayer.tsx
+++ libs/media-player/src/lib/MediaPlayer.tsx
@@ -1,12 +1,46 @@
-import styles from './MediaPlayer.module.css';
+import PauseIcon from '@mui/icons-material/Pause';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import { Button } from '@mui/material';
+import { MouseEventHandler, useEffect, useState } from 'react';
+import styles from './MediaPlayer.module.scss';
 
-/* eslint-disable-next-line */
-export interface MediaPlayerProps {}
+const useAudio = (audioUrl: string): [boolean, MouseEventHandler<HTMLButtonElement>] => {
+    const [audio] = useState(new Audio(audioUrl));
+    const [playing, setPlaying] = useState(false);
 
-export function MediaPlayer(props: MediaPlayerProps) {
+    const toggle: MouseEventHandler<HTMLButtonElement> = (): void => setPlaying(!playing);
+
+    useEffect(() => {
+        playing ? audio.play() : audio.pause();
+    }, [playing]);
+
+    useEffect(() => {
+        audio.addEventListener('ended', () => setPlaying(false));
+        return () => {
+            audio.removeEventListener('ended', () => setPlaying(false));
+        };
+    }, []);
+
+    return [playing, toggle];
+};
+
+export interface MediaPlayerProps {
+    audioUrl: string;
+}
+
+export function MediaPlayer({ audioUrl }: MediaPlayerProps) {
+    const [playing, toggle] = useAudio(audioUrl);
+
     return (
         <div className={styles['container']}>
-            <h1>Welcome to MediaPlayer!</h1>
+            <Button id={styles['radioButton']} variant="contained" onClick={toggle}>
+                {playing ? <div>Now Playing!</div> : <div>Listen Live!</div>}
+                {playing ? (
+                    <PauseIcon className={styles['actionButton']} />
+                ) : (
+                    <PlayArrowIcon className={styles['actionButton']} />
+                )}
+            </Button>
         </div>
     );
 }
