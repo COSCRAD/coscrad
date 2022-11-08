f1687c5aaa6790fa287be4d57d27a97c0b5e9ef2
yaanahuu2 <gahorner@gmail.com>
2022-11-07 17:23:09 -0800

    WIP table display

--- apps/coscrad-frontend/src/utils/generic-components/presenters/generic-index-presenter.tsx
+++ apps/coscrad-frontend/src/utils/generic-components/presenters/generic-index-presenter.tsx
@@ -1,19 +1,36 @@
 import { IBaseViewModel, IIndexQueryResult } from '@coscrad/api-interfaces';
-import { Link } from 'react-router-dom';
 import { FunctionalComponent } from '../../types/functional-component';
+import './generic-detail-presenter.css';
 
 export const GenericIndexPresenter: FunctionalComponent<IIndexQueryResult<IBaseViewModel>> = ({
     data: viewModelsAndActions,
 }: IIndexQueryResult<IBaseViewModel>) => {
     const viewModels = viewModelsAndActions.map(({ data }) => data);
 
+    const displayDataTableRow = (tableDataRow: IBaseViewModel) => {
+        return (
+            <tr>
+                {Object.entries(tableDataRow).map(([key, value]) => {
+                    return (
+                        <td>{key}: {value}</td>
+                    )
+                })}
+            </tr>
+        );
+    };
+
     return (
         <div>
-            {viewModels.map((viewModel) => (
-                <Link to={viewModel.id} key={viewModel.id}>
-                    <div data-testid={viewModel.id}>{JSON.stringify(viewModel)}</div>
-                </Link>
-            ))}
+            <h3>JSON Data</h3>
+            <div className="json-data">{JSON.stringify(viewModels)}</div>
+            <h3>Records</h3>
+            <div className="records-table">
+                <table border={1} cellSpacing={0}>
+                    {viewModels.map((viewModel) => (
+                        {displayDataTableRow(viewModel)}
+                    ))}
+                </table>
+            </div>
         </div>
     );
 };
