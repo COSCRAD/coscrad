0c2988e592e19a398992df45f58104669bf144d2
yaanahuu2 <gahorner@gmail.com>
2022-11-09 22:21:03 -0800

    WIP begin refactor to generic table component

--- apps/coscrad-frontend/src/components/resources/photographs/photograph-index.presenter.tsx
+++ apps/coscrad-frontend/src/components/resources/photographs/photograph-index.presenter.tsx
@@ -1,13 +1,51 @@
 import { IIndexQueryResult, IPhotographViewModel } from '@coscrad/api-interfaces';
-import { GenericIndexPresenter } from '../../../utils/generic-components/presenters/generic-index-presenter';
 
-export const PhotographIndexPresenter = (
-    photographIndexResult: IIndexQueryResult<IPhotographViewModel>
-) => {
+export const PhotographIndexPresenter = (indexResult: IIndexQueryResult<IPhotographViewModel>) => {
     /**
      *  TODO [https://www.pivotaltracker.com/story/show/183681839]
      * We may some day read the actions and allow for bulk command execution in
      * an index view.
      */
-    return <GenericIndexPresenter {...photographIndexResult} />;
+    const { data } = indexResult;
+    const dataLabels = ['id', 'imageUrl', 'photographer'];
+    // pass dataLabels and photographs array into table component
+    const photographs = data
+        .map(({ data: photograph }) => photograph)
+        .map((photograph) => {
+            return {
+                id: photograph.id,
+                imageUrl: photograph.imageURL,
+                photographer: photograph.photographer,
+            };
+        });
+
+    return (
+        <div>
+            <h3>Photographs</h3>
+            <div className="records-table">
+                <table border={0} cellSpacing={0} cellPadding={7}>
+                    <tbody>
+                        <tr>
+                            <th>Link</th>
+                            <th>Image File URL</th>
+                            <th>Photographer</th>
+                        </tr>
+                        {/* {photographs.map(({ data: photograph }) => (
+                            <tr>
+                                <td>
+                                    <Link to={photograph.id}>View ID {photograph.id}</Link>
+                                </td>
+                                <td>{photograph.imageURL}</td>
+                                <td>{photograph.photographer}</td>
+                            </tr>
+                        ))} */}
+                    </tbody>
+                </table>
+            </div>
+            <h3>JSON Data</h3>
+            <div className="json-data">
+                <pre>{JSON.stringify(photographs, null, 2)}</pre>
+            </div>
+        </div>
+    );
 };
--- apps/coscrad-frontend/src/components/resources/transcribed-audio/transcribed-audio-index.presenter.tsx
+++ apps/coscrad-frontend/src/components/resources/transcribed-audio/transcribed-audio-index.presenter.tsx
@@ -3,37 +3,47 @@ import { Link } from 'react-router-dom';
 
 export const TranscribedAudioIndexPresenter = (
     indexResult: IIndexQueryResult<ITranscribedAudioViewModel>
-) => (
+) => {
     /**
      *  TODO [https://www.pivotaltracker.com/story/show/183681839]
      * We may some day read the actions and allow for bulk command execution in
      * an index view.
      */
-    <div>
-        <h3>Transcribed Audio</h3>
-        <div className="records-table">
-            <table border={1} cellSpacing={0}>
-                <tbody>
-                    <tr>
-                        <th>Link</th>
-                        <th>Audio Length</th>
-                        <th>Transcription</th>
-                    </tr>
-                    {indexResult.data.map((transcribedAudio) => (
+    const { data: transcribedAudioItems } = indexResult;
+
+    return (
+        <div>
+            <h3>Transcribed Audio</h3>
+            <div className="records-table">
+                <table border={1} cellSpacing={0}>
+                    <tbody>
                         <tr>
-                            <td>
-                                <Link to={transcribedAudio.data.id}>View</Link>
-                            </td>
-                            <td>{transcribedAudio.data.length}</td>
-                            <td>{transcribedAudio.data.plainText}</td>
+                            <th>Link</th>
+                            <th>Audio Length</th>
+                            <th>Transcription</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                        {transcribedAudioItems.map(({ data: transcribedAudio }) => (
+                            <tr>
+                                <td>
+                                    <Link to={transcribedAudio.id}>View</Link>
+                                </td>
+                                <td>{transcribedAudio.length}</td>
+                                <td>{transcribedAudio.plainText}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <h3>JSON Data</h3>
+            <div className="json-data">
+                <pre>
+                    {JSON.stringify(
+                        transcribedAudioItems.map(({ data }) => data),
+                        null,
+                        2
+                    )}
+                </pre>
+            </div>
         </div>
-        <h3>JSON Data</h3>
-        <div className="json-data">
-            <pre>{JSON.stringify(indexResult, null, 2)}</pre>
-        </div>
-    </div>
-);
+    );
+};
