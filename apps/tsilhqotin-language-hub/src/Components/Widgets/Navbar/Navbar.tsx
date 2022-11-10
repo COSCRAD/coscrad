9261cbd9bce783b735f6fe654dd611471b179adf
Justin Bambrick <55550194+jbambrick@users.noreply.github.com>
2022-11-09 16:19:34 -0800

    style(tng-language-hub): add new logos to site (#225)

--- apps/tsilhqotin-language-hub/src/Components/Widgets/Navbar/Navbar.tsx
+++ apps/tsilhqotin-language-hub/src/Components/Widgets/Navbar/Navbar.tsx
@@ -1,60 +1,54 @@
 import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 import Dropdown from './dropdown';
 import { Nav, NavBtn, NavBtnLink, NavLink, NavMenu } from './NavbarElements';
 
 const Navbar = () => {
     return (
-        <Nav>
-            <NavLink to="/">
-                <img
-                    src={
-                        'https://api.tsilhqotinlanguage.ca/uploads/tng_log_for_language_hub_2e4ec30f17.png'
-                    }
-                    alt="logo"
-                    className="logo"
-                    width={35}
-                />
-            </NavLink>
-            <input id="menu__toggle" type="checkbox" />
+        <div>
+            <Link to="/Contact" className="topNav">
+                Tŝilhqot’in Language Hub | (778) 412-1112
+            </Link>
 
-            <label className="menu__btn" htmlFor="menu__toggle" aria-hidden="true">
-                <span></span>
-            </label>
-            <ul className="menu__box">
-                <Dropdown />
-            </ul>
-            <Typography component={'span'} sx={{ margin: '15px', padding: '15px' }}>
-                <NavMenu className="navLinks">
-                    <NavLink to="/About">About</NavLink>
-                    <NavLink id="radioNav" to="/Radio">
-                        104.5 FM
-                    </NavLink>
-                    <NavLink to="/Apps">Apps</NavLink>
-                    <NavLink to="/Songs">Songs</NavLink>
-                    <NavLink to="/Videos">Videos</NavLink>
-                    <NavLink to="/Teachers">Teachers</NavLink>
-                    <NavLink to="/Funders">Funders</NavLink>
-                    <NavLink to="/Links">Links</NavLink>
-                    <NavLink to="/Contact">Contact</NavLink>
-                    {/* Second Nav */}
-                    {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
-                </NavMenu>
-            </Typography>
-            <NavBtn>
-                <NavBtnLink className="hamburder-menu">
-                    <a
-                        href="https://play.google.com/store/apps/developer?id=Aaron+Plahn+%28Ts%CC%82ilhqot%E2%80%99in+National+Government%29"
-                        target={'noreferrer'}
-                    >
-                        <img
-                            src="https://www.tsilhqotin.ca/wp-content/uploads/2022/05/playstore.png"
-                            alt="playstore"
-                            width={40}
-                        />
-                    </a>
-                </NavBtnLink>
-            </NavBtn>
-        </Nav>
+            <Nav>
+                <NavLink style={{ color: 'black' }} to="/">
+                    <img
+                        src="https://www.tsilhqotin.ca/wp-content/uploads/2022/11/tsilhqotin_language_logo_final.png"
+                        alt="logo"
+                        className="logo"
+                        width={60}
+                    />
+                </NavLink>
+                <input id="menu__toggle" type="checkbox" />
+
+                <label className="menu__btn" htmlFor="menu__toggle" aria-hidden="true">
+                    <span></span>
+                </label>
+                <ul className="menu__box">
+                    <Dropdown />
+                </ul>
+                <Typography component={'span'} sx={{ margin: '15px', padding: '15px' }}>
+                    <NavMenu className="navLinks">
+                        <NavLink to="/About">About</NavLink>
+                        <NavLink id="radioNav" to="/Radio">
+                            104.5 FM
+                        </NavLink>
+                        <NavLink to="/Apps">Apps</NavLink>
+                        <NavLink to="/Songs">Songs</NavLink>
+                        <NavLink to="/Videos">Videos</NavLink>
+                        <NavLink to="/Teachers">Teachers</NavLink>
+                        <NavLink to="/Funders">Funders</NavLink>
+                        <NavLink to="/Links">Links</NavLink>
+                        <NavLink to="/Contact">Contact</NavLink>
+                        {/* Second Nav */}
+                        {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+                    </NavMenu>
+                </Typography>
+                <NavBtn>
+                    <NavBtnLink className="hamburder-menu"></NavBtnLink>
+                </NavBtn>
+            </Nav>
+        </div>
     );
 };
 
