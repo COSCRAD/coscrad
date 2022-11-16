d5997a34e458ca3be9663021bd701056df716a4b
Aaron Plahn <aplahn85@gmail.com>
2022-11-15 18:20:37 -0800

    feat(coscrad-api): expose connections in resource detail view

--- apps/coscrad-frontend/src/components/higher-order-components/aggregate-page.tsx
+++ apps/coscrad-frontend/src/components/higher-order-components/aggregate-page.tsx
@@ -1,4 +1,5 @@
-import { ResourceType } from '../../../../../libs/api-interfaces/src';
+import { ResourceType } from '@coscrad/api-interfaces';
+import { ConnectedResourcesPanel } from '../../store/slices/resources/shared/connected-resources/connected-resources-panel';
 import { useIdFromLocation } from '../../utils/custom-hooks/use-id-from-location';
 import { fullViewResourcePresenterFactory } from '../resources/factories/full-view-resource-presenter-factory';
 import { AggregateDetailContainer } from './aggregate-detail-container';
@@ -6,12 +7,15 @@ import { AggregateDetailContainer } from './aggregate-detail-container';
 export const AggregatePage = (resourceType: ResourceType): JSX.Element => {
     const id = useIdFromLocation();
 
+    const compositeIdentifier = { type: resourceType, id };
+
     return (
         <div>
             <AggregateDetailContainer
-                compositeIdentifier={{ type: resourceType, id }}
+                compositeIdentifier={compositeIdentifier}
                 detailPresenterFactory={fullViewResourcePresenterFactory}
             />
+            <ConnectedResourcesPanel compositeIdentifier={compositeIdentifier} />
         </div>
     );
 };
