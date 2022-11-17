a1b153c7151091e13e71eed8f6c3f81303ce3f8c
Aaron Plahn <aplahn85@gmail.com>
2022-11-16 16:45:43 -0800

    fix(coscrad-frontend): fix typo in media item thunk action prefix

--- apps/coscrad-frontend/src/store/slices/resources/media-items/thunks.ts
+++ apps/coscrad-frontend/src/store/slices/resources/media-items/thunks.ts
@@ -1,10 +1,10 @@
 import { buildResourceFetchActionPrefix } from '../../utils/build-resource-fetch-action-prefix';
 import { createFetchThunk } from '../../utils/create-fetch-thunk';
 import { getApiResourcesBaseRoute } from '../shared';
-import { SPATIAL_FEATURES } from '../spatial-features';
+import { MEDIA_ITEMS } from './constants';
 import { MediaItemIndexState } from './types';
 
 export const fetchMediaItems = createFetchThunk<MediaItemIndexState>(
-    buildResourceFetchActionPrefix(SPATIAL_FEATURES),
+    buildResourceFetchActionPrefix(MEDIA_ITEMS),
     `${getApiResourcesBaseRoute()}/mediaItems`
 );
