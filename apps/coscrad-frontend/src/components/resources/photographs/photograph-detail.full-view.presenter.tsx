8a2e04e07beded1de812bdd245e0872e0120cfc7
yaanahuu2 <gahorner@gmail.com>
2022-11-21 13:49:02 -0800

    WIP minor refactor to full-view presenter

--- apps/coscrad-frontend/src/components/resources/photographs/photograph-detail.full-view.presenter.tsx
+++ apps/coscrad-frontend/src/components/resources/photographs/photograph-detail.full-view.presenter.tsx
@@ -3,13 +3,11 @@ import './photograph-detail.full-view.presenter.css';
 
 export const PhotographDetailFullViewPresenter = ({
     data: { id, imageURL },
-}: IDetailQueryResult<IPhotographViewModel>): JSX.Element => {
-    return (
-        <div className="photograph-detail-container" data-testid={id}>
-            <h3>Photograph {id}</h3>
-            <div className="detail-image-container">
-                <img src={imageURL} />
-            </div>
+}: IDetailQueryResult<IPhotographViewModel>): JSX.Element => (
+    <div className="photograph-detail-container" data-testid={id}>
+        <h3>Photograph {id}</h3>
+        <div className="detail-image-container">
+            <img src={imageURL} />
         </div>
-    );
-};
+    </div>
+);
