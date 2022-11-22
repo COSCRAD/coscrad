baf31332f375ae97ba520827a027f1603bb03cf2
Justin Bambrick <jbambrick@tsilhqotin.ca>
2022-11-22 12:00:33 -0800

    style(coscrad-frontend): style index and detail presenters for term

--- apps/coscrad-frontend/src/components/resources/terms/term-detail.presenter.tsx
+++ apps/coscrad-frontend/src/components/resources/terms/term-detail.presenter.tsx
@@ -1,8 +1,29 @@
 import { IDetailQueryResult, ITermViewModel } from '@coscrad/api-interfaces';
-import { GenericDetailPresenter } from '../../../utils/generic-components/presenters/generic-detail-presenter';
+import { Card, Divider } from '@mui/material';
+import { MediaPlayer } from '../../../../../../libs/media-player/src';
+import './term-detail.presenter.css';
 
-// TODO Bring across the custom component from the `tng-dictionary` project
 // TODO[https://www.pivotaltracker.com/story/show/183681722] expose commands
-export const TermDetailPresenter = (
-    dataAndActions: IDetailQueryResult<ITermViewModel>
-): JSX.Element => GenericDetailPresenter(dataAndActions);
+export const TermDetailPresenter = ({
+    data: { term, termEnglish, audioURL, contributor },
+}: IDetailQueryResult<ITermViewModel>): JSX.Element => (
+    <div className="term-detail-page">
+        <Card className="term-detail-card">
+            <div id="term-detail-term">{term || ''}</div>
+            <Divider id="term-detail-divider" />
+            <div className="term-detail-meta">
+                <h3 className="term-detail-headers"> English: </h3>
+                {termEnglish || ''}
+            </div>
+
+            <div className="term-detail-meta">
+                <h3 className="term-detail-headers">Contributor: </h3>
+                {contributor}
+            </div>
+
+            <div id="term-media-player">
+                <MediaPlayer listenMessage="Play!" audioUrl={audioURL} />
+            </div>
+        </Card>
+    </div>
+);
