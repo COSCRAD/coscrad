e984acbba5243b14aa3ff74450d8cb51607e4760
Justin Bambrick <55550194+jbambrick@users.noreply.github.com>
2022-11-23 11:29:08 -0800

    style(coscrad-frontend): improve presentation and styling of presenters for Term (#234)
    * style(coscrad-frontend): style index and detail presenters for term
    
    * register independent thumbnail presenter for term
    
    * refactor based on PR (#234)
    
    Co-authored-by: Aaron Plahn <aplahn85@gmail.com>

---
+++ apps/coscrad-frontend/src/components/resources/terms/term-detail.thumbnail.presenter.tsx
@@ -0,0 +1,12 @@
+import { IDetailQueryResult, ITermViewModel } from '@coscrad/api-interfaces';
+import { TermDetailFullViewPresenter } from './term-detail.full-view.presenter';
+
+export const TermDetailThumbnailPresenter = (
+    termAndActions: IDetailQueryResult<ITermViewModel>
+): JSX.Element => (
+    /**
+     * For now, we will use the full-view as the thumbnail, since the full-view
+     * is fairly concise to start with.
+     */
+    <TermDetailFullViewPresenter {...termAndActions} />
+);
