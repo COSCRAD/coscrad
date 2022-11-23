0d139935be4cd652b9d9cb138fae85b9ca61f6d1
Aaron Plahn <aplahn85@gmail.com>
2022-11-23 15:06:36 -0800

    style(api): improve styling and layout of vocabulary list presenters (#233)
    * style(api): improve styling and layout of vocabulary list presenters
    
    * update styling based on PR (#233)
    
    * refactor based on PR (#233)
    
    Co-authored-by: Justin Bambrick <jbambrick@tsilhqotin.ca>

--- apps/coscrad-frontend/src/components/resources/vocabulary-lists/vocabulary-list-index.presenter.tsx
+++ apps/coscrad-frontend/src/components/resources/vocabulary-lists/vocabulary-list-index.presenter.tsx
@@ -1,13 +1,35 @@
 import { IIndexQueryResult, IVocabularyListViewModel } from '@coscrad/api-interfaces';
-import { GenericIndexPresenter } from '../../../utils/generic-components/presenters/generic-index-presenter';
+import { HeadingLabel, IndexTable } from '../../../utils/generic-components/presenters/tables';
+import { CellRenderersDefinition } from '../../../utils/generic-components/presenters/tables/generic-index-table-presenter/types/cell-renderers-definition';
+import { renderAggregateIdCell } from '../utils/render-aggregate-id-cell';
+import { formatBilingualText } from './utils';
 
-export const VocabularyListIndexPresenter = (
-    indexResult: IIndexQueryResult<IVocabularyListViewModel>
-) => (
+export const VocabularyListIndexPresenter = ({
+    data: vocabularyListsAndActions,
+}: IIndexQueryResult<IVocabularyListViewModel>) => {
     /**
-     *  TODO [https://www.pivotaltracker.com/story/show/183681839]
-     * We may some day read the actions and allow for bulk command execution in
-     * an index view.
+     * TODO [https://www.pivotaltracker.com/story/show/183681556]
+     * Remove the need to map here.
      */
-    <GenericIndexPresenter {...indexResult} indexLabel={'Vocabulary Lists'} />
-);
+    const vocabularyLists = vocabularyListsAndActions.map(({ data }) => data);
+
+    const headingLabels: HeadingLabel<IVocabularyListViewModel>[] = [
+        { propertyKey: 'id', headingLabel: 'link' },
+        { propertyKey: 'name', headingLabel: 'Vocabulary List' },
+    ];
+
+    const cellRenderersDefinition: CellRenderersDefinition<IVocabularyListViewModel> = {
+        id: renderAggregateIdCell,
+        name: ({ name, nameEnglish }) => formatBilingualText(name, nameEnglish),
+    };
+
+    return (
+        <IndexTable
+            headingLabels={headingLabels}
+            tableData={vocabularyLists}
+            cellRenderersDefinition={cellRenderersDefinition}
+            // This should be a resource label from resource info
+            heading={'Vocabulary Lists'}
+        />
+    );
+};
