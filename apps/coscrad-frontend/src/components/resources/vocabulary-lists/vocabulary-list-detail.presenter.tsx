038719c6f25195cc3b891118b4606ad5c0726896
Aaron Plahn <aplahn85@gmail.com>
2022-11-23 15:06:36 -0800

    style(coscrad-frontend): improve styling and layout of vocabulary list presenters (#233)
    * style(api): improve styling and layout of vocabulary list presenters
    
    * update styling based on PR (#233)
    
    * refactor based on PR (#233)
    
    Co-authored-by: Justin Bambrick <jbambrick@tsilhqotin.ca>

--- apps/coscrad-frontend/src/components/resources/vocabulary-lists/vocabulary-list-detail.presenter.tsx
+++
@@ -1,6 +0,0 @@
-import { IDetailQueryResult, IVocabularyListViewModel } from '@coscrad/api-interfaces';
-import { GenericDetailPresenter } from '../../../utils/generic-components/presenters/generic-detail-presenter';
-
-export const VocabularyListDetailPresenter = (
-    vocabularyListAndActions: IDetailQueryResult<IVocabularyListViewModel>
-): JSX.Element => GenericDetailPresenter(vocabularyListAndActions);
