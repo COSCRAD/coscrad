11fcb85a278f84cfb1ac416b0e746ef608dd7891
Aaron Plahn <aplahn85@gmail.com>
2022-11-29 17:45:36 -0800

    style(coscrad-frontend): update resource info presentation

--- apps/coscrad-frontend/src/components/AllResources/ResourceInfoPresenter/ResourceInfo.presenter.tsx
+++ apps/coscrad-frontend/src/components/AllResources/ResourceInfoPresenter/ResourceInfo.presenter.tsx
@@ -1,4 +1,13 @@
 import { IAggregateInfo, ResourceType } from '@coscrad/api-interfaces';
+import { LinkSharp as LinkIcon } from '@mui/icons-material';
+import {
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    CardHeader,
+    IconButton,
+} from '@mui/material';
 import { Link } from 'react-router-dom';
 import { routes } from '../../../app/routes/routes';
 import './ResourceInfo.presenter.css';
@@ -11,22 +20,26 @@ export const ResourceInfoPresenter = ({
  * TODO [https://www.pivotaltracker.com/story/show/183766033
  * We should expose the Schema as part of our own API docs somehow.
  */
-IAggregateInfo<ResourceType>): JSX.Element => {
-    const resourceIndexLink = (
-        <Link to={`/${routes.resources.ofType(resourceType).index}`}>View {label}s</Link>
-    );
-
-    return (
-        <>
-            <h3>
-                {label} ({resourceType})
-            </h3>
-
-            <div className="resource-meta" data-testid={label}>
-                {resourceIndexLink}
-
-                <div>{description}</div>
-            </div>
-        </>
-    );
-};
+IAggregateInfo<ResourceType>): JSX.Element => (
+    <div>
+        <Card>
+            {/* TODO Handle pluralization properly as soon as we have a Resource Type whose plural form is irregular */}
+            <CardHeader title={`${label}s`} />
+            <CardContent>
+                <div className="resource-meta" data-testid={label}>
+                    {description}
+                </div>
+            </CardContent>
+            <CardActionArea>
+                <CardActions>
+                    <Link to={`/${routes.resources.ofType(resourceType).index}`}>
+                        <IconButton aria-label="View">
+                            <LinkIcon />
+                        </IconButton>
+                    </Link>
+                </CardActions>
+            </CardActionArea>
+        </Card>
+        <br />
+    </div>
+);
--- apps/coscrad-frontend/src/components/AllResources/ResourceInfos.presenter.tsx
+++ apps/coscrad-frontend/src/components/AllResources/ResourceInfos.presenter.tsx
@@ -1,4 +1,5 @@
 import { IAggregateInfo, ResourceType } from '@coscrad/api-interfaces';
+import { Divider } from '@mui/material';
 import { FunctionalComponent } from '../../utils/types/functional-component';
 import { HasData } from '../higher-order-components';
 import { ResourceInfoPresenter } from './ResourceInfoPresenter';
@@ -12,10 +13,11 @@ export const ResourceInfosPresenter: FunctionalComponent<
 > = ({ data: resourceInfos }: HasData<IAggregateInfo<ResourceType>[]>) => (
     <div>
         <h2>Available Resources</h2>
-        <div>
-            {resourceInfos.map((info) => (
-                <ResourceInfoPresenter {...info} key={info.type}></ResourceInfoPresenter>
-            ))}
-        </div>
+        <Divider />
+        <br />
+        <br />
+        {resourceInfos.map((info) => (
+            <ResourceInfoPresenter {...info} key={info.type}></ResourceInfoPresenter>
+        ))}
     </div>
 );
