31823e3e5f984ead542c79da8690e05f9475e491
Aaron Plahn <aplahn85@gmail.com>
2022-12-05 08:55:17 -0800

    style(coscrad-frontend): update resource info presentation (#243)
    * style(coscrad-frontend): update resource info presentation
    
    * Refactor based on PR (#243)

--- apps/coscrad-frontend/src/components/AllResources/ResourceInfos.presenter.tsx
+++ apps/coscrad-frontend/src/components/AllResources/ResourceInfos.presenter.tsx
@@ -1,4 +1,5 @@
 import { IAggregateInfo, ResourceType } from '@coscrad/api-interfaces';
+import { Divider, Stack } from '@mui/material';
 import { FunctionalComponent } from '../../utils/types/functional-component';
 import { HasData } from '../higher-order-components';
 import { ResourceInfoPresenter } from './ResourceInfoPresenter';
@@ -12,10 +13,11 @@ export const ResourceInfosPresenter: FunctionalComponent<
 > = ({ data: resourceInfos }: HasData<IAggregateInfo<ResourceType>[]>) => (
     <div>
         <h2>Available Resources</h2>
-        <div>
+        <Divider />
+        <Stack spacing={1}>
             {resourceInfos.map((info) => (
                 <ResourceInfoPresenter {...info} key={info.type}></ResourceInfoPresenter>
             ))}
-        </div>
+        </Stack>
     </div>
 );
