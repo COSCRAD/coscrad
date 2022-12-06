9f8ed91bb812c187bba52efc2b219fe906b5189f
Aaron Plahn <aplahn85@gmail.com>
2022-12-06 15:22:37 -0800

    Fix SVG issue in jest setup tests

--- apps/coscrad-frontend/setupTests.js
+++ apps/coscrad-frontend/setupTests.js
@@ -1,2 +1,13 @@
 // Polyfill "window.fetch" in Jest runtime
 import 'whatwg-fetch';
+
+var createElementNSOrig = global.document.createElementNS;
+global.document.createElementNS = function (namespaceURI, qualifiedName) {
+    if (namespaceURI === 'http://www.w3.org/2000/svg' && qualifiedName === 'svg') {
+        var element = createElementNSOrig.apply(this, arguments);
+        /* eslint-disable */
+        element.createSVGRect = function () {};
+        return element;
+    }
+    return createElementNSOrig.apply(this, arguments);
+};
