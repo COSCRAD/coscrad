3e5883be2bb6be0a0da4f7d1f3aa84c53317461e
Aaron Plahn <aplahn85@gmail.com>
2022-12-07 12:38:21 -0800

    feat(coscrad-frontend): join resource views into category tree (#253)
    * feat(coscrad-frontend): join resource views into category tree
    
    * refactor based on PR (#253)

--- apps/coscrad-frontend/src/components/tree-of-knowledge/category-tree.presenter.tsx
+++ apps/coscrad-frontend/src/components/tree-of-knowledge/category-tree.presenter.tsx
@@ -1,15 +1,43 @@
-import { ICategoryTreeViewModel } from '@coscrad/api-interfaces';
+import { CategorizableType, ICategoryTreeViewModel } from '@coscrad/api-interfaces';
+import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material';
 import { FunctionalComponent } from '../../utils/types/functional-component';
+import { CategorizablesOfMultipleTypeContainer } from '../higher-order-components';
+import { thumbnailCategorizableDetailPresenterFactory } from '../resources/factories/thumbnail-categorizable-detail-presenter-factory';
+import { createMultipleCategorizableLookupTable } from '../tags/createMultipleCategorizableLookupTable';
+
+const wrapTree = ({
+    label,
+    children,
+    members,
+}: ICategoryTreeViewModel<CategorizableType>): JSX.Element => (
+    <div>
+        {
+            <Accordion>
+                <AccordionSummary>{label}</AccordionSummary>
+                <AccordionDetails>
+                    <CategorizablesOfMultipleTypeContainer
+                        categorizableTypeAndIds={createMultipleCategorizableLookupTable(members)}
+                        detailPresenterFactory={thumbnailCategorizableDetailPresenterFactory}
+                    />
+                    {
+                        // recurse on the children
+                        children.map(wrapTree)
+                    }
+                </AccordionDetails>
+            </Accordion>
+        }
+    </div>
+);
 
 /**
  * Note that this same presenter can also be used to present a sub-tree, thanks
  * to self-similarity.
  */
 export const CategoryTreePresenter: FunctionalComponent<ICategoryTreeViewModel> = (
-    tree: ICategoryTreeViewModel
+    tree: ICategoryTreeViewModel<CategorizableType>
 ) => (
     <div data-testid="categoryTree">
         <h2>Tree of Knowledge</h2>
-        {JSON.stringify(tree)}
+        {wrapTree(tree)}
     </div>
 );
