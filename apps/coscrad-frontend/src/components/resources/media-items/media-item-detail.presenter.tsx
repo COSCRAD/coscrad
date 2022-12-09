d9209f0cd4012dee9e8e3acc2bc84163598f1daa
Justin Bambrick <55550194+jbambrick@users.noreply.github.com>
2022-12-09 14:00:52 -0800

    style(coscrad-frontend): update media item index and detial presenters (#251)
    * style(coscrad-frontend): update mediaItems index-to-detail
    
    * Refactor based on PR (#251)
    
    * Fix front-end test failures an warnings
    
    Co-authored-by: Aaron Plahn <aplahn85@gmail.com>

--- apps/coscrad-frontend/src/components/resources/media-items/media-item-detail.presenter.tsx
+++
@@ -1,6 +0,0 @@
-import { IDetailQueryResult, IMediaItemViewModel } from '@coscrad/api-interfaces';
-import { GenericDetailPresenter } from '../../../utils/generic-components/presenters/generic-detail-presenter';
-
-export const MediaItemDetailPresenter = (
-    mediaItemAndActions: IDetailQueryResult<IMediaItemViewModel>
-): JSX.Element => GenericDetailPresenter(mediaItemAndActions);
---
+++ apps/coscrad-frontend/src/components/resources/media-items/media-item-detail.thumbnail.presenter.tsx
@@ -0,0 +1,24 @@
+import { IDetailQueryResult, IMediaItemViewModel } from '@coscrad/api-interfaces';
+import { Card, Divider } from '@mui/material';
+import { renderMediaLengthInSeconds } from '../utils/render-media-length-in-seconds-cell';
+import { formatBilingualText } from '../vocabulary-lists/utils';
+
+export const MediaItemDetailThumbnailPresenter = ({
+    data: { id, title, titleEnglish, url, lengthMilliseconds },
+}: IDetailQueryResult<IMediaItemViewModel>): JSX.Element => {
+    return (
+        <div data-testid={id}>
+            <Card className="detail-card">
+                <div id="detail-term" className="detail-meta">
+                    {formatBilingualText(title, titleEnglish)}
+                </div>
+                <Divider id="detail-divider" />
+                <div className="detail-meta">
+                    <h3>Length:</h3>
+                    {renderMediaLengthInSeconds(lengthMilliseconds)}
+                </div>
+                <div className="detail-meta">{url}</div>
+            </Card>
+        </div>
+    );
+};
