9e5764673777006ab3043e5642ae3dd510a7611c
Aaron Plahn <aplahn85@gmail.com>
2022-12-20 13:34:15 -0800

    Support property labels and descriptions in data-types lib

--- libs/data-types/src/lib/decorators/common/mixinDefaultTypeDecoratorOptions.ts
+++ libs/data-types/src/lib/decorators/common/mixinDefaultTypeDecoratorOptions.ts
@@ -1,7 +1,7 @@
 import { TypeDecoratorOptions } from '../types/TypeDecoratorOptions';
 import buildDefaultTypeDecoratorOptions from './buildDefaultTypeDecoratorOptions';
 
-export default (userOptions: Partial<TypeDecoratorOptions>): TypeDecoratorOptions => ({
+export default (userOptions: TypeDecoratorOptions): Required<TypeDecoratorOptions> => ({
     ...buildDefaultTypeDecoratorOptions(),
     ...userOptions,
 });
