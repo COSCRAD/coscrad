a72dcf4af60cd05fef399007a6988aeeea1070df
Aaron Plahn <aplahn85@gmail.com>
2022-12-26 09:27:09 -0800

    feat(coscrad): expose tag commands (#266)

--- apps/coscrad-frontend/src/components/tags/tag-index.container.tsx
+++ apps/coscrad-frontend/src/components/tags/tag-index.container.tsx
@@ -1,5 +1,4 @@
 import { useLoadableTags } from '../../store/slices/tagSlice/hooks/use-loadable-tags';
-import { wrapArrayProps } from '../../utils/prop-manipulation/wrap-array-props';
 import { displayLoadableWithErrorsAndLoading } from '../higher-order-components';
 import { TagIndexPresenter } from './tag-index.presenter';
 
@@ -7,11 +6,7 @@ export const TagIndexContainer = (): JSX.Element => {
     const loadableTags = useLoadableTags();
 
     // wrap the presenter with handling for errors and pending state
-    const LoadableTagPresenter = displayLoadableWithErrorsAndLoading(
-        TagIndexPresenter,
-        // wrap the loaded array in a nested prop
-        wrapArrayProps
-    );
+    const LoadableTagPresenter = displayLoadableWithErrorsAndLoading(TagIndexPresenter);
 
     return <LoadableTagPresenter {...loadableTags} />;
 };
