eec4e3c436066baa5588523736e07c18283fafb9
Aaron Plahn <aplahn85@gmail.com>
2022-12-26 15:06:24 -0800

    feat(coscrad-frontend): acquire IDs for index-scoped commands (#265)
    * feat(coscrad-frontend): acquire IDs for index-scoped commands
    
    * Refactor based on PR (#265)

--- apps/coscrad-frontend/src/components/higher-order-components/aggregate-index-container.tsx
+++ apps/coscrad-frontend/src/components/higher-order-components/aggregate-index-container.tsx
@@ -1,20 +1,23 @@
-import { IBaseViewModel, IIndexQueryResult } from '@coscrad/api-interfaces';
+import { AggregateType, IBaseViewModel, IIndexQueryResult } from '@coscrad/api-interfaces';
 import { ILoadable } from '../../store/slices/interfaces/loadable.interface';
 import { FunctionalComponent } from '../../utils/types/functional-component';
-import { CommandPanel, INDEX_COMMAND_CONTEXT } from '../commands';
+import { CommandPanel } from '../commands';
 import { displayLoadableWithErrorsAndLoading } from './display-loadable-with-errors-and-loading';
 
 export interface AggregateIndexContainerProps<
     T extends IIndexQueryResult<IBaseViewModel>,
     UPresenterProps = T
 > {
+    // TODO Consider building this from the aggregate type
     useLoadableModels: () => ILoadable<T>;
     IndexPresenter: FunctionalComponent<UPresenterProps>;
+    aggregateType: AggregateType;
 }
 
 export const AggregateIndexContainer = <T extends IIndexQueryResult<IBaseViewModel>, U = T>({
     useLoadableModels,
     IndexPresenter,
+    aggregateType,
 }: AggregateIndexContainerProps<T, U>): JSX.Element => {
     const loadableModels = useLoadableModels();
 
@@ -28,7 +31,7 @@ export const AggregateIndexContainer = <T extends IIndexQueryResult<IBaseViewMod
             {loadableModels.data?.indexScopedActions && (
                 <CommandPanel
                     actions={loadableModels.data.indexScopedActions}
-                    commandContext={INDEX_COMMAND_CONTEXT}
+                    commandContext={aggregateType}
                 />
             )}
         </div>
