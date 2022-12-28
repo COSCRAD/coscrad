2f11a155d4a07707b423789179af95fdac312899
Aaron Plahn <aplahn85@gmail.com>
2022-12-27 17:43:53 -0800

    feat(api): introduce RELABEL_TAG (#268)
    * feat(api): introduce RELABEL_TAG
    
    * Refactor based on PR (#268)

--- apps/api/src/domain/models/tag/commands/create-tag.command.ts
+++ apps/api/src/domain/models/tag/commands/create-tag.command.ts
@@ -1,33 +1,13 @@
 import { Command } from '@coscrad/commands';
-import { NestedDataType, NonEmptyString, UUID } from '@coscrad/data-types';
-import { Equals } from '@coscrad/validation';
+import { NestedDataType, NonEmptyString } from '@coscrad/data-types';
 import { AggregateCompositeIdentifier } from '../../../types/AggregateCompositeIdentifier';
 import { AggregateType } from '../../../types/AggregateType';
 import { ICreateCommand } from '../../shared/command-handlers/interfaces/create-command.interface';
-
-class TagCompositeId {
-    /**
-     * This is a bit of a hack. It circumvents our `CoscradDataTypes` and may
-     * cause problems for
-     * - Schema management
-     * - Anyone using our API directly (not via front-end)
-     *
-     * The simple answer is that you always have to tack on an
-     * `aggregateCompositeIdentifier`.
-     */
-    @Equals(AggregateType.tag)
-    type = AggregateType.tag;
-
-    @UUID({
-        label: 'ID',
-        description: 'unique identifier',
-    })
-    id: string;
-}
+import { TagCompositeIdentifier } from './tag-composite-identifier';
 
 @Command({ type: 'CREATE_TAG', label: 'Create Tag', description: 'Creates a new tag' })
 export class CreateTag implements ICreateCommand {
-    @NestedDataType(TagCompositeId, {
+    @NestedDataType(TagCompositeIdentifier, {
         label: 'Composite Identifier',
         description: 'system-wide unique identifier',
     })
