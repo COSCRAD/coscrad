f4be414409a1ca714a5aadb546ba9b96dc22cf1c
Aaron Plahn <aplahn85@gmail.com>
2023-01-12 16:36:24 -0800

    feat(api): make resources menu configurable

--- apps/coscrad-frontend/src/components/resource-info/presenters/resource-info.presenter.tsx
+++ apps/coscrad-frontend/src/components/resource-info/presenters/resource-info.presenter.tsx
@@ -1,19 +1,18 @@
 import { IAggregateInfo, ResourceType } from '@coscrad/api-interfaces';
 import { Card, CardActionArea, CardActions, CardContent, CardHeader } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { routes } from '../../../app/routes/routes';
 import './resource-info.presenter.css';
 
 export const ResourceInfoPresenter = ({
-    type: resourceType,
     description,
     label,
+    route,
 }: /**
  * TODO [https://www.pivotaltracker.com/story/show/183766033
  * We should expose the Schema as part of our own API docs somehow.
  */
-IAggregateInfo<ResourceType>): JSX.Element => (
-    <Link to={`/${routes.resources.ofType(resourceType).index}`}>
+IAggregateInfo<ResourceType> & { route: string }): JSX.Element => (
+    <Link to={route}>
         <Card>
             {/* TODO Handle pluralization properly as soon as we have a Resource Type whose plural form is irregular */}
             <CardHeader title={`${label}s`} />
--- apps/coscrad-frontend/src/components/resource-info/presenters/resource-infos.presenter.tsx
+++ apps/coscrad-frontend/src/components/resource-info/presenters/resource-infos.presenter.tsx
@@ -2,22 +2,34 @@ import { IAggregateInfo, ResourceType } from '@coscrad/api-interfaces';
 import { Divider, Stack } from '@mui/material';
 import { FunctionalComponent } from '../../../utils/types/functional-component';
 import { HasData } from '../../higher-order-components';
+import { ResourceInfoContainerProps } from '../resource-info.container';
 import { ResourceInfoPresenter } from './resource-info.presenter';
 
+type ResourceInfosPresenterProps = HasData<IAggregateInfo<ResourceType>[]> &
+    ResourceInfoContainerProps;
 /**
  * Note the plural in the name. This presents all of the resource infos. It
  * maps through the singular `ResourceInfoPresenter` to do so.
  */
-export const ResourceInfosPresenter: FunctionalComponent<
-    HasData<IAggregateInfo<ResourceType>[]>
-> = ({ data: resourceInfos }: HasData<IAggregateInfo<ResourceType>[]>) => (
+export const ResourceInfosPresenter: FunctionalComponent<ResourceInfosPresenterProps> = ({
+    data: resourceInfos,
+    resourceTypesAndLabels,
+    resourceTypesAndRoutes,
+}: ResourceInfosPresenterProps) => (
     <div>
         <h2>Available Resources</h2>
         <Divider />
         <Stack spacing={1}>
-            {resourceInfos.map((info) => (
-                <ResourceInfoPresenter {...info} key={info.type}></ResourceInfoPresenter>
-            ))}
+            {resourceInfos
+                .filter(({ type }) => Object.keys(resourceTypesAndLabels).includes(type))
+                .map((info) => (
+                    <ResourceInfoPresenter
+                        {...info}
+                        key={info.type}
+                        label={resourceTypesAndLabels[info.type]}
+                        route={resourceTypesAndRoutes[info.type]}
+                    ></ResourceInfoPresenter>
+                ))}
         </Stack>
     </div>
 );
