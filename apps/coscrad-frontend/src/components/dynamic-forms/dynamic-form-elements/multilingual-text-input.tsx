ee472265320a9621307114a3b5838c278c7c9050
Aaron Plahn <aplahn85@gmail.com>
2023-02-08 17:51:02 -0800

    WIP Multilingual text form

--- apps/coscrad-frontend/src/components/dynamic-forms/dynamic-form-elements/multilingual-text-input.tsx
+++ apps/coscrad-frontend/src/components/dynamic-forms/dynamic-form-elements/multilingual-text-input.tsx
@@ -1,39 +1,117 @@
 import { IFormField } from '@coscrad/api-interfaces';
-import { TextField } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, TextField } from '@mui/material';
+import { useFormState } from '../form-state';
+
+type OnInputChange = (name: string, value: unknown) => void;
 
 interface MultilingualTextInputProps {
     formField: IFormField;
-    onInputChange?: (name: string, value: unknown) => void;
+    onInputChange?: OnInputChange;
     required: boolean;
 }
 
+interface SingleLanguageFieldProps {
+    name: string;
+    label: string;
+    languageId: string;
+    languageName: string;
+    currentFormValue: Record<string, unknown>;
+    onInputChange: OnInputChange;
+}
+
+const SingleLanguageField = ({
+    name,
+    label,
+    languageId,
+    languageName,
+    currentFormValue,
+    onInputChange,
+}: SingleLanguageFieldProps) => (
+    <Accordion>
+        <AccordionSummary>{languageName}</AccordionSummary>
+        <AccordionDetails>
+            <div>
+                <TextField
+                    name={`${name}:${languageId}`}
+                    label={label}
+                    onChange={(e) => {
+                        const newItem = {
+                            languageId: languageId,
+                            text: e.target.value,
+                            role: 'original',
+                        };
+
+                        console.log({ currentFormValue });
+
+                        // TODO type safety!
+                        const multiLingualTextValue = {
+                            items: [
+                                // TODO fix types
+                                ...(currentFormValue.items as { languageId: string }[]).filter(
+                                    (item) => item.languageId !== languageId
+                                ),
+                                newItem,
+                            ],
+                        };
+
+                        console.log({ multiLingualTextValue });
+
+                        onInputChange(name, multiLingualTextValue);
+
+                        // updateCurrentValue('items', multiLingualTextValue.items);
+                    }}
+                    required={false}
+                />
+            </div>
+        </AccordionDetails>
+    </Accordion>
+);
+
+/**
+ * TODO We need to
+ * - generate one field for each available language based on a config \ backend query
+ * - populate a dropbox for selecting the "role" of the text (original \ prompt \ gloss \ free translation)
+ *
+ */
 export const MultilingualTextInput = ({
     formField: { name, label },
     onInputChange,
-    required,
-}: MultilingualTextInputProps): JSX.Element => (
-    <div>
-        English:
-        <TextField
-            name={`${name}:eng`}
-            label={label}
-            onChange={(e) => {
-                // TODO type safety!
-                const multiLingualTextValue = {
-                    items: [
-                        {
-                            languageId: 'eng',
-                            text: e.target.value,
-                            role: 'original',
-                        },
-                    ],
-                };
+}: MultilingualTextInputProps): JSX.Element => {
+    const [currentValue, updateCurrentValue] = useFormState({
+        items: [],
+    });
 
-                console.log({ multiLingualTextValue });
+    const onInputChangeWithStateUpdate = (name: string, value: unknown) => {
+        onInputChange(name, value);
 
-                onInputChange(name, multiLingualTextValue);
-            }}
-            required={required}
-        />
-    </div>
-);
+        updateCurrentValue('items', (value as { items: Array<unknown> }).items);
+    };
+
+    return (
+        <>
+            {[
+                {
+                    languageId: 'hai',
+                    languageName: 'Haida',
+                },
+                {
+                    languageId: 'clc',
+                    languageName: 'Tŝilhqot’in',
+                },
+                {
+                    languageId: 'eng',
+                    languageName: 'English',
+                },
+            ].map(({ languageId, languageName }) => (
+                <SingleLanguageField
+                    languageId={languageId}
+                    languageName={languageName}
+                    onInputChange={onInputChangeWithStateUpdate}
+                    name={name}
+                    label={label}
+                    currentFormValue={currentValue}
+                />
+            ))}
+        </>
+    );
+};
--- apps/coscrad-frontend/src/components/dynamic-forms/form-state/hooks.tsx
+++ apps/coscrad-frontend/src/components/dynamic-forms/form-state/hooks.tsx
@@ -2,8 +2,8 @@ import { useReducer } from 'react';
 import { updateFormState } from './actions/update-form-state';
 import { formStateReducer } from './form-state-reducer';
 
-export const useFormState = () => {
-    const [formState, dispatch] = useReducer(formStateReducer, {});
+export const useFormState = (initialValue: Record<string, unknown> = {}) => {
+    const [formState, dispatch] = useReducer(formStateReducer, initialValue);
 
     const updateForm = (propertyKey: string, value: unknown) =>
         dispatch(updateFormState(propertyKey, value));
