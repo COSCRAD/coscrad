11b2ad02996858c270da6cbb1d8f33847aaa03e3
Aaron Plahn <aplahn85@gmail.com>
2023-02-14 18:31:37 -0800

    feat(api): support multilingual text for audio item names (#309)
    fix front-end tests

---
+++ apps/coscrad-frontend/src/utils/generic-components/presenters/multilingual-text-presenter.tsx
@@ -0,0 +1,26 @@
+import { IMultilingualText } from '@coscrad/api-interfaces';
+
+export interface MultilingualTextPresenterProps {
+    text: IMultilingualText;
+}
+
+export const MultilingualTextPresenter = ({
+    text,
+}: MultilingualTextPresenterProps): JSX.Element => {
+    const { items } = text;
+
+    if (!Array.isArray(items)) {
+        throw new Error(`invalid input to Multlingual text!: ${text}`);
+    }
+
+    return (
+        <>
+            {/* TODO We need to separate the original in some way */}
+            {items.map(({ languageCode, text, role }) => (
+                <div>
+                    {`{${languageCode}} [${role}]`} {text}
+                </div>
+            ))}
+        </>
+    );
+};
