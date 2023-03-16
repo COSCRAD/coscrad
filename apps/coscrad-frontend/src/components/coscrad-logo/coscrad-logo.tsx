8ebd770a692d2fcef49302ddbcbd051864caa1e8
yaanahuu2 <63014783+yaanahuu2@users.noreply.github.com>
2023-03-16 11:32:00 -0700

    style(coscrad-frontend): update image view components with MUI styled  (#333)
    * WIP modify image view components
    
    * WIP fixed lint errors
    
    ---------
    
    Co-authored-by: yaanahuu2 <gahorner@gmail.com>

--- apps/coscrad-frontend/src/components/coscrad-logo/coscrad-logo.tsx
+++ apps/coscrad-frontend/src/components/coscrad-logo/coscrad-logo.tsx
@@ -5,14 +5,5 @@ import { StyledImage } from '../../utils/generic-components/presenters/styled-im
 export const COSCRADLogo = (): JSX.Element => {
     const { coscradLogoUrl } = useContext(ConfigurableContentContext);
 
-    const testId = '455';
-
-    return (
-        <StyledImage
-            className={`test-${testId}`}
-            sx={{ width: '100px' }}
-            src={coscradLogoUrl}
-            alt="COSCRAD Logo"
-        />
-    );
+    return <StyledImage sx={{ width: '100px' }} src={coscradLogoUrl} alt="COSCRAD Logo" />;
 };
