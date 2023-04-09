e85f5393d6613f15ba5a9893e360edb4fc2c3bf7
yaanahuu2 <63014783+yaanahuu2@users.noreply.github.com>
2023-04-09 13:51:21 -0700

    style(coscrad-frontend): remove css import from audio item presenters (#345)
    * WIP removed css import from presenters
    
    * WIP address review comments
    
    * WIP fixed video-detail test
    
    ---------
    
    Co-authored-by: yaanahuu2 <gahorner@gmail.com>

--- apps/coscrad-frontend/src/components/resources/audio-item/audio-item-detail.full-view.presenter.tsx
+++ apps/coscrad-frontend/src/components/resources/audio-item/audio-item-detail.full-view.presenter.tsx
@@ -1,11 +1,14 @@
-import { IAudioItemViewModel, ICategorizableDetailQueryResult } from '@coscrad/api-interfaces';
+import {
+    IAudioItemViewModel,
+    ICategorizableDetailQueryResult,
+    ResourceType,
+} from '@coscrad/api-interfaces';
 import { MediaPlayer } from '@coscrad/media-player';
-import { Stack } from '@mui/material';
-import { FloatSpacerDiv } from '../../../utils/generic-components';
-import { MultilingualTextPresenter } from '../../../utils/generic-components/presenters/multilingual-text-presenter';
+import { Box, Grid, Typography } from '@mui/material';
+import { ResourceDetailFullViewPresenter } from '../../../utils/generic-components/presenters/detail-views';
 import { SinglePropertyPresenter } from '../../../utils/generic-components/presenters/single-property-presenter';
+import { CoscradMainContentContainer } from '../../../utils/generic-components/style-components/coscrad-main-content-container';
 import { convertMillisecondsToSeconds } from '../utils/math';
-import './audio-item-detail.full-view.presenter.css';
 
 export const AudioItemDetailFullViewPresenter = ({
     id,
@@ -13,26 +16,35 @@ export const AudioItemDetailFullViewPresenter = ({
     audioURL,
     text: plainText,
     name,
-}: ICategorizableDetailQueryResult<IAudioItemViewModel>): JSX.Element => (
-    <div className="detail-full-view-container" title="View Connected Photograph" data-testid={id}>
-        <div className="detail-full-view-media-container">
-            <MediaPlayer audioUrl={audioURL} />
-        </div>
-        <div className="detail-full-view-meta-container">
-            <MultilingualTextPresenter text={name} />
-            <SinglePropertyPresenter display="Audio Item ID" value={id} />
-            <SinglePropertyPresenter
-                display="Duration"
-                value={`${convertMillisecondsToSeconds(lengthMilliseconds)} secs`}
-            />
-            <SinglePropertyPresenter display="Audio Url" value={audioURL} />
-            {/* TODO We should send the full Multilingual text to the front-end in the view model */}
-            <Stack>
-                {plainText.split('\n').map((line, index) => (
-                    <p key={index}>{line}</p>
-                ))}
-            </Stack>
-        </div>
-        <FloatSpacerDiv />
-    </div>
-);
+}: ICategorizableDetailQueryResult<IAudioItemViewModel>): JSX.Element => {
+    const formatedPlainText = plainText.split('\n').map((line, index) => (
+        <Box mb={1} key={index}>
+            {line}
+        </Box>
+    ));
+
+    return (
+        <CoscradMainContentContainer>
+            <ResourceDetailFullViewPresenter name={name} id={id} type={ResourceType.audioItem}>
+                <div data-testid={id} />
+                <MediaPlayer audioUrl={audioURL} />
+                <SinglePropertyPresenter
+                    display="Duration"
+                    value={`${convertMillisecondsToSeconds(lengthMilliseconds)} secs`}
+                />
+                <SinglePropertyPresenter display="Audio Url" value={audioURL} />
+                <Grid container columns={6} sx={{ mb: 1 }}>
+                    <Grid item xs={1}>
+                        {/* TODO: introduce bold (not heading) custom theme typography variant? */}
+                        <Typography component={'span'} sx={{ fontWeight: 'bold' }}>
+                            Text:
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={5}>
+                        {formatedPlainText}
+                    </Grid>
+                </Grid>
+            </ResourceDetailFullViewPresenter>
+        </CoscradMainContentContainer>
+    );
+};
