e3d488bf8c5b0b5e271ac51ca5aa94635f73c1e6
yaanahuu2 <gahorner@gmail.com>
2023-04-13 10:35:05 -0700

    WIP remove custom cypress command

--- apps/coscrad-frontend-e2e/src/support/commands.ts
+++ apps/coscrad-frontend-e2e/src/support/commands.ts
@@ -5,8 +5,6 @@ declare namespace Cypress {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interface Chainable<Subject> {
         login(email: string, password: string): void;
-
-        dragMapFromCenter(element): void;
     }
 }
 
