3ea638083edfd15706274b01e62b370cde336a5f
yaanahuu2 <63014783+yaanahuu2@users.noreply.github.com>
2023-04-20 14:05:05 -0700

    style(coscrad-frontend): remove css from term presenters (#353)
    * WIP update term presenter and remove CSS
    
    * WIP delete generic presenter css file
    
    * WIP fix tests and multiple-property-presenter
    
    * refactor(coscrad-frontend): tighten up multi-property presenter prop types
    
    * WIP add single optional property presenter
    
    ---------
    
    Co-authored-by: yaanahuu2 <gahorner@gmail.com>
    Co-authored-by: Aaron Plahn <aplahn85@gmail.com>

--- apps/coscrad-frontend/src/components/resources/terms/term-detail.thumbnail.presenter.tsx
+++ apps/coscrad-frontend/src/components/resources/terms/term-detail.thumbnail.presenter.tsx
@@ -1,12 +1,33 @@
-import { ICategorizableDetailQueryResult, ITermViewModel } from '@coscrad/api-interfaces';
-import { TermDetailFullViewPresenter } from './term-detail.full-view.presenter';
+import {
+    ICategorizableDetailQueryResult,
+    ITermViewModel,
+    ResourceType,
+} from '@coscrad/api-interfaces';
+import { MediaPlayer } from '@coscrad/media-player';
+import { Typography } from '@mui/material';
+import {
+    ResourceDetailThumbnailPresenter,
+    SingleOptionalPropertyPresenter,
+} from '../../../utils/generic-components/';
 
-export const TermDetailThumbnailPresenter = (
-    termAndActions: ICategorizableDetailQueryResult<ITermViewModel>
-): JSX.Element => (
-    /**
-     * For now, we will use the full-view as the thumbnail, since the full-view
-     * is fairly concise to start with.
-     */
-    <TermDetailFullViewPresenter {...termAndActions} />
-);
+export const TermDetailThumbnailPresenter = ({
+    id,
+    term,
+    termEnglish,
+    contributor,
+    audioURL,
+}: ICategorizableDetailQueryResult<ITermViewModel>): JSX.Element => {
+    // Temporary workaround until `name` is on IBaseViewModel
+    const name = term;
+
+    return (
+        <ResourceDetailThumbnailPresenter id={id} name={name} type={ResourceType.term}>
+            <div data-testid={id} />
+            <Typography variant="h4">{termEnglish}</Typography>
+            <SingleOptionalPropertyPresenter display="Contributor" value={contributor} />
+            <div id="media-player">
+                <MediaPlayer listenMessage="Play!" audioUrl={audioURL} />
+            </div>
+        </ResourceDetailThumbnailPresenter>
+    );
+};
