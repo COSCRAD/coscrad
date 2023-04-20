10f40e723f5a9d505b7b3c724672c12bc0a73821
yaanahuu2 <gahorner@gmail.com>
2023-04-19 22:19:51 -0700

    WIP add copy button and dialog

--- apps/coscrad-frontend/src/utils/generic-components/presenters/id-info-icon.tsx
+++ apps/coscrad-frontend/src/utils/generic-components/presenters/id-info-icon.tsx
@@ -1,16 +1,67 @@
 import { ResourceType } from '@coscrad/api-interfaces';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import InfoIcon from '@mui/icons-material/Info';
-import { Box, Tooltip } from '@mui/material';
+import {
+    Dialog,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    IconButton,
+    Tooltip,
+    Typography,
+} from '@mui/material';
+import { useState } from 'react';
 
 interface IdInfoIconProps {
     id: string;
     type: ResourceType;
 }
 
-export const IdInfoIcon = ({ id, type }: IdInfoIconProps): JSX.Element => (
-    <Box component="span">
-        <Tooltip title={`ID: ${type}/${id}`}>
-            <InfoIcon />
-        </Tooltip>
-    </Box>
-);
+export const IdInfoIcon = ({ id, type }: IdInfoIconProps): JSX.Element => {
+    const [openDialog, setOpenDialog] = useState(false);
+
+    const [showTooltip, setShowTooltip] = useState(false);
+
+    const handleDialogOpen = () => {
+        setOpenDialog(true);
+    };
+
+    const handleDialogClose = () => {
+        setOpenDialog(false);
+    };
+
+    const handleCopyClick = () => {
+        navigator.clipboard.writeText(`${type}/${id}`);
+        setShowTooltip(true);
+    };
+
+    const handleOnTooltipClose = () => {
+        setShowTooltip(false);
+    };
+
+    return (
+        <>
+            <IconButton component="span" onClick={handleDialogOpen}>
+                <InfoIcon />
+            </IconButton>
+            <Dialog onClose={handleDialogClose} open={openDialog}>
+                <DialogTitle>Record ID:</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        <Typography component="span">{`${type}/${id}`}</Typography>
+                        <Tooltip
+                            open={showTooltip}
+                            title={'Copied to clipboard!'}
+                            leaveDelay={1000}
+                            onClose={handleOnTooltipClose}
+                        >
+                            <IconButton onClick={handleCopyClick}>
+                                <ContentCopyIcon />
+                            </IconButton>
+                        </Tooltip>
+                    </DialogContentText>
+                </DialogContent>
+            </Dialog>
+        </>
+    );
+};
