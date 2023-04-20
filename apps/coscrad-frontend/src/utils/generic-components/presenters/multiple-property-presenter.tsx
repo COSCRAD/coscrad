0c48b11bc022f98da5046ea710c2b78f9b0f03a3
Aaron Plahn <aplahn85@gmail.com>
2023-04-20 10:35:57 -0700

    refactor(coscrad-frontend): tighten up multi-property presenter prop types

--- apps/coscrad-frontend/src/utils/generic-components/presenters/multiple-property-presenter.tsx
+++ apps/coscrad-frontend/src/utils/generic-components/presenters/multiple-property-presenter.tsx
@@ -7,7 +7,7 @@ import {
 import { SinglePropertyPresenter } from './single-property-presenter';
 
 export type PropertyLabels<T> = {
-    [K in keyof T]?: string;
+    [K in keyof T]?: T[K] extends string | number ? string : never;
 };
 
 interface MultiPropertyPresenterProps<T> {
@@ -18,35 +18,33 @@ interface MultiPropertyPresenterProps<T> {
 export const MultiplePropertyPresenter = <T,>({
     keysAndLabels,
     data,
-}: MultiPropertyPresenterProps<T>): JSX.Element => {
-    return (
-        <>
-            {Object.entries(keysAndLabels)
-                .filter(([propertyKey, _]) => !isNullOrUndefined(data[propertyKey]))
-                .filter(([propertyKey, _]) => {
-                    if (!isString(data[propertyKey]) && !isNumber(data[propertyKey])) {
-                        throw new Error(
-                            'Only string or number valued properties are supported by MultiplePropertyPresenter'
-                        );
-                    }
-
-                    if (!isNonEmptyString(data[propertyKey].toString())) return false;
-
-                    return true;
-                })
-                .map(([propertyKey, label]) => {
-                    const propertyValue = data[propertyKey];
-
-                    const propertyValueAsString = propertyValue.toString();
-
-                    return (
-                        <SinglePropertyPresenter
-                            key={label as string}
-                            display={label as string}
-                            value={propertyValueAsString}
-                        />
+}: MultiPropertyPresenterProps<T>): JSX.Element => (
+    <>
+        {Object.entries(keysAndLabels)
+            .filter(([propertyKey, _]) => !isNullOrUndefined(data[propertyKey]))
+            .filter(([propertyKey, _]) => {
+                if (!isString(data[propertyKey]) && !isNumber(data[propertyKey])) {
+                    throw new Error(
+                        'Only string or number valued properties are supported by MultiplePropertyPresenter'
                     );
-                })}
-        </>
-    );
-};
+                }
+
+                if (!isNonEmptyString(data[propertyKey].toString())) return false;
+
+                return true;
+            })
+            .map(([propertyKey, label]) => {
+                const propertyValue = data[propertyKey];
+
+                const propertyValueAsString = propertyValue.toString();
+
+                return (
+                    <SinglePropertyPresenter
+                        key={label as string}
+                        display={label as string}
+                        value={propertyValueAsString}
+                    />
+                );
+            })}
+    </>
+);
