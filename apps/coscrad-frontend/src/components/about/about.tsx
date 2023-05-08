207245ca5865e0105aa4e2f5db1642e70208920a
yaanahuu2 <63014783+yaanahuu2@users.noreply.github.com>
2023-05-08 11:39:07 -0700

    style(coscrad-frontend): remove coscrad main content container (#367)
    * WIP remove coscrad main content container
    
    * WIP fixed tests and lint
    
    ---------
    
    Co-authored-by: yaanahuu2 <gahorner@gmail.com>

--- apps/coscrad-frontend/src/components/about/about.tsx
+++ apps/coscrad-frontend/src/components/about/about.tsx
@@ -1,7 +1,6 @@
 import { Typography } from '@mui/material';
 import { useContext } from 'react';
 import { ConfigurableContentContext } from '../../configurable-front-matter/configurable-content-provider';
-import { CoscradMainContentContainer } from '../../utils/generic-components/style-components/coscrad-main-content-container';
 
 export interface AboutProps {
     about: string;
@@ -14,9 +13,9 @@ export const About = (): JSX.Element => {
     const { about } = useContext(ConfigurableContentContext);
 
     return (
-        <CoscradMainContentContainer>
+        <>
             <Typography variant="h2">About</Typography>
             <Typography variant="body1">{about}</Typography>
-        </CoscradMainContentContainer>
+        </>
     );
 };
--- apps/coscrad-frontend/src/components/resources/audio-item/audio-item-detail.full-view.presenter.tsx
+++ apps/coscrad-frontend/src/components/resources/audio-item/audio-item-detail.full-view.presenter.tsx
@@ -7,7 +7,6 @@ import { MediaPlayer } from '@coscrad/media-player';
 import { Box, Grid, Typography } from '@mui/material';
 import { ResourceDetailFullViewPresenter } from '../../../utils/generic-components/presenters/detail-views';
 import { SinglePropertyPresenter } from '../../../utils/generic-components/presenters/single-property-presenter';
-import { CoscradMainContentContainer } from '../../../utils/generic-components/style-components/coscrad-main-content-container';
 import { convertMillisecondsToSeconds } from '../utils/math';
 
 export const AudioItemDetailFullViewPresenter = ({
@@ -24,27 +23,25 @@ export const AudioItemDetailFullViewPresenter = ({
     ));
 
     return (
-        <CoscradMainContentContainer>
-            <ResourceDetailFullViewPresenter name={name} id={id} type={ResourceType.audioItem}>
-                <div data-testid={id} />
-                <MediaPlayer audioUrl={audioURL} />
-                <SinglePropertyPresenter
-                    display="Duration"
-                    value={`${convertMillisecondsToSeconds(lengthMilliseconds)} secs`}
-                />
-                <SinglePropertyPresenter display="Audio Url" value={audioURL} />
-                <Grid container columns={6} sx={{ mb: 1 }}>
-                    <Grid item xs={1}>
-                        {/* TODO: introduce bold (not heading) custom theme typography variant? */}
-                        <Typography component={'span'} sx={{ fontWeight: 'bold' }}>
-                            Text:
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={5}>
-                        {formatedPlainText}
-                    </Grid>
+        <ResourceDetailFullViewPresenter name={name} id={id} type={ResourceType.audioItem}>
+            <div data-testid={id} />
+            <MediaPlayer audioUrl={audioURL} />
+            <SinglePropertyPresenter
+                display="Duration"
+                value={`${convertMillisecondsToSeconds(lengthMilliseconds)} secs`}
+            />
+            <SinglePropertyPresenter display="Audio Url" value={audioURL} />
+            <Grid container columns={6} sx={{ mb: 1 }}>
+                <Grid item xs={1}>
+                    {/* TODO: introduce bold (not heading) custom theme typography variant? */}
+                    <Typography component={'span'} sx={{ fontWeight: 'bold' }}>
+                        Text:
+                    </Typography>
                 </Grid>
-            </ResourceDetailFullViewPresenter>
-        </CoscradMainContentContainer>
+                <Grid item xs={5}>
+                    {formatedPlainText}
+                </Grid>
+            </Grid>
+        </ResourceDetailFullViewPresenter>
     );
 };
