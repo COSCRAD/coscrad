687b0d6c5dc73d24d2353e7c8a9591dbd6659b92
Aaron Plahn <aplahn85@gmail.com>
2023-05-10 14:42:55 -0700

    test(api): discover command payload schemas dynamically

--- apps/api/src/app/controllers/command/command-payload-schemas.spec.ts
+++ apps/api/src/app/controllers/command/command-payload-schemas.spec.ts
@@ -1,48 +1,40 @@
-import { ICommand } from '@coscrad/commands';
-import { getCoscradDataSchema } from '@coscrad/data-types';
-import { CreateBookBibliographicReference } from '../../../domain/models/bibliographic-reference/book-bibliographic-reference/commands/create-book-bibliographic-reference/create-book-bibliographic-reference.command';
-import { CreateCourtCaseBibliographicReference } from '../../../domain/models/bibliographic-reference/court-case-bibliographic-reference/commands/create-court-case-bibliographic-reference.command';
-import { CreateJournalArticleBibliographicReference } from '../../../domain/models/bibliographic-reference/journal-article-bibliographic-reference/commands/create-journal-article-bibliographic-reference.command';
-import { GrantResourceReadAccessToUser } from '../../../domain/models/shared/common-commands/grant-user-read-access/grant-resource-read-access-to-user.command';
-import { CreateSong } from '../../../domain/models/song/commands/create-song.command';
-import { AddUserToGroup } from '../../../domain/models/user-management/group/commands/add-user-to-group/add-user-to-group.command';
-import { CreateGroup } from '../../../domain/models/user-management/group/commands/create-group/create-group.command';
-import { GrantUserRole } from '../../../domain/models/user-management/user/commands/grant-user-role/grant-user-role.command';
-import { RegisterUser } from '../../../domain/models/user-management/user/commands/register-user/register-user.command';
-import { Ctor } from '../../../lib/types/Ctor';
-
-type CommandTypeAndCtor = [string, Ctor<ICommand>];
-
-const commandTypesAndCtors: CommandTypeAndCtor[] = [
-    // TODO We should be getting these from `createTestModule`
-    ['CREATE_SONG', CreateSong],
-    ['REGISTER_USER', RegisterUser],
-    ['CREATE_USER_GROUP', CreateGroup],
-    ['ADD_USER_TO_GROUP', AddUserToGroup],
-    ['GRANT_RESOURCE_READ_ACCESS_TO_USER', GrantResourceReadAccessToUser],
-    ['GRANT_USER_ROLE', GrantUserRole],
-    ['CREATE_BOOK_BIBLIOGRAPHIC_REFERENCE', CreateBookBibliographicReference],
-    ['CREATE_JOURNAL_ARTICLE_BIBLIOGRAPHIC_REFERENCE', CreateJournalArticleBibliographicReference],
-    ['CREATE_COURT_CASE_BIBLIOGRAPHIC_REFERENCE', CreateCourtCaseBibliographicReference],
-];
-
-/**
- * TODO [https://www.pivotaltracker.com/story/show/182576828]
- *
- * This is a placeholder. Currently we need to manually register each new
- * command here, but eventually we will have a service that will do this. We should
- * call that service as part of the setup for this test when that time comes.
- * That will make this test completely dynamic.
- */
-const getAllCommandSchemas = () =>
-    commandTypesAndCtors.map(([commandType, Ctor]) => [commandType, getCoscradDataSchema(Ctor)]);
+import { CommandModule } from '@coscrad/commands';
+import generateDatabaseNameForTestSuite from '../../../persistence/repositories/__tests__/generateDatabaseNameForTestSuite';
+import createTestModule from '../__tests__/createTestModule';
+import { CommandInfoService } from './services/command-info-service';
 
 describe('command payload schemas', () => {
-    getAllCommandSchemas().forEach(([commandType, schema]) => {
-        describe(`The schema for command ${commandType}`, () => {
-            it('should have the expected value', () => {
-                expect(schema).toMatchSnapshot();
-            });
+    let commandInfoService: CommandInfoService;
+
+    beforeAll(async () => {
+        const testModule = await createTestModule({
+            ARANGO_DB_NAME: generateDatabaseNameForTestSuite(),
         });
+
+        await testModule.get<CommandModule>(CommandModule).onApplicationBootstrap();
+
+        commandInfoService = testModule.get<CommandInfoService>(CommandInfoService);
+
+        console.log({ commandInfoService });
+    });
+
+    describe(`Command payload schema`, () => {
+        it(`should match the snapshot`, () => {
+            const schema = commandInfoService.getCommandSchemasWithMetadata();
+
+            expect(schema).toMatchSnapshot();
+        });
+
+        // commandInfoService
+        //     .getCommandSchemasWithMetadata()
+        //     .map(({ type, schema }) => [type, schema])
+        //     .forEach(([commandType, schema]) => {
+
+        //         describe(`The schema for command ${commandType}`, () => {
+        //             it('should have the expected value', () => {
+        //                 expect(schema).toMatchSnapshot();
+        //             });
+        //         });
+        //     });
     });
 });
