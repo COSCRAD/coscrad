3943b9ebb6a9420fb3b3d8893db987cd6b550086
Justin Bambrick <jbambrick@tsilhqotin.ca>
2023-05-15 11:18:37 -0700

    WIP: present multilingual text in term views

--- apps/coscrad-frontend/src/utils/generic-components/presenters/multilingual-text-presenter.tsx
+++ apps/coscrad-frontend/src/utils/generic-components/presenters/multilingual-text-presenter.tsx
@@ -1,4 +1,12 @@
 import { IMultilingualText } from '@coscrad/api-interfaces';
+import { Box, Typography } from '@mui/material';
+
+// TODO use contentConfigContext
+// default language code
+// find the text iterm with the default language code
+// find the text item with english
+// note: either of these may not exist (fallback logic)
+// build an appropriate presentation of these properties
 
 export interface MultilingualTextPresenterProps {
     text: IMultilingualText;
@@ -17,9 +25,13 @@ export const MultilingualTextPresenter = ({
         <>
             {/* TODO We need to separate the original in some way */}
             {items.map(({ languageCode, text, role }) => (
-                <div key={`${languageCode}-${role}`}>
-                    {`{${languageCode}} [${role}]`} {text}
-                </div>
+                <Box key={`${languageCode}-${role}`}>
+                    {text}
+                    <Typography
+                        variant="subtitle1"
+                        color={'secondary.dark'}
+                    >{`${languageCode}, '${role}'`}</Typography>
+                </Box>
             ))}
         </>
     );
