9964d95222d0d3e4e5962f20beab700730d69834
bsellars <101149169+bsellars@users.noreply.github.com>
2023-05-17 11:30:45 -0700

    feat(api): introduce CREATE_PLAYLIST  (#378)
    * test(api): write happy path test
    
    * test(api): created test for when ID is not generated by our system
    
    * add failing test for missing name case
    
    * WIP: flow multilingual text invariant validation through to playlist
    
    * fix stale snapshot
    
    * Remove multilingual text property from payload in favor of text and language code for name
    
    ---------
    
    Co-authored-by: Aaron Plahn <aplahn85@gmail.com>

---
+++ apps/api/src/domain/models/playlist/commands/create-playlist.command.ts
@@ -0,0 +1,64 @@
+import { AggregateType, ICommandBase, LanguageCode } from '@coscrad/api-interfaces';
+import { Command } from '@coscrad/commands';
+import { ExternalEnum, NestedDataType, NonEmptyString, UUID } from '@coscrad/data-types';
+import { AggregateCompositeIdentifier } from '../../../types/AggregateCompositeIdentifier';
+
+export class PlayListCompositeId {
+    /**
+     * This is a bit of a hack. It circumvents our `CoscradDataTypes` and may
+     * cause problems for
+     * - Schema management
+     * - Anyone using our API directly (not via front-end)
+     *
+     * The simple answer is that you always have to tack on an
+     * `aggregateCompositeIdentifier`.
+     */
+    @NonEmptyString({
+        label: 'type',
+        description: 'song',
+    })
+    type = AggregateType.playlist;
+
+    @UUID({
+        label: 'ID',
+        description: 'unique identifier',
+    })
+    id: string;
+}
+
+@Command({
+    type: 'CREATE_PLAYLIST',
+    label: 'Create PLaylist',
+    description: 'Creates a new playlist',
+})
+export class CreatePlayList implements ICommandBase {
+    @NestedDataType(PlayListCompositeId, {
+        label: 'Composite Identifier',
+        description: 'system-wide unique identifier',
+    })
+    readonly aggregateCompositeIdentifier: AggregateCompositeIdentifier<
+        typeof AggregateType.playlist
+    >;
+
+    @NonEmptyString({
+        label: 'name',
+        description: 'name of the playlist',
+    })
+    name: string;
+
+    @ExternalEnum(
+        {
+            enumLabel: `language code`,
+            enumName: `LanguageCode`,
+            labelsAndValues: Object.entries(LanguageCode).map(([label, languageCode]) => ({
+                label,
+                value: languageCode,
+            })),
+        },
+        {
+            label: 'language code',
+            description: 'the language being used to name the playlist',
+        }
+    )
+    languageCodeForName: LanguageCode;
+}
