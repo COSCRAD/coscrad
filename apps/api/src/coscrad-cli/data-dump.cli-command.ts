932762b73375a6164bdffe030f0e0c8afb18a2ad
Aaron Plahn <aplahn85@gmail.com>
2023-05-18 12:04:47 -0700

    infra(api): ensure that **data-dump** dumps full db snapshot (#383)

--- apps/api/src/coscrad-cli/data-dump.cli-command.ts
+++ apps/api/src/coscrad-cli/data-dump.cli-command.ts
@@ -1,9 +1,5 @@
-import { Inject } from '@nestjs/common';
 import { writeFileSync } from 'fs';
-import { IRepositoryProvider } from '../domain/repositories/interfaces/repository-provider.interface';
-import { REPOSITORY_PROVIDER_TOKEN } from '../persistence/constants/persistenceConstants';
-import { DomainDataExporter } from '../persistence/repositories/domain-data-exporter';
-import convertInMemorySnapshotToDatabaseFormat from '../test-data/utilities/convertInMemorySnapshotToDatabaseFormat';
+import { ArangoDataExporter } from '../persistence/repositories/arango-data-exporter';
 import { CliCommand, CliCommandOption, CliCommandRunner } from './cli-command.decorator';
 
 @CliCommand({
@@ -11,22 +7,14 @@ import { CliCommand, CliCommandOption, CliCommandRunner } from './cli-command.de
     description: 'dumps the database state to a snapshot file',
 })
 export class DomainDumpCliCommand extends CliCommandRunner {
-    private readonly dataExporter: DomainDataExporter;
-
-    constructor(@Inject(REPOSITORY_PROVIDER_TOKEN) repositoryProvider: IRepositoryProvider) {
+    constructor(private readonly arangoDataExporter: ArangoDataExporter) {
         super();
-
-        this.dataExporter = new DomainDataExporter(repositoryProvider);
     }
 
     async run(_passedParams: string[], options?: { filepath: string }): Promise<void> {
         const NUMBER_OF_SPACES_TO_INDENT = 4;
 
-        const domainSnapshot = await this.dataExporter.fetchSnapshot();
-
-        const snapshotInDatabaseFormat = convertInMemorySnapshotToDatabaseFormat(
-            domainSnapshot.fetchFullSnapshotInLegacyFormat()
-        );
+        const snapshotInDatabaseFormat = await this.arangoDataExporter.fetchSnapshot();
 
         writeFileSync(
             options.filepath,
