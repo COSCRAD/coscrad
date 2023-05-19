da2aabd19d1ed2649bd90da3b2115068dc25d063
Aaron Plahn <aplahn85@gmail.com>
2023-05-19 13:50:30 -0700

    infra(api): validate invariants post-migration (#386)
    * infra(api): introduce **validate-invariants**
    
    * fix unawaited promises
    
    * Add post-migration invariant validation check
    
    * Improve test coverage and respond to PR (#386)

--- apps/api/src/coscrad-cli/coscrad-cli.module.ts
+++ apps/api/src/coscrad-cli/coscrad-cli.module.ts
@@ -8,6 +8,7 @@ import { ListMigrationsCliCommand } from './list-migrations.cli-command';
 import { ConsoleCoscradCliLogger, COSCRAD_LOGGER_TOKEN } from './logging';
 import { RevertLatestMigrationCliCommand } from './revert-latest-migration';
 import { RunMigrationsCliCommand } from './run-migrations.cli-command';
+import { ValidateInvariantsCliCommand } from './validate-invariants.cli-command';
 
 @Module({
     providers: [
@@ -16,6 +17,7 @@ import { RunMigrationsCliCommand } from './run-migrations.cli-command';
         ListMigrationsCliCommand,
         RunMigrationsCliCommand,
         RevertLatestMigrationCliCommand,
+        ValidateInvariantsCliCommand,
         {
             provide: COSCRAD_LOGGER_TOKEN,
             useClass: ConsoleCoscradCliLogger,
