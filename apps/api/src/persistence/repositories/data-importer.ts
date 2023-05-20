a477ad358d3b1510bb710f584a4addab513e73b1
Aaron Plahn <aplahn85@gmail.com>
2023-05-20 12:04:13 -0700

    Fix issues with migration cli commands

--- apps/api/src/persistence/repositories/data-importer.ts
+++ apps/api/src/persistence/repositories/data-importer.ts
@@ -1,7 +1,5 @@
 import { readFileSync } from 'fs';
-import { Environment } from '../../app/config/constants/Environment';
 import { HasAggregateId } from '../../domain/types/HasAggregateId';
-import { InternalError } from '../../lib/errors/InternalError';
 import { ArangoCollectionId } from '../database/collection-references/ArangoCollectionId';
 import { ArangoDatabaseProvider } from '../database/database.provider';
 import { DatabaseDTO } from '../database/utilities/mapEntityDTOToDatabaseDTO';
@@ -10,11 +8,6 @@ export class DataImporter {
     constructor(private readonly databaseProvider: ArangoDatabaseProvider) {}
 
     async restore({ filepath }: { filepath: string }): Promise<void> {
-        if (process.env.NODE_ENV === Environment.production) {
-            throw new InternalError(`You cannot overwrite the production database with this tool.`);
-        }
-
-        // TODO move this to a service class so we can reuse it outside of the CLI if needed
         const parsedSnapshot = JSON.parse(readFileSync(filepath, { encoding: 'utf-8' }));
 
         const writeDocumentData = ([collectionName, documents]: [
