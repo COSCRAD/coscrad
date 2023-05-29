1eb39aeaee4d92f37cd2567ad0f231a2903e472c
Aaron Plahn <aplahn85@gmail.com>
2023-05-29 10:01:02 -0700

    build(cicd) introduce separate front-end deployment for Haida project (#389)
    * WIP add experimental function to Jenkinsfile
    
    * WIP refactor the branch condition
    
    * WIP break out function to copy env dependent content config
    
    * update sample content config with more realistic text
    
    * comment out ci stage for troubleshooting purposes
    
    * force deployment stage to run for this PR
    
    * WIP Introduce separate Haida staging front-end build
    
    * WIP refactor Jenkinsfile
    
    * WIP configure auth for the staging environment
    
    * remove project-specific labels for staging data from front-end
    
    * force staging build to use production config
    
    * WIP troubleshoot build
    
    * Use actual test data term for word-of-the-day in sample content config
    
    * troubleshoot build
    
    * troubleshoot build
    
    * nuke node modules
    
    * troubleshoot nx issues
    
    * revert Jenkinsfile
    
    * fix front-end tests
    
    * break out build steps in Jenkinsfile
    
    * break out function to copy the staging content config
    
    * refactor Jenkinsfile to use multiple stages for deployment
    
    * break out dependency install into separate stage
    
    * break the front-end build out into a separate stage
    
    * break out back-end and cli builds into separate stages
    
    * parametrize the front-end build
    
    * break out entire front-end build block
    
    * opt back in to the front-end deployment
    
    * tweak front-end artifacts pattern
    
    * troubleshoot front-end deployment
    
    * troubleshoot deployment...
    
    * update front-end artifacts path in publisher
    
    * tweak deployment
    
    * reinstate full front-end deployment
    
    * move front-end deployment logic into reuseable function
    
    * revert to working state
    
    * tweak deployment
    
    * archive the front-end only
    
    * break out front-end deployment into reuseable function
    
    * Add Haida front-end deployment
    
    * reformat publish step for readability
    
    * wrap publish over ssh in simpler API
    
    * revert previous
    
    * wrap artifact publication block in simpler API
    
    * pass through additional params
    
    * flow through remaining parameters
    
    * refactor front-end deployment block
    
    * run backend deployment
    
    * use multi-line strings and function calls for readability
    
    * deploy cli
    
    * fix Groovy lint
    
    * revert prevoius change
    
    * fix placement of cli deploy post block
    
    * copy cli to different directory
    
    * deploy node_modules with back-end build
    
    * skip nx cache for front-end build
    
    * restore proper stage conditionals
    
    * restore another conditional stage

--- apps/coscrad-frontend/src/components/coscrad-logo/coscrad-logo.spec.tsx
+++ apps/coscrad-frontend/src/components/coscrad-logo/coscrad-logo.spec.tsx
@@ -1,16 +1,28 @@
 import { renderWithProviders } from '../../utils/test-utils';
+import { getDummyConfigurableContent } from '../../utils/test-utils/get-dummy-configurable-content';
 import { COSCRADLogo } from './coscrad-logo';
+
+const coscradLogoUrl = 'coscrad-logo.png';
+
+const act = () =>
+    renderWithProviders(<COSCRADLogo />, {
+        contentConfig: getDummyConfigurableContent({
+            coscradLogoUrl: coscradLogoUrl,
+        }),
+    });
 
 describe('COSCRADLogo', () => {
     it('should render the logo image successfully', () => {
-        const { baseElement } = renderWithProviders(<COSCRADLogo />);
+        const { baseElement } = act();
 
         expect(baseElement).toBeTruthy();
     });
+
     it('should display the logo image', () => {
-        const { baseElement } = renderWithProviders(<COSCRADLogo />);
+        const { baseElement } = act();
+
         const logo = baseElement.querySelector('img');
 
-        expect(logo.src).toContain('coscrad-logo.png');
+        expect(logo.src).toContain(coscradLogoUrl);
     });
 });
