ecc0508ead44b590b796b2a4d1c5284826f94324
Justin Bambrick <jbambrick@tsilhqotin.ca>
2023-05-30 09:56:56 -0700

    style(coscrad-frontend): style native audio player with mui styled

--- libs/media-player/src/lib/MediaPlayer.tsx
+++ libs/media-player/src/lib/MediaPlayer.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { styled, useTheme } from '@mui/material';
 import { useEffect, useRef } from 'react';
 
 export interface MediaPlayerProps {
@@ -15,9 +15,57 @@ export function MediaPlayer({ audioUrl }: MediaPlayerProps) {
         }
     }, [audioUrl]);
 
+    const {
+        palette: {
+            secondary: { main },
+        },
+    } = useTheme();
+
+    const StyledMediaPlayer = styled('audio')`
+        & {
+            width: 100px !important;
+            overflow: clip;
+        }
+
+        &::-webkit-media-controls-panel {
+            background: #ffffff;
+            overflow: clip;
+        }
+
+        &::-webkit-media-controls-mute-button,
+        &::-webkit-media-controls-volume-slider,
+        &::-webkit-media-controls-volume-slider-thumb,
+        &::-webkit-media-controls-timeline,
+        &::-webkit-media-controls-current-time-display,
+        &::-webkit-media-controls-time-remaining-display,
+        &::-webkit-media-controls-duration-display,
+        &::-webkit-media-controls-fullscreen-button {
+            display: none;
+        }
+
+        &::-webkit-media-controls-play-button {
+            background-color: ${main};
+            border-radius: 20px;
+            margin: 0 100px 0 0px;
+            overflow: clip !important;
+        }
+
+        /* FIREFOX */
+        @supports (-moz-animation: none) {
+            & {
+                background: ${main} !important;
+                border-radius: 20px;
+                width: 60px !important;
+            }
+        }
+    `;
+
     return (
-        <Box>
-            <audio ref={audioRef} controls data-testid="audio-player"></audio>
-        </Box>
+        <StyledMediaPlayer
+            ref={audioRef}
+            controls
+            controlsList="nodownload noplaybackrate"
+            data-testid="audio-player"
+        />
     );
 }
