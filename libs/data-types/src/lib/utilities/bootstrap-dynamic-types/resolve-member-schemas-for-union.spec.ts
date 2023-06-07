dfe0b69851eca76a345737b41fdf353b6706f30c
Aaron Plahn <aplahn85@gmail.com>
2023-06-06 18:16:25 -0700

    WIP Resolve union member schemas recursively

--- libs/data-types/src/lib/utilities/bootstrap-dynamic-types/resolve-member-schemas-for-union.spec.ts
+++ libs/data-types/src/lib/utilities/bootstrap-dynamic-types/resolve-member-schemas-for-union.spec.ts
@@ -1,57 +1,44 @@
 import { ThingDataOne, ThingDataTwo, Widget } from '../../../test/widget';
-import getCoscradDataSchema from '../getCoscradDataSchema';
 import {
     resolveMemberSchemasForUnion,
     UnionMemberSchemaDefinition,
 } from './resolve-member-schemas-for-union';
 
-type DiscriminantValueAndCtorName = {
-    discriminantValue: string;
+const assertMembersResolved = (
+    schemaDefinitions: UnionMemberSchemaDefinition[],
+    expectedNumberOfMembers: number,
+    expectedDiscriminantValues: string[]
+): void => {
+    expect(schemaDefinitions.length).toBe(expectedNumberOfMembers);
 
-    ctorName: string;
+    const missingMembers = expectedDiscriminantValues.filter(
+        (discriminantValue) =>
+            !schemaDefinitions.some(
+                ({ discriminantValue: discriminantValueForThisSchema }) =>
+                    discriminantValue === discriminantValueForThisSchema
+            )
+    );
+
+    expect(missingMembers).toEqual([]);
 };
 
 describe(`resolveMemberSchemasForUnion`, () => {
-    const assertMembersResolved = (
-        schemaDefinitions: UnionMemberSchemaDefinition[],
-        expectedNumberOfMembers: number,
-        expectedDiscriminantsAndCtorNames: DiscriminantValueAndCtorName[]
-    ): void => {
-        expect(schemaDefinitions.length).toBe(expectedNumberOfMembers);
-
-        const missingMembers = expectedDiscriminantsAndCtorNames.filter(
-            ({ discriminantValue }) =>
-                !schemaDefinitions.some(
-                    ({ discriminantValue: discriminantValueForThisSchema }) =>
-                        discriminantValue === discriminantValueForThisSchema
-                )
-        );
-
-        expect(missingMembers).toEqual([]);
-    };
-
     describe(`when there are multiple union members with no nesting`, () => {
         it(`should resolve the members`, () => {
-            const widgetSchema = getCoscradDataSchema(Widget);
-
-            const unionPropertySchema = widgetSchema['data'];
-
             const result = resolveMemberSchemasForUnion(
                 [Widget, ThingDataOne, ThingDataTwo],
-                // @ts-expect-error fix types \ add a proper assertion
-                unionPropertySchema
+                'THING_UNION'
             );
 
-            assertMembersResolved(result, 2, [
-                {
-                    discriminantValue: 'widget',
-                    ctorName: 'Widget',
-                },
-                {
-                    discriminantValue: 'whatsit',
-                    ctorName: 'Whatsit',
-                },
-            ]);
+            assertMembersResolved(result, 2, ['one', 'two']);
         });
+    });
+
+    describe(`when there is a nested type that leverages another union`, () => {
+        it.todo('should have a test');
+    });
+
+    describe(`when there is a nested type that does not leverage another union`, () => {
+        it.todo('should have a test');
     });
 });
--- libs/data-types/src/lib/utilities/bootstrap-dynamic-types/resolve-member-schemas-for-union.ts
+++ libs/data-types/src/lib/utilities/bootstrap-dynamic-types/resolve-member-schemas-for-union.ts
@@ -1,13 +1,66 @@
 import { UnionMemberMetadata } from '../../decorators';
-import { ClassSchema } from '../../types';
+import { ClassSchema, isSimpleCoscradPropertyTypeDefinition } from '../../types';
+import getCoscradDataSchema from '../getCoscradDataSchema';
+import { buildUnionTypesMap } from './bootstrap-dynamic-types';
 
 export type UnionMemberSchemaDefinition = Pick<UnionMemberMetadata, 'discriminantValue'> & {
     schema: ClassSchema;
 };
 
 export const resolveMemberSchemasForUnion = (
-    _allCtorCandidates: unknown,
-    _unionName: string
+    allCtorCandidates: unknown[],
+    unionName: string
 ): UnionMemberSchemaDefinition[] => {
-    throw new Error(`not implemented`);
+    const unionMap = buildUnionTypesMap(allCtorCandidates);
+
+    const membersMap = unionMap.get(unionName);
+
+    if (!membersMap) {
+        return [];
+    }
+
+    return Array.from(membersMap.keys()).map((discriminantValue) => {
+        const ctor = membersMap.get(discriminantValue);
+
+        const rawSchema = getCoscradDataSchema(ctor);
+
+        const updatedSchema = Object.entries(rawSchema).reduce(
+            (acc, [propertyKey, propertyTypeDefinition]) => {
+                if (
+                    isSimpleCoscradPropertyTypeDefinition(propertyTypeDefinition) ||
+                    !['NESTED_TYPE', 'UNION_TYPE'].includes(propertyTypeDefinition.complexDataType)
+                )
+                    return {
+                        ...acc,
+                        [propertyKey]: propertyTypeDefinition,
+                    };
+
+                const { complexDataType } = propertyTypeDefinition;
+
+                if (complexDataType === 'NESTED_TYPE') {
+                    throw new Error(
+                        `not implemented: failed to resolve union that leverages a nested data type`
+                    );
+                }
+
+                if (complexDataType === 'UNION_TYPE')
+                    return {
+                        ...acc,
+                        [propertyKey]: {
+                            ...propertyTypeDefinition,
+                            schemaDefinitions: resolveMemberSchemasForUnion(
+                                allCtorCandidates,
+                                propertyTypeDefinition.unionName
+                            ),
+                        },
+                    };
+            },
+            {}
+        );
+
+        return {
+            discriminantValue,
+            schema: updatedSchema,
+        };
+    });
 };
