14c68a21e7e4b50c00505e14f30e57c85280d9ec
Aaron Plahn <aplahn85@gmail.com>
2023-06-15 09:07:33 -0700

    test(api): remove stale test case that used text field context for a media item

--- apps/api/src/domain/domainModelValidators/errors/context/invalidContextStateErrors/DisallowedContextTypeForResourceError.ts
+++ apps/api/src/domain/domainModelValidators/errors/context/invalidContextStateErrors/DisallowedContextTypeForResourceError.ts
@@ -1,10 +1,13 @@
 import { InternalError } from '../../../../../lib/errors/InternalError';
+import formatAggregateCompositeIdentifier from '../../../../../view-models/presentation/formatAggregateCompositeIdentifier';
 import { AggregateCompositeIdentifier } from '../../../../types/AggregateCompositeIdentifier';
 
 export default class DisallowedContextTypeForResourceError extends InternalError {
     constructor(contextType: unknown, resourceCompositeIdentifier: AggregateCompositeIdentifier) {
         super(
-            `Disallowed context type: ${contextType} for resource type: ${resourceCompositeIdentifier}`
+            `Disallowed context type: ${contextType} for resource ${formatAggregateCompositeIdentifier(
+                resourceCompositeIdentifier
+            )}`
         );
     }
 }
--- apps/api/src/domain/models/__tests__/buildResourceModelContextStateValidatorTestCases/buildMediaItemResourceModelContextStateValidatorTestCase.ts
+++ apps/api/src/domain/models/__tests__/buildResourceModelContextStateValidatorTestCases/buildMediaItemResourceModelContextStateValidatorTestCase.ts
@@ -1,8 +1,5 @@
-import EmptyTargetForTextFieldContextError from '../../../domainModelValidators/errors/context/invalidContextStateErrors/textFieldContext/EmptyTargetForTextFieldContextError';
-import InconsistentCharRangeError from '../../../domainModelValidators/errors/context/invalidContextStateErrors/textFieldContext/InconsistentCharRangeError';
 import InconsistentTimeRangeError from '../../../domainModelValidators/errors/context/invalidContextStateErrors/timeRangeContext/InconsistentTimeRangeError';
 import { ResourceType } from '../../../types/ResourceType';
-import { TextFieldContext } from '../../context/text-field-context/text-field-context.entity';
 import { TimeRangeContext } from '../../context/time-range-context/time-range-context.entity';
 import { EdgeConnectionContextType } from '../../context/types/EdgeConnectionContextType';
 import {
@@ -18,10 +15,6 @@ const inconsistentContextTypeTestCases = buildAllInconsistentContextTypeTestCase
 );
 const validMediaItem = validCases[0].resource;
 
-const mediaItemWithNoTitle = validMediaItem.clone({
-    title: undefined,
-});
-
 const invalidTimeRangeContext = new TimeRangeContext({
     type: EdgeConnectionContextType.timeRange,
     timeRange: {
@@ -32,30 +25,6 @@ const invalidTimeRangeContext = new TimeRangeContext({
 
 const invalidCases: ResourceModelContextStateValidatorInvalidTestCase[] = [
     ...(inconsistentContextTypeTestCases as unknown as ResourceModelContextStateValidatorInvalidTestCase[]),
-    {
-        description: `the target property is undefined`,
-        resource: mediaItemWithNoTitle,
-        context: new TextFieldContext({
-            type: EdgeConnectionContextType.textField,
-            target: 'title',
-            charRange: [0, 2],
-        }),
-        expectedError: new EmptyTargetForTextFieldContextError(mediaItemWithNoTitle, 'title'),
-    },
-    {
-        description: `the text field context targets a last char index that is too big`,
-        resource: validMediaItem,
-        context: new TextFieldContext({
-            type: EdgeConnectionContextType.textField,
-            target: 'title',
-            charRange: [0, validMediaItem.title.length + 5],
-        }),
-        expectedError: new InconsistentCharRangeError(
-            [0, validMediaItem.title.length + 5],
-            validMediaItem,
-            'title'
-        ),
-    },
     {
         description: 'the time range context targets an end point that is too big',
         resource: validMediaItem,
