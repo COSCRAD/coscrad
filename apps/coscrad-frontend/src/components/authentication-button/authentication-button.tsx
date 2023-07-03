5d16d8e90bff5f0ad5eaa729cdffe6556cdb48d3
Justin Bambrick <55550194+jbambrick@users.noreply.github.com>
2023-07-03 09:36:14 -0700

    feat(coscrad-frontend): support configurable site title and favicon (#417)
    * feat(coscrad-frontend): support configurable site title and favicon image
    
    * WIP: changes from PR

--- apps/coscrad-frontend/src/components/authentication-button/authentication-button.tsx
+++ apps/coscrad-frontend/src/components/authentication-button/authentication-button.tsx
@@ -1,10 +1,15 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import { useContext } from 'react';
+import { ConfigurableContentContext } from '../../configurable-front-matter/configurable-content-provider';
 import LoginButton from '../login-button/login-button';
 import LogoutButton from '../logout-button/logout-button';
 
 const AuthenticationButton = () => {
     const { isAuthenticated } = useAuth0();
 
+    const { shouldEnableAdminMode } = useContext(ConfigurableContentContext);
+
+    if (!shouldEnableAdminMode) return null;
     return isAuthenticated ? <LogoutButton /> : <LoginButton />;
 };
 
