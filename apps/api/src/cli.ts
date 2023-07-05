706745dc70372f316a61c1b8286fd60837a545ac
Aaron Plahn <aplahn85@gmail.com>
2023-07-05 08:41:19 -0700

    infra(coscrad-frontend-e2e): support seeding data between Cypress tests (#422)
    * support seeding the admin user between Cypress tests
    
    * refactor based on PR (#421)

--- apps/api/src/cli.ts
+++ apps/api/src/cli.ts
@@ -1,8 +1,13 @@
 import { CommandFactory } from 'nest-commander';
+import { Environment } from './app/config/constants/Environment';
 import { CoscradCliModule } from './coscrad-cli/coscrad-cli.module';
 import { InternalError } from './lib/errors/InternalError';
 
 async function bootstrapCli() {
+    if (!Object.values(Environment).includes(process.env.NODE_ENV as Environment)) {
+        throw new Error(`Cannot run COSCRAD CLI in invalid environment: ${process.env.NODE_ENV}`);
+    }
+
     await CommandFactory.run(CoscradCliModule);
 }
 
