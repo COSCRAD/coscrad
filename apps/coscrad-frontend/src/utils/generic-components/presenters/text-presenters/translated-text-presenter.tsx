07ba9f1dabb8ca4267649e60ecb6f537ce4a67c2
yaanahuu2 <gahorner@gmail.com>
2023-07-12 12:17:02 -0700

    WIP add styling to multilingual textarea

--- apps/coscrad-frontend/src/utils/generic-components/presenters/text-presenters/translated-text-presenter.tsx
+++ apps/coscrad-frontend/src/utils/generic-components/presenters/text-presenters/translated-text-presenter.tsx
@@ -1,6 +1,6 @@
 import { LanguageCode, MultilingualTextItemRole } from '@coscrad/api-interfaces';
 import LanguageIcon from '@mui/icons-material/Language';
-import { AccordionDetails, IconButton, Tooltip, Typography } from '@mui/material';
+import { AccordionDetails, IconButton, Tooltip, styled } from '@mui/material';
 import { getLabelForLanguage } from './get-label-for-language';
 
 interface TranslatedLanguageTextPresenterProps {
@@ -10,6 +10,16 @@ interface TranslatedLanguageTextPresenterProps {
     onTextSelection?: (charRange: [number, number], languageCode: LanguageCode) => void;
 }
 
+const MultilingualTextField = styled('textarea')({
+    minHeight: '10px',
+    border: '0px',
+    backgroundColor: 'inherit',
+    resize: 'none',
+    fontFamily: 'inherit',
+    outline: 'none',
+    overflow: 'auto',
+});
+
 export const TranslatedLanguageTextPresenter = ({
     languageCode,
     text,
@@ -17,29 +27,31 @@ export const TranslatedLanguageTextPresenter = ({
     onTextSelection,
 }: TranslatedLanguageTextPresenterProps): JSX.Element => {
     return (
-        <AccordionDetails key={`${languageCode}-${role}`}>
-            <Typography color={'text.primary'}>
-                <textarea
-                    onSelect={(e) => {
-                        console.log({ eventdata: e });
+        <AccordionDetails
+            key={`${languageCode}-${role}`}
+            sx={{ display: 'flex', alignItems: 'center' }}
+        >
+            <MultilingualTextField
+                onSelect={(e) => {
+                    console.log({ eventdata: e });
 
-                        const charRange: [number, number] = [
-                            e.currentTarget.selectionStart,
-                            e.currentTarget.selectionEnd,
-                        ];
+                    const charRange: [number, number] = [
+                        e.currentTarget.selectionStart,
+                        e.currentTarget.selectionEnd,
+                    ];
 
-                        if (typeof onTextSelection === 'function')
-                            onTextSelection(charRange, languageCode);
-                    }}
-                >
-                    {text}
-                </textarea>
-                <Tooltip title={`${getLabelForLanguage(languageCode)}, '${role}'`}>
-                    <IconButton>
-                        <LanguageIcon />
-                    </IconButton>
-                </Tooltip>
-            </Typography>
+                    if (typeof onTextSelection === 'function')
+                        onTextSelection(charRange, languageCode);
+                }}
+                readOnly
+            >
+                {text}
+            </MultilingualTextField>
+            <Tooltip title={`${getLabelForLanguage(languageCode)}, '${role}'`}>
+                <IconButton>
+                    <LanguageIcon />
+                </IconButton>
+            </Tooltip>
         </AccordionDetails>
     );
 };
