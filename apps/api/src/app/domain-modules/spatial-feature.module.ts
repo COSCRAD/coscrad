3dbaba1b51743a5c9f2aa2a641ef38da7541fe01
Aaron Plahn <aplahn85@gmail.com>
2023-07-21 13:49:15 -0700

    register handler for CREATE_POINT

--- apps/api/src/app/domain-modules/spatial-feature.module.ts
+++ apps/api/src/app/domain-modules/spatial-feature.module.ts
@@ -1,6 +1,9 @@
 import { CommandModule } from '@coscrad/commands';
 import { Module } from '@nestjs/common';
-import { CreatePoint } from '../../domain/models/spatial-feature/point/commands';
+import {
+    CreatePoint,
+    CreatePointCommandHandler,
+} from '../../domain/models/spatial-feature/point/commands';
 import { SpatialFeatureQueryService } from '../../domain/services/query-services/spatial-feature-query.service';
 import { PersistenceModule } from '../../persistence/persistence.module';
 import { CommandInfoService } from '../controllers/command/services/command-info-service';
@@ -12,6 +15,7 @@ import { SpatialFeatureController } from '../controllers/resources/spatial-featu
     providers: [
         CommandInfoService,
         SpatialFeatureQueryService,
+        CreatePointCommandHandler,
         // Data Classes
         ...[CreatePoint].map((ctor) => ({
             provide: ctor,
