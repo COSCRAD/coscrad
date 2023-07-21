bb47d4b0919b8c7f361d3db05863eec4ccb86a2c
bsellars <101149169+bsellars@users.noreply.github.com>
2023-07-21 15:59:54 -0700

    feat(coscrad-frontend): support playing an icecast stream (#438)
    * WIP: create icecast player with html audio player
    
    * support playing icecast streams

--- libs/media-player/src/index.ts
+++ libs/media-player/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/IcecastPlayer';
+export * from './lib/IcecastStreamPlayer';
 export * from './lib/MediaPlayer';
 export * from './lib/audio-clip-player';
 export * from './lib/default-clip-player-button';
