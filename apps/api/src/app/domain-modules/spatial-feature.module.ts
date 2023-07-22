10063674b1c5e8ef664fd057d039b7eb98784b78
yaanahuu2 <gahorner@gmail.com>
2023-07-22 09:02:54 -0700

    WIP address cli error

--- apps/api/src/app/domain-modules/spatial-feature.module.ts
+++ apps/api/src/app/domain-modules/spatial-feature.module.ts
@@ -5,12 +5,14 @@ import {
     CreatePointCommandHandler,
 } from '../../domain/models/spatial-feature/point/commands';
 import { SpatialFeatureQueryService } from '../../domain/services/query-services/spatial-feature-query.service';
+import { IdGenerationModule } from '../../lib/id-generation/id-generation.module';
 import { PersistenceModule } from '../../persistence/persistence.module';
+import { DynamicDataTypeModule } from '../../validation';
 import { CommandInfoService } from '../controllers/command/services/command-info-service';
 import { SpatialFeatureController } from '../controllers/resources/spatial-feature.controller';
 
 @Module({
-    imports: [PersistenceModule, CommandModule],
+    imports: [PersistenceModule, CommandModule, IdGenerationModule, DynamicDataTypeModule],
     controllers: [SpatialFeatureController],
     providers: [
         CommandInfoService,
