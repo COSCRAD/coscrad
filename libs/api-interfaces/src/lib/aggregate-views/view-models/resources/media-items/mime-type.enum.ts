494b96873ffff93630c1f7f0ba281ec0bfe696fa
Justin Bambrick <55550194+jbambrick@users.noreply.github.com>
2023-07-27 16:43:25 -0700

    feat(media-player): add video player (#452)
    * feat(coscrad-frontend): create video player and component test
    
    * WIP: make changes from PR
    
    * test(api): update stale snapshots
    
    * Update enum decorator on CREATE_MEDIA_ITEM payload
    
    ---------
    
    Co-authored-by: Aaron Plahn <aplahn85@gmail.com>

--- libs/api-interfaces/src/lib/aggregate-views/view-models/resources/media-items/mime-type.enum.ts
+++ libs/api-interfaces/src/lib/aggregate-views/view-models/resources/media-items/mime-type.enum.ts
@@ -1,6 +1,11 @@
 export enum MIMEType {
+    // images
+    png = 'image/png',
+    // audio
     mp3 = 'audio/mpeg',
     wav = 'audio/x-wav',
+    // video
     mp4 = 'video/mp4',
-    png = 'image/png',
+    ogg = 'video/ogg',
+    webm = 'video/webm',
 }
