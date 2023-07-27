bdb0bcf86802dc09bcf2a41b3e841f59afe3e0c7
Justin Bambrick <jbambrick@tsilhqotin.ca>
2023-07-27 15:20:55 -0700

    WIP: make changes from PR

--- libs/media-player/src/lib/video-player.tsx
+++ libs/media-player/src/lib/video-player.tsx
@@ -1,18 +1,25 @@
 import { styled } from '@mui/material';
 
+export enum VideoMIMEType {
+    Ogg = 'video/ogg',
+    MP4 = 'video/mp4',
+    Webm = 'video/webm',
+}
+
 interface VideoPlayerProps {
     videoUrl: string;
+    mimeType: VideoMIMEType;
 }
+
 const StyledVideoPlayer = styled('video')`
     border-radius: 5px;
     width: 100%;
 `;
 
-export const VideoPlayer = ({ videoUrl }: VideoPlayerProps) => {
+export const VideoPlayer = ({ videoUrl, mimeType }: VideoPlayerProps) => {
     return (
         <StyledVideoPlayer controls>
-            <source style={{ padding: 0 }} src={videoUrl} type="video/mp4" />
-            <source style={{ padding: 0 }} src={videoUrl} type="video/webm" />
+            <source style={{ padding: 0 }} src={videoUrl} type={mimeType} />
             Your browser does not support the video tag.
         </StyledVideoPlayer>
     );
--- libs/media-player/src/video-player.cy.tsx
+++ libs/media-player/src/video-player.cy.tsx
@@ -1,4 +1,4 @@
-import { VideoPlayer } from './lib/video-player';
+import { VideoMIMEType, VideoPlayer } from './lib/video-player';
 
 describe('<VideoPlayer />', () => {
     const validVideoUrl =
@@ -7,35 +7,12 @@ describe('<VideoPlayer />', () => {
     describe(`when there is a single, fixed value for videoURL`, () => {
         describe(`when there is a valid video url`, () => {
             beforeEach(() => {
-                cy.mount(<VideoPlayer videoUrl={validVideoUrl} />);
+                cy.mount(<VideoPlayer videoUrl={validVideoUrl} mimeType={VideoMIMEType.MP4} />);
             });
 
             it('should display video controls', () => {
                 cy.get('video').should('be.visible');
                 cy.get('video').should('have.prop', 'controls', true);
-            });
-
-            it(`should play video`, () => {
-                cy.get('video')
-                    .should('have.prop', 'paused', true)
-                    .and('have.prop', 'ended', false)
-                    .then(($video) => {
-                        $video[0].play();
-                    });
-                cy.wait(2000);
-            });
-
-            it(`should pause video`, () => {
-                cy.get('video')
-                    .should('have.prop', 'paused', true)
-                    .and('have.prop', 'ended', false)
-                    .then(($video) => {
-                        $video[0].play();
-                    });
-                cy.wait(4000);
-                cy.get('video').then(($video) => {
-                    $video[0].pause();
-                });
             });
         });
     });
