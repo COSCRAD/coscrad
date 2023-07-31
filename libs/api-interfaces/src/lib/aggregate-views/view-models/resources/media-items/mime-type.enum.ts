484c9dd904ab201442bc38870ff0e615058751cf
Justin Bambrick <55550194+jbambrick@users.noreply.github.com>
2023-07-31 15:01:32 -0700

    feat(coscrad-frontend): create audio player component (#453)
    * feat(coscrad-frontend): create audio player component
    
    * WIP: make changes based on feedback from PR
    
    * WIP: add mime types for audio
    
    * update stale snapshots

--- libs/api-interfaces/src/lib/aggregate-views/view-models/resources/media-items/mime-type.enum.ts
+++ libs/api-interfaces/src/lib/aggregate-views/view-models/resources/media-items/mime-type.enum.ts
@@ -1,11 +1,14 @@
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
 export enum MIMEType {
     // images
     png = 'image/png',
     // audio
     mp3 = 'audio/mpeg',
+    // TODO change this to audio/wav. This requires a migration
     wav = 'audio/x-wav',
+    audioOgg = 'audio/ogg',
     // video
     mp4 = 'video/mp4',
-    ogg = 'video/ogg',
-    webm = 'video/webm',
+    videoOgg = 'video/ogg',
+    videoWebm = 'video/webm',
 }
