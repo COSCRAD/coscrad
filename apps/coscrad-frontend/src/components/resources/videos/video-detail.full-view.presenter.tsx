143bdbbcfa226653456bc2c5554f64d2f19b903d
Justin Bambrick <jbambrick@tsilhqotin.ca>
2023-08-02 08:42:03 -0700

    WIP: update video detail and create e2e test for query flow

--- apps/coscrad-frontend/src/components/resources/videos/video-detail.full-view.presenter.tsx
+++ apps/coscrad-frontend/src/components/resources/videos/video-detail.full-view.presenter.tsx
@@ -3,6 +3,7 @@ import {
     IVideoViewModel,
     ResourceType,
 } from '@coscrad/api-interfaces';
+import { VideoPlayer } from '@coscrad/media-player';
 import { SinglePropertyPresenter } from '../../../utils/generic-components';
 import { ResourceDetailFullViewPresenter } from '../../../utils/generic-components/presenters/detail-views';
 import { convertMillisecondsToSeconds } from '../utils/math';
@@ -12,13 +13,14 @@ export const VideoDetailFullViewPresenter = ({
     text: plainText,
     name,
     id,
+    videoUrl,
 }: ICategorizableDetailQueryResult<IVideoViewModel>): JSX.Element => (
     <ResourceDetailFullViewPresenter name={name} id={id} type={ResourceType.video}>
         <SinglePropertyPresenter
             display="Duration"
             value={convertMillisecondsToSeconds(lengthMilliseconds)}
         />
-        {/* TODO[https://www.pivotaltracker.com/story/show/184530937] Support video playback via the media player lib  */}
+        <VideoPlayer videoUrl={videoUrl} />
         {/* TODO[https://www.pivotaltracker.com/story/show/184666073] Create a transcript presenter */}
         <h3>Transcript:</h3>
         <p>{plainText}</p>
