de0ee2d6ba8cc5339882f241052da19bda8299b1
Aaron Plahn <aplahn85@gmail.com>
2023-08-15 11:27:40 -0700

    breakout steps utility

--- apps/coscrad-frontend-e2e/src/e2e/resources/videos/commands/video.TRANSLATE_LINE_ITEM.e2e.cy.ts
+++ apps/coscrad-frontend-e2e/src/e2e/resources/videos/commands/video.TRANSLATE_LINE_ITEM.e2e.cy.ts
@@ -1,35 +1,5 @@
 import { AggregateType, LanguageCode } from '@coscrad/api-interfaces';
-import { buildDummyAggregateCompositeIdentifier } from '../../../../support/utilities';
-
-class Steps {
-    private stepsMap: Map<string, () => void>;
-
-    constructor() {
-        this.stepsMap = new Map<string, () => void>();
-    }
-
-    addStep(stepName: string, step: () => void): this {
-        if (this.stepsMap.has(stepName)) {
-            throw new Error(`There is already a step with name: ${stepName}`);
-        }
-
-        this.stepsMap.set(stepName, step);
-
-        return this;
-    }
-
-    apply(namesOfStepsToApply: string[]) {
-        namesOfStepsToApply.forEach((stepName) => {
-            if (!this.stepsMap.has(stepName)) {
-                throw new Error(`Failed to apply unknown step: ${stepName}`);
-            }
-
-            const step = this.stepsMap.get(stepName);
-
-            step();
-        });
-    }
-}
+import { Steps, buildDummyAggregateCompositeIdentifier } from '../../../../support/utilities';
 
 describe(`TRANSLATE_LINE_ITEM`, () => {
     before(() => {
