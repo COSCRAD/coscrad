47ab5dc1901079b92ccb439b3400896fa5174d0f
bsellars <101149169+bsellars@users.noreply.github.com>
2023-09-06 13:00:45 -0700

    feat(api): introduce 185579460-TRANSLATE_VIDEO_NAME (#469)
    * WIP: translate-video-name
    
    * feat(api): introduce TRANSLATE_VIDEO_NAME
    
    * revert a previous method rename error
    
    * fix previous unintentional method rename
    
    * updated snapshots
    
    * feat(api): introduce TRANSLATE_VIDEO_NAME
    
    ---------
    
    Co-authored-by: Aaron Plahn <aplahn85@gmail.com>

--- apps/api/src/domain/models/video/commands/index.ts
+++ apps/api/src/domain/models/video/commands/index.ts
@@ -1,1 +1,2 @@
 export * from './create-video';
+export * from './translate-video-name';
---
+++ apps/api/src/domain/models/video/commands/translate-video-name/translate-video-name.command-handler.ts
@@ -0,0 +1,36 @@
+import { CommandHandler, ICommand } from '@coscrad/commands';
+import { Valid } from '../../../../../domain/domainModelValidators/Valid';
+import { DeluxeInMemoryStore } from '../../../../../domain/types/DeluxeInMemoryStore';
+import { InMemorySnapshot } from '../../../../../domain/types/ResourceType';
+import { InternalError } from '../../../../../lib/errors/InternalError';
+import { ResultOrError } from '../../../../../types/ResultOrError';
+import { Video } from '../../../audio-item/entities/video.entity';
+import { BaseUpdateCommandHandler } from '../../../shared/command-handlers/base-update-command-handler';
+import { BaseEvent } from '../../../shared/events/base-event.entity';
+import { TranslateVideoName } from './translate-video-name.command';
+import { VideoNameTranslated } from './video-name-translated.event';
+
+@CommandHandler(TranslateVideoName)
+export class TranslateVideoNameCommandHandler extends BaseUpdateCommandHandler<Video> {
+    protected async fetchRequiredExternalState(_: TranslateVideoName): Promise<InMemorySnapshot> {
+        return Promise.resolve(new DeluxeInMemoryStore({}).fetchFullSnapshotInLegacyFormat());
+    }
+
+    protected validateExternalState(
+        _state: InMemorySnapshot,
+        _instance: Video
+    ): InternalError | Valid {
+        return Valid;
+    }
+
+    protected actOnInstance(
+        video: Video,
+        { text, languageCode }: TranslateVideoName
+    ): ResultOrError<Video> {
+        return video.translateName(text, languageCode);
+    }
+
+    protected buildEvent(command: ICommand, eventId: string, userId: string): BaseEvent {
+        return new VideoNameTranslated(command, eventId, userId);
+    }
+}
