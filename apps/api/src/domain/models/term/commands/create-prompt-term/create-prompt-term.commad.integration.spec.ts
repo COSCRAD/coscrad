43c7272c0e1f681504272879ee31bca8700d3991
Aaron Plahn <aplahn85@gmail.com>
2023-09-23 12:14:04 -0700

    test(api): update some stale snapshots

--- apps/api/src/domain/models/term/commands/create-prompt-term/create-prompt-term.commad.integration.spec.ts
+++ apps/api/src/domain/models/term/commands/create-prompt-term/create-prompt-term.commad.integration.spec.ts
@@ -5,17 +5,26 @@ import setUpIntegrationTest from '../../../../../app/controllers/__tests__/setUp
 import { CommandFSA } from '../../../../../app/controllers/command/command-fsa/command-fsa.entity';
 import { IIdManager } from '../../../../../domain/interfaces/id-manager.interface';
 import { DeluxeInMemoryStore } from '../../../../../domain/types/DeluxeInMemoryStore';
+import assertErrorAsExpected from '../../../../../lib/__tests__/assertErrorAsExpected';
 import { clonePlainObjectWithOverrides } from '../../../../../lib/utilities/clonePlainObjectWithOverrides';
 import { ArangoDatabaseProvider } from '../../../../../persistence/database/database.provider';
 import TestRepositoryProvider from '../../../../../persistence/repositories/__tests__/TestRepositoryProvider';
 import generateDatabaseNameForTestSuite from '../../../../../persistence/repositories/__tests__/generateDatabaseNameForTestSuite';
 import { buildTestCommandFsaMap } from '../../../../../test-data/commands';
+import getValidAggregateInstanceForTest from '../../../../__tests__/utilities/getValidAggregateInstanceForTest';
 import { MultilingualTextItemRole } from '../../../../common/entities/multilingual-text';
 import { AggregateId } from '../../../../types/AggregateId';
 import { AggregateType } from '../../../../types/AggregateType';
+import { assertCommandFailsDueToTypeError } from '../../../__tests__/command-helpers/assert-command-payload-type-error';
+import { assertCreateCommandError } from '../../../__tests__/command-helpers/assert-create-command-error';
 import { assertCreateCommandSuccess } from '../../../__tests__/command-helpers/assert-create-command-success';
+import { generateCommandFuzzTestCases } from '../../../__tests__/command-helpers/generate-command-fuzz-test-cases';
 import { CommandAssertionDependencies } from '../../../__tests__/command-helpers/types/CommandAssertionDependencies';
+import buildDummyUuid from '../../../__tests__/utilities/buildDummyUuid';
 import { dummySystemUserId } from '../../../__tests__/utilities/dummySystemUserId';
+import AggregateIdAlreadyInUseError from '../../../shared/common-command-errors/AggregateIdAlreadyInUseError';
+import CommandExecutionError from '../../../shared/common-command-errors/CommandExecutionError';
+import InvalidExternalStateError from '../../../shared/common-command-errors/InvalidExternalStateError';
 import { Term } from '../../entities/term.entity';
 import { CREATE_PROMPT_TERM } from './constants';
 import { CreatePromptTerm } from './create-prompt-term.command';
@@ -103,6 +112,78 @@ describe(commandType, () => {
                     expect(termTextItem.languageCode).toBe(LanguageCode.English);
                 },
             });
+        });
+    });
+
+    describe(`when the command is invalid`, () => {
+        describe(`when there is already a term with the given ID`, () => {
+            it(`should fail with the expected errors`, async () => {
+                const usedId = await idManager.generate();
+
+                await assertCreateCommandError(assertionHelperDependencies, {
+                    systemUserId: dummySystemUserId,
+                    initialState: new DeluxeInMemoryStore({
+                        term: [
+                            getValidAggregateInstanceForTest(AggregateType.term).clone({
+                                id: usedId,
+                            }),
+                        ],
+                    }).fetchFullSnapshotInLegacyFormat(),
+                    buildCommandFSA: (_id) => buildValidCommandFSA(usedId),
+                    checkError: (error) => {
+                        assertErrorAsExpected(
+                            error,
+                            new CommandExecutionError([
+                                new InvalidExternalStateError([
+                                    new AggregateIdAlreadyInUseError({
+                                        type: AggregateType.term,
+                                        id: usedId,
+                                    }),
+                                ]),
+                            ])
+                        );
+                    },
+                });
+            });
+        });
+
+        describe(`when the payload type is invalid`, () => {
+            describe(`when the aggregate type is not term`, () => {
+                Object.values(AggregateType)
+                    .filter((aggregateType) => aggregateType !== AggregateType.term)
+                    .forEach((invalidAggregateType) => {
+                        describe(`when the invalid aggregate type is: ${invalidAggregateType}`, () => {
+                            it(`should fail with the expected errors`, async () => {
+                                await assertCommandFailsDueToTypeError(
+                                    assertionHelperDependencies,
+                                    {
+                                        propertyName: 'aggregateCompositeIdentifier',
+                                        invalidValue: {
+                                            type: invalidAggregateType,
+                                            id: commandFsa.payload.aggregateCompositeIdentifier.id,
+                                        },
+                                    },
+                                    commandFsa
+                                );
+                            });
+                        });
+                    });
+            });
+
+            // Fuzz test
+            generateCommandFuzzTestCases(CreatePromptTerm).forEach(
+                ({ description, propertyName, invalidValue }) => {
+                    describe(`when the property: ${propertyName} has the invalid value:${invalidValue} (${description}`, () => {
+                        it('should fail with the appropriate error', async () => {
+                            await assertCommandFailsDueToTypeError(
+                                assertionHelperDependencies,
+                                { propertyName, invalidValue },
+                                buildValidCommandFSA(buildDummyUuid(123))
+                            );
+                        });
+                    });
+                }
+            );
         });
     });
 });
