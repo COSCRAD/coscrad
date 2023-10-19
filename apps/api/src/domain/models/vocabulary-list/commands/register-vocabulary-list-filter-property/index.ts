b7e77569ee03a20f69d5a2f1e33eb46464a77147
Blake Sellars <blakesellars02@gmail.com>
2023-10-19 10:28:34 -0700

    scaffold new command

---
+++ apps/api/src/domain/models/vocabulary-list/commands/register-vocabulary-list-filter-property/index.ts
@@ -0,0 +1,4 @@
+export * from './constants';
+export * from './register-vocabulary-list-filter-property.command';
+export * from './register-vocabulary-list-filter-property.command-handler';
+export * from './vocabulary-list-registered-property-filter.event';
