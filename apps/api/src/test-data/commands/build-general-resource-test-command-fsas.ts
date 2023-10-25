92c3616826db70aff501058cdfc7a5faefa3bc0b
Aaron Plahn <aplahn85@gmail.com>
2023-10-25 16:34:08 -0700

    test(api): update test command FSAs

--- apps/api/src/test-data/commands/build-general-resource-test-command-fsas.ts
+++ apps/api/src/test-data/commands/build-general-resource-test-command-fsas.ts
@@ -3,7 +3,6 @@ import buildDummyUuid from '../../domain/models/__tests__/utilities/buildDummyUu
 import { PublishResource } from '../../domain/models/shared/common-commands';
 import { GrantResourceReadAccessToUser } from '../../domain/models/shared/common-commands/grant-user-read-access/grant-resource-read-access-to-user.command';
 import { AggregateType } from '../../domain/types/AggregateType';
-import { ResourceType } from '../../domain/types/ResourceType';
 
 const id = buildDummyUuid(5555);
 
@@ -21,13 +20,10 @@ const grantResourceReadAccessToUser: CommandFSA<GrantResourceReadAccessToUser> =
     type: `GRANT_RESOURCE_READ_ACCESS_TO_USER`,
     payload: {
         aggregateCompositeIdentifier: {
-            type: AggregateType.user,
+            type: AggregateType.digitalText,
             id,
         },
-        resourceCompositeIdentifier: {
-            type: ResourceType.digitalText,
-            id: buildDummyUuid(5556),
-        },
+        userId: buildDummyUuid(123),
     },
 };
 
