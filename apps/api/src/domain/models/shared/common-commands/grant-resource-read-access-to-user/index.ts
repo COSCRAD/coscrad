1201497a4196847d226d15d94f93419d9d7125c7
Aaron Plahn <aplahn85@gmail.com>
2023-10-30 09:07:58 -0700

    remove accidental barrel export of test file

--- apps/api/src/domain/models/shared/common-commands/grant-resource-read-access-to-user/index.ts
+++ apps/api/src/domain/models/shared/common-commands/grant-resource-read-access-to-user/index.ts
@@ -1,4 +1,3 @@
 export * from './grant-resource-read-access-to-user.command';
 export * from './grant-resource-read-access-to-user.command-handler';
-export * from './grant-resource-read-access-to-user.command.integration.spec';
 export * from './resource-read-access-granted-to-user.event';
