acdec790a616639bf5573148cabf5dfb0d750ca7
Aaron Plahn <aplahn85@gmail.com>
2023-10-30 09:28:31 -0700

    fix(api): make resource the aggregate root for GRANT_RESOURCE_READ_ACCESS_TO_USER (#493)
    * WIP[fix-grant-resource-read-access] make resoruce the aggregate context for this command
    
    * test(api): update test command FSAs
    
    * implement event handler for ACL access
    
    * refactor(api): export constants for command and event type
    
    * remove accidental barrel export of test file

---
+++ apps/api/src/domain/models/shared/common-commands/grant-resource-read-access-to-user/index.ts
@@ -0,0 +1,3 @@
+export * from './grant-resource-read-access-to-user.command';
+export * from './grant-resource-read-access-to-user.command-handler';
+export * from './resource-read-access-granted-to-user.event';
