44625323b25fb2df6588922392c9c2e5df3094e3
Justin Bambrick <jbambrick@tsilhqotin.ca>
2023-11-21 13:40:24 -0800

    WIP: change home screen button to Alphabet

--- apps/mobile-alphabet/src/app/Components/Home.tsx
+++ apps/mobile-alphabet/src/app/Components/Home.tsx
@@ -5,8 +5,7 @@ const HomeScreen = ({ navigation }) => {
     return (
         <View testID="Home" style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home screen works!</Text>
-            <Button title="Terms" />
-            <Button title="Paradigms and Vocabulary Lists" />
+            <Button title="Tŝilhqot’in Alphabet" />
             <Button testID="Credits" title="Credits" onPress={() => navigation.push('Credits')} />
         </View>
     );
