a71707b8c478f3f4253d6c1752e5be6de938aaa7
Justin Bambrick <jbambrick@tsilhqotin.ca>
2023-11-21 11:19:55 -0800

    WIP: test routes with detox using typescript

---
+++ e2e/starter.test.ts
@@ -0,0 +1,20 @@
+import { by, device, element, expect } from 'detox';
+
+describe('Example', () => {
+    beforeAll(async () => {
+        await device.launchApp();
+    });
+
+    beforeEach(async () => {
+        await device.reloadReactNative();
+    });
+
+    it('should have welcome screen', async () => {
+        await expect(element(by.id('Home'))).toBeVisible();
+    });
+
+    it('should show credits screen after pressing credits button', async () => {
+        await element(by.id('Credits')).tap();
+        await expect(element(by.text('Credits'))).toBeVisible();
+    });
+});
