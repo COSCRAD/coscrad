32f9b2ea19dcfced6bf210b96ec5d44b28a9dd56
Aaron Plahn <aplahn85@gmail.com>
2023-12-14 11:01:54 -0800

    fix some failing tests

--- apps/api/src/domain/factories/__tests__/buildAggregateFactoryTestCases/media-item.aggregate-factory.test-set.ts
+++ apps/api/src/domain/factories/__tests__/buildAggregateFactoryTestCases/media-item.aggregate-factory.test-set.ts
@@ -1,10 +1,6 @@
 import { FactoryTestSuiteForAggregate } from '.';
-import { clonePlainObjectWithoutProperties } from '../../../../lib/utilities/clonePlainObjectWithoutProperties';
-import assertErrorAsExpected from '../../../../lib/__tests__/assertErrorAsExpected';
-import MediaItemHasNoTitleInAnyLanguageError from '../../../domainModelValidators/errors/mediaItem/MediaItemHasNoTitleInAnyLanguageError';
-import { AggregateType } from '../../../types/AggregateType';
-import buildInvariantValidationErrorFactoryFunction from '../../../__tests__/utilities/buildInvariantValidationErrorFactoryFunction';
 import getValidAggregateInstanceForTest from '../../../__tests__/utilities/getValidAggregateInstanceForTest';
+import { AggregateType } from '../../../types/AggregateType';
 import buildNullAndUndefinedAggregateFactoryInvalidTestCases from './common/buildNullAndUndefinedAggregateFactoryInvalidTestCases';
 import { generateFuzzAggregateFactoryTestCases } from './utilities/generate-fuzz-aggregate-factory-test-cases';
 
@@ -25,17 +21,6 @@ export const buildMediaItemFactoryTestSet = (): FactoryTestSuiteForAggregate<
         },
     ],
     invalidCases: [
-        {
-            description: 'the media item has no title in either language',
-            dto: clonePlainObjectWithoutProperties(validDto, ['title', 'titleEnglish']),
-            checkError: (result: unknown) =>
-                assertErrorAsExpected(
-                    result,
-                    buildInvariantValidationErrorFactoryFunction(aggregateType)(validDto.id, [
-                        new MediaItemHasNoTitleInAnyLanguageError(validDto.id),
-                    ])
-                ),
-        },
         ...buildNullAndUndefinedAggregateFactoryInvalidTestCases(aggregateType),
         ...generateFuzzAggregateFactoryTestCases(aggregateType, validDto),
     ],
