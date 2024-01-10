8b666605d2000cbf06c51dd40c178c137116bef7
Aaron Plahn <aplahn85@gmail.com>
2024-01-10 15:32:18 -0800

    loosen interface and fix build

--- apps/api/src/queries/interfaces/aggregate-root.query-repository.interface.ts
+++ apps/api/src/queries/interfaces/aggregate-root.query-repository.interface.ts
@@ -1,18 +1,7 @@
-import { CoscradUserWithGroups } from '../../domain/models/user-management/user/entities/user/coscrad-user-with-groups';
+import { IBaseViewModel } from '@coscrad/api-interfaces';
 import { Maybe } from '../../lib/types/maybe';
 
-interface HasAggregateCompositeIdentifier {
-    type: string;
-    id: string;
-}
-
-interface BaseAggregateRootViewModel extends HasAggregateCompositeIdentifier {
-    isPublished: boolean;
-    // TODO Use an interface for the param type here
-    hasReadAccess(userWithGroups: CoscradUserWithGroups): boolean;
-}
-
-export interface IAggregateRootQueryRepository<T extends BaseAggregateRootViewModel> {
+export interface IAggregateRootQueryRepository<T extends IBaseViewModel> {
     // TODO Include filters \ specifications
     fetchById(id: string): Promise<Maybe<T>>;
 
