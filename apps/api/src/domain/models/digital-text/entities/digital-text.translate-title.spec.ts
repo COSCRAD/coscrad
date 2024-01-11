36f9fa5af9dac5783696545b47c13f97196888e0
Blake Sellars <blakesellars02@gmail.com>
2024-01-11 10:56:51 -0800

    implement invalid test cases for translateTitle update method

--- apps/api/src/domain/models/digital-text/entities/digital-text.translate-title.spec.ts
+++ apps/api/src/domain/models/digital-text/entities/digital-text.translate-title.spec.ts
@@ -1,8 +1,11 @@
 import { LanguageCode, MultilingualTextItemRole } from '@coscrad/api-interfaces';
+import assertErrorAsExpected from '../../../../lib/__tests__/assertErrorAsExpected';
 import { NotFound } from '../../../../lib/types/not-found';
 import getValidAggregateInstanceForTest from '../../../__tests__/utilities/getValidAggregateInstanceForTest';
 import { buildMultilingualTextWithSingleItem } from '../../../common/build-multilingual-text-with-single-item';
+import { CannotAddDuplicateTranslationError } from '../../../common/entities/errors';
 import { MultilingualTextItem } from '../../../common/entities/multilingual-text';
+import InvariantValidationError from '../../../domainModelValidators/errors/InvariantValidationError';
 import { AggregateType } from '../../../types/AggregateType';
 import { DigitalText } from './digital-text.entity';
 
@@ -18,10 +21,10 @@ const digitalText = getValidAggregateInstanceForTest(AggregateType.digitalText).
     title: existingTitle,
 });
 
+const translationOfTitle = `translation of title`;
+
 describe(`DigitalText.translateTitle`, () => {
     describe(`when the translation is valid`, () => {
-        const translationOfTitle = `translation of title`;
-
         it(`should return the updated title`, () => {
             const result = digitalText.translateTitle(translationOfTitle, translationLanguageCode);
 
@@ -41,6 +44,63 @@ describe(`DigitalText.translateTitle`, () => {
             expect(text).toBe(translationOfTitle);
 
             expect(role).toBe(MultilingualTextItemRole.freeTranslation);
+        });
+    });
+
+    describe(`when the translation is invalid`, () => {
+        describe(`when the translation and original languages are the same`, () => {
+            it(`should return the expected error`, () => {
+                const result = digitalText.translateTitle(translationOfTitle, existingLanguageCode);
+
+                assertErrorAsExpected(
+                    result,
+                    new CannotAddDuplicateTranslationError(
+                        new MultilingualTextItem({
+                            text: translationOfTitle,
+                            languageCode: existingLanguageCode,
+                            role: MultilingualTextItemRole.freeTranslation,
+                        }),
+                        digitalText.title
+                    )
+                );
+            });
+        });
+
+        describe(`when the title has already been translated to the target language`, () => {
+            it(`should return the expected error`, () => {
+                const digitalTextWithTranslation = digitalText.translateTitle(
+                    translationOfTitle,
+                    translationLanguageCode
+                ) as DigitalText;
+
+                const result = digitalTextWithTranslation.translateTitle(
+                    translationOfTitle,
+                    translationLanguageCode
+                );
+
+                assertErrorAsExpected(
+                    result,
+                    new CannotAddDuplicateTranslationError(
+                        new MultilingualTextItem({
+                            text: translationOfTitle,
+                            languageCode: translationLanguageCode,
+                            role: MultilingualTextItemRole.freeTranslation,
+                        }),
+                        digitalTextWithTranslation.title
+                    )
+                );
+            });
+        });
+
+        describe(`when the translation is an empty string`, () => {
+            it(`should return an error`, () => {
+                const result = digitalText.translateTitle('', translationLanguageCode);
+
+                assertErrorAsExpected(
+                    result,
+                    new InvariantValidationError(digitalText.getCompositeIdentifier(), [])
+                );
+            });
         });
     });
 });
