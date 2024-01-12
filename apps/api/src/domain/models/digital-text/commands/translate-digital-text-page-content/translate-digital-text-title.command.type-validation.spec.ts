6f5660d89a14258f29adb1aea03c05e68c8fa5eb
Blake Sellars <blakesellars02@gmail.com>
2024-01-12 13:24:58 -0800

    removed comments

--- apps/api/src/domain/models/digital-text/commands/translate-digital-text-page-content/translate-digital-text-title.command.type-validation.spec.ts
+++ apps/api/src/domain/models/digital-text/commands/translate-digital-text-page-content/translate-digital-text-title.command.type-validation.spec.ts
@@ -27,21 +27,12 @@ const commandFsaFactory = new DummyCommandFsaFactory<TranslateDigitalTextTitle>(
     })
 );
 
-/**
- * We have moved these comprehensive payload type validation tests here to
- * a (closer to) unit test and out of the integration (almost e2e) command
- * test that uses the API and live database over the network.
- *
- * This is because the cost of driving the network doesn't justify the marginal
- * gain in confidence.
- */
 describe(`${commandType} (payload type validation)`, () => {
     describe(`when the command payload type is invalid`, () => {
         describe(`when the aggregate type is not digital text`, () => {
             Object.values(AggregateType)
                 .filter((aggregateType) => aggregateType !== AggregateType.digitalText)
                 .forEach((invalidAggregateType) => {
-                    // TODO add a test helper for this
                     const invalidFsa = commandFsaFactory.build(undefined, {
                         aggregateCompositeIdentifier: {
                             type: invalidAggregateType,
@@ -59,7 +50,6 @@ describe(`${commandType} (payload type validation)`, () => {
 
                     const error = result as InternalError;
 
-                    // This pattern logs the failing message on failure
                     const messagesToCheck = [error.toString()];
 
                     const errorMessagesThatDoNotReferencePropertyName = messagesToCheck.filter(
@@ -75,7 +65,6 @@ describe(`${commandType} (payload type validation)`, () => {
                 ({ description, propertyName, invalidValue }) => {
                     describe(`when the property: ${propertyName} has the invalid value:${invalidValue} (${description}`, () => {
                         it('should fail with the appropriate error', () => {
-                            // TODO add a test helper for this
                             const invalidFsa = commandFsaFactory.build(undefined, {
                                 aggregateCompositeIdentifier: {
                                     [propertyName]: invalidValue,
@@ -93,7 +82,6 @@ describe(`${commandType} (payload type validation)`, () => {
 
                             const error = result as InternalError;
 
-                            // This pattern logs the failing message on failure
                             const messagesToCheck = [error.toString()];
 
                             const errorMessagesThatDoNotReferencePropertyName =
