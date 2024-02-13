d646081446eae75ea291e5b7d5a4b9857ff3c171
Blake Sellars <blakesellars02@gmail.com>
2024-02-13 09:50:40 -0800

    WIP: add failing test for media-item-queries.e2e

--- apps/api/src/app/controllers/resources/media-item-queries.e2e.spec.ts
+++ apps/api/src/app/controllers/resources/media-item-queries.e2e.spec.ts
@@ -1,6 +1,45 @@
+import { INestApplication } from '@nestjs/common';
+import buildDummyUuid from '../../../domain/models/__tests__/utilities/buildDummyUuid';
+import { InternalError } from '../../../lib/errors/InternalError';
+import { NotFound } from '../../../lib/types/not-found';
+import { ArangoDatabaseProvider } from '../../../persistence/database/database.provider';
+import TestRepositoryProvider from '../../../persistence/repositories/__tests__/TestRepositoryProvider';
+import generateDatabaseNameForTestSuite from '../../../persistence/repositories/__tests__/generateDatabaseNameForTestSuite';
+import buildTestData from '../../../test-data/buildTestData';
+
+const mediaItemId = buildDummyUuid(123);
+
 describe('when querying for a media item by name (/resources/mediaItems/download?name=foo)', () => {
+    const _testDatabaseName = generateDatabaseNameForTestSuite();
+
+    let _app: INestApplication;
+
+    let _testRepositoryProvider: TestRepositoryProvider;
+
+    let _databaseProvider: ArangoDatabaseProvider;
+
+    const testData = buildTestData();
+
+    const _tagTestData = testData.tag;
+
+    const _resourceTestData = testData.resources;
+
+    // beforeAll(async () => {
+    // ({ app, testRepositoryProvider, databaseProvider } = await setUpIntegrationTest({
+    // ARANGO_DB_NAME: testDatabaseName,
+    // }));
+    // });
+
     describe(`when the media item does not exist`, () => {
-        it.todo('should return a 400');
+        it.only('should return a 400', () => {
+            if (mediaItemId) {
+                return new InternalError(
+                    `400 error, failed to find a media item id. ${mediaItemId}`
+                );
+            }
+
+            expect(mediaItemId).toBeInstanceOf(NotFound);
+        });
     });
 
     describe(`when the media item exists`, () => {
