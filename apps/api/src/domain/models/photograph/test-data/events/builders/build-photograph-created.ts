3a14c93d43fe33cb31e9a85042aab3cc2d7a5515
Aaron Plahn <aplahn85@gmail.com>
2024-02-14 09:46:02 -0800

    refactor(api): introduce event sourcing logic for Photographs (#549)

---
+++ apps/api/src/domain/models/photograph/test-data/events/builders/build-photograph-created.ts
@@ -0,0 +1,29 @@
+import { AggregateType, LanguageCode } from '@coscrad/api-interfaces';
+import { clonePlainObjectWithOverrides } from '../../../../../../lib/utilities/clonePlainObjectWithOverrides';
+import { EventMetadataBuilder } from '../../../../../../test-data/events';
+import { DeepPartial } from '../../../../../../types/DeepPartial';
+import buildDummyUuid from '../../../../__tests__/utilities/buildDummyUuid';
+import { PhotographCreated, PhotographCreatedPayload } from '../../../commands';
+
+export const buildPhotographCreated = (
+    payloadOverrides: DeepPartial<PhotographCreatedPayload>,
+    buildMetadata: EventMetadataBuilder
+) => {
+    const defaultPayload: PhotographCreatedPayload = {
+        aggregateCompositeIdentifier: {
+            type: AggregateType.photograph,
+            id: buildDummyUuid(1),
+        },
+        title: 'a nice picture',
+        languageCodeForTitle: LanguageCode.English,
+        heightPx: 900,
+        widthPx: 1200,
+        mediaItemId: buildDummyUuid(2),
+        photographer: 'Aaron Plahn',
+    };
+
+    return new PhotographCreated(
+        clonePlainObjectWithOverrides(defaultPayload, payloadOverrides),
+        buildMetadata()
+    );
+};
