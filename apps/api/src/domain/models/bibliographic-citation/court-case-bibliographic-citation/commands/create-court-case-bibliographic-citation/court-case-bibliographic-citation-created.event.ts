ef4dcce1cad6d04b9e970a8a42a69ca0adeba809
Aaron Plahn <aplahn85@gmail.com>
2024-03-04 17:15:14 -0800

    refactor(api): introduce event sourcing logic for court cases (#554)

--- apps/api/src/domain/models/bibliographic-citation/court-case-bibliographic-citation/commands/create-court-case-bibliographic-citation/court-case-bibliographic-citation-created.event.ts
+++ apps/api/src/domain/models/bibliographic-citation/court-case-bibliographic-citation/commands/create-court-case-bibliographic-citation/court-case-bibliographic-citation-created.event.ts
@@ -1,5 +1,8 @@
 import { BaseEvent } from '../../../../shared/events/base-event.entity';
+import { CreateCourtCaseBibliographicCitation } from './create-court-case-bibliographic-citation.command';
 
-export class CourtCaseBibliographicCitationCreated extends BaseEvent {
-    type = 'COURT_CASE_BIBLIOGRAPHIC_CITATION_CREATED';
+export type CourtCaseBibliographicCitationCreatedPayload = CreateCourtCaseBibliographicCitation;
+
+export class CourtCaseBibliographicCitationCreated extends BaseEvent<CourtCaseBibliographicCitationCreatedPayload> {
+    readonly type = 'COURT_CASE_BIBLIOGRAPHIC_CITATION_CREATED';
 }
