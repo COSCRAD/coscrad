b4f88e62aa8ee540bae72f346da304a03f675f54
Aaron Plahn <aplahn85@gmail.com>
2024-03-19 16:18:07 -0700

    feat(api): validate COSCRAD dates

--- apps/api/src/domain/models/user-management/utilities/coscrad-date.entity.ts
+++ apps/api/src/domain/models/user-management/utilities/coscrad-date.entity.ts
@@ -6,7 +6,9 @@ import { DTO } from '../../../../types/DTO';
 import { ResultOrError } from '../../../../types/ResultOrError';
 import { isUndefined } from '../../../utilities/validation/is-null-or-undefined';
 import BaseDomainModel from '../../BaseDomainModel';
+import { DayOfMonthTooLargeError } from './day-of-month-too-large.error';
 import { InvalidDateError } from './invalid-date.error';
+import { NotALeapYearError } from './not-a-leap-year.error';
 
 export enum Month {
     January = '01',
@@ -34,6 +36,22 @@ export const intToMonth = (int: number): Maybe<Month> => {
 
     return searchResult;
 };
+
+const daysInMonthNonLeapYear: { [K in Month]: number } = {
+    [Month.January]: 31,
+    [Month.February]: 28,
+    [Month.March]: 31,
+    [Month.April]: 30,
+    [Month.May]: 31,
+    [Month.June]: 30,
+    [Month.July]: 31,
+    [Month.August]: 31,
+    [Month.September]: 30,
+    [Month.October]: 31,
+    [Month.November]: 30,
+    [Month.December]: 31,
+};
+
 export class CoscradDate extends BaseDomainModel {
     /**
      * TODO[https://www.pivotaltracker.com/story/show/187270553]
@@ -106,6 +124,39 @@ export class CoscradDate extends BaseDomainModel {
             month: monthParseResult,
             year: parseInt(yearAsString),
         });
+    }
+
+    validateComplexInvariants(): InternalError[] {
+        if (this.month === Month.February && this.day === 29) {
+            if (!this.isLeapYear()) {
+                return [new NotALeapYearError(this.year)];
+            }
+        }
+
+        // at this point, we know we are not dealing with February 29 on a leap year
+        const lastDayOfMonth = daysInMonthNonLeapYear[this.month];
+
+        if (this.day > lastDayOfMonth) {
+            return [new DayOfMonthTooLargeError(this, lastDayOfMonth)];
+        }
+
+        return [];
+    }
+
+    private isLeapYear(): boolean {
+        const { year } = this;
+
+        if (year % 4 !== 0) {
+            return false;
+        }
+
+        if (year % 400 === 0) {
+            return true;
+        }
+
+        // We know that the year is divisble by 4 but not 400
+        // So as long as the year is not divisible by 100, it's a leap year
+        return year % 100 !== 0;
     }
 
     /**
