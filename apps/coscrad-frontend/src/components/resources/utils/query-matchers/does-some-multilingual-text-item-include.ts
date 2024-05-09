3df644051691fcfed1a7b0cf5a2f2aa327d35340
Aaron Plahn <aplahn85@gmail.com>
2024-05-09 12:02:40 -0700

    WIP add language-specific search

--- apps/coscrad-frontend/src/components/resources/utils/query-matchers/does-some-multilingual-text-item-include.ts
+++ apps/coscrad-frontend/src/components/resources/utils/query-matchers/does-some-multilingual-text-item-include.ts
@@ -1,6 +1,33 @@
-import { IMultilingualText } from '@coscrad/api-interfaces';
+import { IMultilingualText, LanguageCode } from '@coscrad/api-interfaces';
+import { isNonEmptyString } from '@coscrad/validation-constraints';
+
+const parseLanguageCode = (query: string): LanguageCode | undefined => {
+    if (!query.includes('{')) return undefined;
+
+    const splitOnBracket = query.split('{');
+
+    const beforeClosingBracket = splitOnBracket[1].split('}')[0];
+
+    return Object.values(LanguageCode).find(
+        (languageCode) => languageCode === beforeClosingBracket
+    );
+};
 
 export const doesSomeMultilingualTextItemInclude = (
     multilingualText: IMultilingualText,
     query: string
-) => multilingualText.items.some(({ text }) => text.toLowerCase().includes(query.toLowerCase()));
+) => {
+    const languageCodeInQuery = parseLanguageCode(query);
+
+    if (!isNonEmptyString(languageCodeInQuery)) {
+        // language independent search
+        return multilingualText.items.some(({ text }) =>
+            text.toLowerCase().includes(query.toLowerCase())
+        );
+    }
+
+    return multilingualText.items.some(
+        ({ text, languageCode }) =>
+            text.toLowerCase().includes(query.toLowerCase()) && languageCodeInQuery === languageCode
+    );
+};
