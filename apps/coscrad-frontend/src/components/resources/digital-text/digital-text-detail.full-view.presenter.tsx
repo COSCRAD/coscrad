2a412db129793a69dbea3e989c882c662aba5736
yaanahuu2 <gahorner@gmail.com>
2024-06-29 10:12:51 -0700

    reinstate page forms for live instance

--- apps/coscrad-frontend/src/components/resources/digital-text/digital-text-detail.full-view.presenter.tsx
+++ apps/coscrad-frontend/src/components/resources/digital-text/digital-text-detail.full-view.presenter.tsx
@@ -5,7 +5,7 @@ import {
     ResourceType,
 } from '@coscrad/api-interfaces';
 import { isNonEmptyString } from '@coscrad/validation-constraints';
-import { Button } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 import { useState } from 'react';
 import { useAppDispatch } from '../../../app/hooks';
 import { executeCommand } from '../../../store/slices/command-status';
@@ -14,8 +14,8 @@ import {
     ResourceDetailFullViewPresenter,
     SinglePropertyPresenter,
 } from '../../../utils/generic-components';
-import { cyclicDecrement, cyclicIncrement } from '../../../utils/math';
 import { ImmersiveCreateNoteForm } from '../shared/immersive-create-note-form';
+import { PageDetailNavigation } from './page-detail-navigation';
 import { PublicationForm } from './page-publication-form';
 import { PagesPresenter } from './pages-presenter';
 
@@ -45,113 +45,116 @@ export const DigitalTextDetailFullViewPresenter = ({
             // TODO [https://www.pivotaltracker.com/story/show/187668980] flow through the contributions
             contributions={[]}
         >
-            {pages.length > 0 ? (
-                /**
-                 * Adding a page or page content re-renders whole page due to re-rendering the
-                 * page icons.  This is the same problem as with the Immersive Audio Annotator.
-                 * Adding a note using the ImmersiveCreateNoteForm does not re-render the page.
-                 */
-
-                <PagesPresenter
-                    id={id}
-                    pages={pages}
-                    currentPageIdentifier={selectedPageIdentifier}
-                    setCurrentIndex={setCurrentIndex}
-                    onSubmitPageIdentifier={(pageIdentifier) => {
-                        dispatch(
-                            executeCommand({
-                                type: 'ADD_PAGE_TO_DIGITAL_TEXT',
-                                payload: {
-                                    aggregateCompositeIdentifier,
-                                    identifier: pageIdentifier,
-                                },
-                            })
-                        );
-                    }}
-                    onSubmitNewContent={({ text, languageCode, pageIdentifier }) =>
-                        dispatch(
-                            executeCommand({
-                                type: 'ADD_CONTENT_TO_DIGITAL_TEXT_PAGE',
-                                payload: {
-                                    aggregateCompositeIdentifier: {
-                                        type: AggregateType.digitalText,
-                                        id,
+            {/* /**
+             * Adding a page or page content re-renders whole page due to re-rendering the
+             * page icons.  This is the same problem as with the Immersive Audio Annotator.
+             * Adding a note using the ImmersiveCreateNoteForm does not re-render the page.
+             */}
+            <Stack>
+                <Box sx={{ mb: 1 }}>
+                    <PagesPresenter
+                        id={id}
+                        pages={pages}
+                        currentPageIdentifier={selectedPageIdentifier}
+                        setCurrentIndex={setCurrentIndex}
+                        onSubmitPageIdentifier={(pageIdentifier) => {
+                            dispatch(
+                                executeCommand({
+                                    type: 'ADD_PAGE_TO_DIGITAL_TEXT',
+                                    payload: {
+                                        aggregateCompositeIdentifier,
+                                        identifier: pageIdentifier,
                                     },
-                                    text,
-                                    languageCode,
-                                    pageIdentifier,
-                                },
-                            })
-                        )
-                    }
-                />
-            ) : null}
-            <Button
-                onClick={() => {
-                    setCurrentIndex(cyclicDecrement(currentIndex, pages.length));
-                }}
-            >
-                {'<< PREV'}
-            </Button>{' '}
-            <Button
-                onClick={() => {
-                    setCurrentIndex(cyclicIncrement(currentIndex, pages.length));
-                }}
-            >
-                {'NEXT >>'}
-            </Button>
-            {!isPublished ? (
-                <PublicationForm
-                    onSubmitForPublication={() => {
-                        dispatch(
-                            executeCommand({
-                                type: 'PUBLISH_RESOURCE',
-                                payload: {
-                                    aggregateCompositeIdentifier: aggregateCompositeIdentifier,
-                                },
-                            })
-                        );
-                    }}
-                />
-            ) : (
-                <SinglePropertyPresenter display="Status" value="Published" />
-            )}
-            <br />
-            {/* TODO [https://www.pivotaltracker.com/story/show/186539279] Include tags once we make tags event sourced */}
-            <CommaSeparatedList>
-                {tags.map(({ label }) => (
-                    <div>{label}</div>
-                ))}
-            </CommaSeparatedList>
-            <br />
-            <ImmersiveCreateNoteForm
-                buttonLabel={`ADD NOTE TO PAGE ${selectedPageIdentifier}`}
-                onSubmit={(text, languageCode, noteId) => {
-                    dispatch(
-                        executeCommand({
-                            type: 'CREATE_NOTE_ABOUT_RESOURCE',
-                            payload: {
-                                aggregateCompositeIdentifier: {
-                                    type: AggregateType.note,
-                                    id: noteId,
-                                },
-                                resourceCompositeIdentifier: {
-                                    type: AggregateType.digitalText,
-                                    id,
-                                },
-                                text,
-                                languageCode,
-                                resourceContext: isNonEmptyString(selectedPageIdentifier)
-                                    ? {
-                                          type: 'pageRange',
-                                          pageIdentifiers: [selectedPageIdentifier],
-                                      }
-                                    : { type: 'general' },
-                            },
-                        })
-                    );
-                }}
-            />
+                                })
+                            );
+                        }}
+                        onSubmitNewContent={({ text, languageCode, pageIdentifier }) =>
+                            dispatch(
+                                executeCommand({
+                                    type: 'ADD_CONTENT_TO_DIGITAL_TEXT_PAGE',
+                                    payload: {
+                                        aggregateCompositeIdentifier: {
+                                            type: AggregateType.digitalText,
+                                            id,
+                                        },
+                                        text,
+                                        languageCode,
+                                        pageIdentifier,
+                                    },
+                                })
+                            )
+                        }
+                    />
+                </Box>
+                {pages.length > 0 ? (
+                    <>
+                        <PageDetailNavigation
+                            currentIndex={currentIndex}
+                            pagesLength={pages.length}
+                            setCurrentIndex={setCurrentIndex}
+                        />
+                        <Box sx={{ m: 1 }}>
+                            {!isPublished ? (
+                                <PublicationForm
+                                    onSubmitForPublication={() => {
+                                        dispatch(
+                                            executeCommand({
+                                                type: 'PUBLISH_RESOURCE',
+                                                payload: {
+                                                    aggregateCompositeIdentifier:
+                                                        aggregateCompositeIdentifier,
+                                                },
+                                            })
+                                        );
+                                    }}
+                                />
+                            ) : (
+                                <SinglePropertyPresenter display="Status" value="Published" />
+                            )}
+                        </Box>
+                        {/* TODO [https://www.pivotaltracker.com/story/show/186539279] Include tags once we make tags event sourced */}
+                        <Box sx={{ m: 1 }}>
+                            <CommaSeparatedList>
+                                {tags.map(({ label }) => (
+                                    <div>{label}</div>
+                                ))}
+                            </CommaSeparatedList>
+                        </Box>
+                        <Box sx={{ m: 1 }}>
+                            <ImmersiveCreateNoteForm
+                                buttonLabel={`ADD NOTE TO PAGE ${selectedPageIdentifier}`}
+                                onSubmit={(text, languageCode, noteId) => {
+                                    dispatch(
+                                        executeCommand({
+                                            type: 'CREATE_NOTE_ABOUT_RESOURCE',
+                                            payload: {
+                                                aggregateCompositeIdentifier: {
+                                                    type: AggregateType.note,
+                                                    id: noteId,
+                                                },
+                                                resourceCompositeIdentifier: {
+                                                    type: AggregateType.digitalText,
+                                                    id,
+                                                },
+                                                text,
+                                                languageCode,
+                                                resourceContext: isNonEmptyString(
+                                                    selectedPageIdentifier
+                                                )
+                                                    ? {
+                                                          type: 'pageRange',
+                                                          pageIdentifiers: [selectedPageIdentifier],
+                                                      }
+                                                    : { type: 'general' },
+                                            },
+                                        })
+                                    );
+                                }}
+                            />
+                        </Box>
+                    </>
+                ) : null}
+            </Stack>
         </ResourceDetailFullViewPresenter>
     );
 };
