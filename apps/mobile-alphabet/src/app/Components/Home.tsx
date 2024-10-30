fd4f4c66eacdb191fa61bae2a7354344712aab18
Justin Bambrick <jbambrick@tsilhqotin.ca>
2024-10-30 11:41:14 -0700

    WIP: support configurable theme

--- apps/mobile-alphabet/src/app/Components/Home.tsx
+++ apps/mobile-alphabet/src/app/Components/Home.tsx
@@ -1,46 +1,21 @@
-import styles, { colors } from 'app/styles';
+import styles, { alphabetButton, creditsButton } from 'app/styles';
 import React from 'react';
 import { Pressable, Text, View } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
     return (
-        <View testID="Home" style={styles.background}>
+        <View testID="Home" style={[styles.background]}>
             <Pressable testID="Menu" onPress={() => navigation.push('Menu')}>
-                <Text
-                    style={{
-                        fontSize: 60,
-                        color: colors.primary,
-                        fontWeight: '900',
-                        fontFamily: 'OpenSans',
-                    }}
-                >
-                    Tŝilhqot’in
-                </Text>
-                <Text style={{ fontSize: 40, color: colors.accent, fontFamily: 'OpenSans' }}>
-                    Alphabet{' '}
-                </Text>
+                <Text style={alphabetButton.alphabetName}>Tŝilhqot’in</Text>
+                <Text style={alphabetButton.alphabet}>Alphabet </Text>
             </Pressable>
 
             <Pressable
-                style={{
-                    position: 'absolute',
-                    bottom: 30,
-                    left: 0,
-                    right: 0,
-                }}
+                style={creditsButton.background}
                 testID="Credits"
                 onPress={() => navigation.navigate('Credits')}
             >
-                <Text
-                    style={{
-                        textAlign: 'center',
-                        fontSize: 30,
-                        fontFamily: 'arial',
-                        color: colors.accent,
-                    }}
-                >
-                    Credits
-                </Text>
+                <Text style={creditsButton.background}>Credits</Text>
             </Pressable>
         </View>
     );
--- apps/mobile-alphabet/src/app/styles.ts
+++ apps/mobile-alphabet/src/app/styles.ts
@@ -1,29 +1,60 @@
 import { StyleSheet } from 'react-native';
+import theme from '../../theme.config.json';
 
 export const colors = {
-    primary: '#5f43b2',
-    secondary: ' #3a3153',
-    text: '#ffff',
-    accent: '#3a3153',
+    primary: theme.colors.primary,
+    secondary: theme.colors.secondary,
+    teritary: theme.colors.teritary,
+    text: theme.colors.text,
+    accent: theme.colors.accent,
     button: ' #FF9800',
 };
 
 const styles = StyleSheet.create({
     background: {
-        backgroundColor: 'white',
+        backgroundColor: colors.teritary,
         height: 1000,
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        fontFamily: theme.fonts.primary,
+    },
+});
+
+export const alphabetButton = StyleSheet.create({
+    alphabetName: {
+        color: theme.colors.text,
+        fontSize: theme.fontSizes.large,
+        fontWeight: '100',
+        fontFamily: theme.fonts.primary,
+    },
+    alphabet: {
+        color: theme.colors.text,
+        fontSize: theme.fontSizes.medium,
+        fontFamily: theme.fonts.secondary,
+    },
+});
+
+export const creditsButton = StyleSheet.create({
+    background: {
+        position: 'absolute',
+        bottom: 30,
+        left: 0,
+        right: 0,
+        textAlign: 'center',
+        fontSize: theme.fontSizes.small,
+        fontFamily: theme.fonts.secondary,
+        color: theme.colors.text,
     },
 });
 
 export const detailStyles = StyleSheet.create({
     background: {
-        backgroundColor: 'white',
+        backgroundColor: colors.primary,
         height: 1000,
         flex: 1,
         justifyContent: 'center',
+        fontFamily: theme.fonts.primary,
     },
 });
 
