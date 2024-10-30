a13d13e8f00b130a90a6a3c3fa26189940365fad
Aaron Plahn <aplahn85@gmail.com>
2024-10-30 11:20:15 -0700

    silence CLI logger by default

--- apps/api/src/coscrad-cli/logging/console-coscrad-cli-logger.ts
+++ apps/api/src/coscrad-cli/logging/console-coscrad-cli-logger.ts
@@ -1,9 +1,26 @@
+import { isNonEmptyObject } from '@coscrad/validation-constraints';
 import { Injectable } from '@nestjs/common';
 import { ICoscradLogger } from './coscrad-logger.interface';
 
+export interface CoscradLoggerOptions {
+    isEnabled: boolean;
+}
+
 @Injectable()
 export class ConsoleCoscradCliLogger implements ICoscradLogger {
+    private isEnabled = false;
+
+    constructor(options?: CoscradLoggerOptions) {
+        if (isNonEmptyObject(options) && options.isEnabled === true) {
+            this.isEnabled = true;
+        } // else leave default value of false
+    }
+
     log(message: string) {
+        if (!this.isEnabled) {
+            return;
+        }
+
         console.log(message);
     }
 }
