5d541e1356b5f451eaa48949dac6ee7154b0deab
Aaron Plahn <aplahn85@gmail.com>
2024-11-18 16:08:01 -0800

    feat(coscrad-frontend): include external links page (#610)
    * feat(coscrad-frontend): include external links page
    
    * style(coscrad-frontend): add styles to links component
    
    * update test for dynamic nav menu to consider external links entry
    
    * remove URL from presentation for external link
    
    ---------
    
    Co-authored-by: Justin Bambrick <jbambrick@tsilhqotin.ca>

---
+++ apps/coscrad-frontend/src/components/links/links.tsx
@@ -0,0 +1,33 @@
+import LaunchIcon from '@mui/icons-material/Launch';
+import { Card, Link, Typography } from '@mui/material';
+import { useContext } from 'react';
+import { ConfigurableContentContext } from '../../configurable-front-matter/configurable-content-provider';
+import { ExternalLink } from '../../configurable-front-matter/data/configurable-content-schema';
+import { FunctionalComponent } from '../../utils/types/functional-component';
+
+const LinkPresenter = ({ title, url, description }: ExternalLink) => {
+    return (
+        <Card sx={{ p: 2, mb: 2 }}>
+            <Typography mb={0.5} variant="h3">
+                {title}
+            </Typography>
+            <Typography variant="body1">{description}</Typography>
+            <Typography
+                component={Link}
+                href={url}
+                target="_blank"
+                rel="noreferrer"
+                sx={{ textDecoration: 'none' }}
+            >
+                Visit <LaunchIcon sx={{ verticalAlign: 'middle' }} />
+            </Typography>
+        </Card>
+    );
+};
+
+export const Links: FunctionalComponent = () => {
+    const { externalLinks } = useContext(ConfigurableContentContext);
+
+    // TODO return null if there are no links and test this case
+    return <>{externalLinks.map(LinkPresenter)}</>;
+};
