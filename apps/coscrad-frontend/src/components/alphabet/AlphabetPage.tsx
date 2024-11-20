1ead68c504de640d9a037828bcc540c31f9618b1
Justin Bambrick <jbambrick@tsilhqotin.ca>
2024-11-20 12:47:28 -0800

    fix media urls on client

--- apps/coscrad-frontend/src/components/alphabet/AlphabetPage.tsx
+++ apps/coscrad-frontend/src/components/alphabet/AlphabetPage.tsx
@@ -1,34 +1,45 @@
-import { IAlphabetCard, IAlphabetChart } from '@coscrad/api-interfaces';
+import { IAlphabetChart } from '@coscrad/api-interfaces';
 import { Box } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Loading } from '../loading';
 import { AlphabetPresenter } from './AlphabetPresenter';
 
-const buildDummyAlphabetCard = (letter: string, sequence_number: number): IAlphabetCard => ({
-    letter,
-    word: `word for ${letter}`,
-    sequence_number: sequence_number.toString(),
-    letter_audio: `https://coscradapi.tsilhqotinlanguage.ca/api/resources/mediaItems/download/4b12031c-2416-4f2c-9fe7-1aee89d3d682`,
-    word_audio: `https://coscradapi.tsilhqotinlanguage.ca/api/resources/mediaItems/download/4b12031c-2416-4f2c-9fe7-1aee89d3d682`,
-    standalone_image: `https://coscradapi.tsilhqotinlanguage.ca/api/resources/mediaItems/download/6518117c-e104-4bee-bf32-a0a54fc16302`,
-    card_image: `https://www.tsilhqotinlanguage.ca/${letter}-c.png`,
-});
-
-const dummyCards = [0, 1, 2, 3, 4, 5].map((index) => buildDummyAlphabetCard(`l${index}`, index));
-
-const dummyName = 'english-alphabet';
-
-const alphabet: IAlphabetChart = {
-    data: {
-        name: dummyName,
-        name_english: `${dummyName} (english)`,
-        poster: {
-            name: '',
-            url: '',
-        },
-        alphabet_cards: dummyCards,
-    },
-};
-
 export const AlphabetPage = (): JSX.Element => {
+    const endpoint = `https://coscradapi.tsilhqotinlanguage.ca/api/games/alphabet`;
+
+    const baseMediaUrl = `https://coscradapi.tsilhqotinlanguage.ca/api/resources/mediaItems/download?name=`;
+
+    const [alphabet, setAlphabet] = useState<IAlphabetChart | null>(null);
+
+    useEffect(() => {
+        fetch(endpoint).then(async (result) => {
+            const alphabetData = (await result.json()) as unknown as IAlphabetChart;
+            console.log({ alphabetData });
+
+            const dataWithFullUrls: IAlphabetChart = {
+                ...alphabetData,
+                data: {
+                    ...alphabetData.data,
+                    alphabet_cards: alphabetData.data.alphabet_cards.map((card) => ({
+                        ...card,
+                        card_image: `${baseMediaUrl}${card.card_image}`,
+                        standalone_image: `${baseMediaUrl}${card.standalone_image}`,
+
+                        word_audio: `${baseMediaUrl}${card.word_audio}`,
+
+                        letter_audio: `${baseMediaUrl}${card.letter_audio}`,
+                    })),
+                },
+            };
+
+            if (alphabet === null) setAlphabet(dataWithFullUrls);
+        });
+    }, [alphabet, baseMediaUrl, endpoint]);
+
+    if (alphabet === null) {
+        return <Loading />;
+    }
+
     return (
         <Box>
             <AlphabetPresenter {...alphabet} />
