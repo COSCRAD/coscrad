f8bd9ce0e546ce9b3ae597bda2599dc75283c382
Justin Bambrick <jbambrick@tsilhqotin.ca>
2024-11-20 14:56:23 -0800

    handle image loading gracefully

--- apps/coscrad-frontend/src/components/alphabet/AlphabetCardPresenter.tsx
+++ apps/coscrad-frontend/src/components/alphabet/AlphabetCardPresenter.tsx
@@ -1,6 +1,8 @@
 import { IAlphabetCard } from '@coscrad/api-interfaces';
 import { AudioClipPlayer } from '@coscrad/media-player';
 import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Loading } from '../loading';
 
 type AlphabetCardPresenterProps = IAlphabetCard;
 
@@ -11,6 +13,21 @@ export const AlphabetCardPresenter = ({
     word_audio,
     standalone_image,
 }: AlphabetCardPresenterProps): JSX.Element => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(true);
+        const image = new Image();
+
+        image.src = standalone_image;
+
+        image.onload = () => {
+            setIsLoading(false);
+        };
+    }, [standalone_image]);
+
+    if (isLoading) return <Loading />;
+
     const PlayLetterButton = ({ onButtonClick }) => (
         <Typography variant={'h1'} component={Box} onClick={onButtonClick}>
             {letter}
