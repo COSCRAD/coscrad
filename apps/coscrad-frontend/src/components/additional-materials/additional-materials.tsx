d71f0536caa7110fdca561ca623d21c71b556523
Justin Bambrick <jbambrick@tsilhqotin.ca>
2024-12-12 16:31:24 -0800

    coscrad(frontend): style additional materials with MUI

--- apps/coscrad-frontend/src/components/additional-materials/additional-materials.tsx
+++ apps/coscrad-frontend/src/components/additional-materials/additional-materials.tsx
@@ -1,6 +1,9 @@
 import { MIMEType } from '@coscrad/api-interfaces';
 import { AudioPlayer } from '@coscrad/media-player';
 import { isNullOrUndefined } from '@coscrad/validation-constraints';
+import ArticleIcon from '@mui/icons-material/Article';
+import TranscribeIcon from '@mui/icons-material/Transcribe';
+import { Box, IconButton, Paper, Typography } from '@mui/material';
 import { useContext } from 'react';
 import { ConfigurableContentContext } from '../../configurable-front-matter/configurable-content-provider';
 import { AdditionalMaterialItem } from '../../configurable-front-matter/data/configurable-content-schema';
@@ -18,23 +21,41 @@ const MediaItemPresenter = ({
     }
 
     return (
-        <>
-            <h2>{name}</h2>
-            <p>{description}</p>
+        <Box mb={2} p={2} component={Paper}>
+            <Typography variant={'h3'} color={'primary.main'}>
+                <IconButton>
+                    <TranscribeIcon
+                        fontSize="large"
+                        sx={{ color: 'secondary.main', verticalAlign: 'middle' }}
+                    />
+                </IconButton>{' '}
+                {name}
+            </Typography>
+            <Typography variant={'body1'}>{description}</Typography>
             <AudioPlayer audioUrl={url} />
-        </>
+        </Box>
     );
 };
 
 const PdfPresenter = ({ url, name, description }: AdditionalMaterialItem['pdf']): JSX.Element => {
     return (
-        <>
-            <h3>pdf</h3>
-            <a href={url} target="_blank" rel="noopener noreferrer">
+        <Box p={2} component={Paper}>
+            <Typography variant="h3" color={'primary.main'}>
+                <IconButton>
+                    <ArticleIcon
+                        fontSize="large"
+                        sx={{ color: 'secondary.main', verticalAlign: 'middle' }}
+                    />
+                </IconButton>
+                pdf
+            </Typography>
+            <Box component={'a'} href={url} target="_blank" rel="noopener noreferrer">
                 {name}
-            </a>
-            <p>{description}</p>
-        </>
+            </Box>
+            <Typography mb={2} mt={2} variant={'body1'} color={'text.secondary'}>
+                {description}
+            </Typography>
+        </Box>
     );
 };
 
@@ -48,10 +69,10 @@ export const AdditionalMaterials = (): JSX.Element => {
             {additionalMaterials
                 .filter(({ pdf, media }) => !isNullOrUndefined(pdf) || !isNullOrUndefined(media))
                 .map(({ pdf, media }) => (
-                    <div>
+                    <Box component={'div'}>
                         {isNullOrUndefined(media) ? null : <MediaItemPresenter {...media} />}
                         {isNullOrUndefined(pdf) ? null : <PdfPresenter {...pdf} />}
-                    </div>
+                    </Box>
                 ))}
         </>
     );
