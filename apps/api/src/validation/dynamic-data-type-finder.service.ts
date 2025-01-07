805f9ae017b53169d9cbc2c8ac0ec52cf076b257
Aaron Plahn <aplahn85@gmail.com>
2025-01-07 09:39:58 -0800

    refactor(api): optimize event queries

--- apps/api/src/validation/dynamic-data-type-finder.service.ts
+++ apps/api/src/validation/dynamic-data-type-finder.service.ts
@@ -1,6 +1,10 @@
-import { bootstrapDynamicTypes } from '@coscrad/data-types';
+import {
+    bootstrapDynamicTypes as bootstrapDynamicTypesUtil,
+    UnionFactory,
+} from '@coscrad/data-types';
 import { DiscoveryService } from '@golevelup/nestjs-discovery';
 import { Injectable } from '@nestjs/common';
+import { COSCRAD_EVENT_UNION } from '../domain/common';
 import { Ctor } from '../lib/types/Ctor';
 
 const isClass = (input): input is Ctor<unknown> => {
@@ -9,12 +13,23 @@ const isClass = (input): input is Ctor<unknown> => {
 
 @Injectable()
 export class DynamicDataTypeFinderService {
+    public unionFactory: UnionFactory;
+
     constructor(private readonly discoverService: DiscoveryService) {}
 
     async bootstrapDynamicTypes() {
         const unionProviders = await this.getAllDataClassCtors();
 
-        bootstrapDynamicTypes(unionProviders);
+        bootstrapDynamicTypesUtil(unionProviders);
+
+        if (!this.unionFactory) {
+            const dataClassCtors = await this.getAllDataClassCtors();
+
+            this.unionFactory = new UnionFactory(
+                dataClassCtors as Ctor<unknown>[],
+                COSCRAD_EVENT_UNION
+            );
+        }
     }
 
     public async getAllDataClassCtors() {
