9f9d3d5f3ace2fae06f8d854f58ba9a9f2f431a8
Aaron Plahn <aplahn85@gmail.com>
2025-01-07 11:26:43 -0800

    remove circular dependency

--- apps/api/src/validation/dynamic-data-type-finder.service.ts
+++ apps/api/src/validation/dynamic-data-type-finder.service.ts
@@ -4,16 +4,19 @@ import {
 } from '@coscrad/data-types';
 import { DiscoveryService } from '@golevelup/nestjs-discovery';
 import { Injectable } from '@nestjs/common';
-import { COSCRAD_EVENT_UNION } from '../domain/common';
 import { Ctor } from '../lib/types/Ctor';
 
 const isClass = (input): input is Ctor<unknown> => {
     return typeof input === 'function' && /^\s*class\s+/.test(input.toString());
 };
 
+interface IUnionFactory<_T = unknown, UProduct = unknown> {
+    build(discriminantValue: string, ...args: unknown[]): UProduct;
+}
+
 @Injectable()
 export class DynamicDataTypeFinderService {
-    public unionFactory: UnionFactory;
+    public unionFactory: IUnionFactory;
 
     constructor(private readonly discoverService: DiscoveryService) {}
 
@@ -27,7 +30,8 @@ export class DynamicDataTypeFinderService {
 
             this.unionFactory = new UnionFactory(
                 dataClassCtors as Ctor<unknown>[],
-                COSCRAD_EVENT_UNION
+                // TODO This doesn't belong here...
+                'COSCRAD_EVENT_UNION'
             );
         }
     }
