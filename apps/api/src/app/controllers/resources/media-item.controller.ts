5369ba356b24c710e13fcc34290817de49e62309
Aaron Plahn <aplahn85@gmail.com>
2025-01-08 15:00:08 -0800

    feat(api): define filename and filetype in binary downloads (#619)
    * feat(api): define filename and filetype in binary downloads
    
    * support inline disposition for audio and video items

--- apps/api/src/app/controllers/resources/media-item.controller.ts
+++ apps/api/src/app/controllers/resources/media-item.controller.ts
@@ -1,9 +1,13 @@
-import { IDetailQueryResult } from '@coscrad/api-interfaces';
+import { IDetailQueryResult, MIMEType } from '@coscrad/api-interfaces';
 import { isNonEmptyString } from '@coscrad/validation-constraints';
 import { Controller, Get, Param, Query, Request, Res, UseFilters, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOkResponse, ApiParam, ApiTags } from '@nestjs/swagger';
 import { existsSync } from 'fs';
 import { OptionalJwtAuthGuard } from '../../../authorization/optional-jwt-auth-guard';
+import { MultilingualText } from '../../../domain/common/entities/multilingual-text';
+import { isAudioMimeType } from '../../../domain/models/audio-visual/audio-item/entities/audio-item.entity';
+import { isVideoMimeType } from '../../../domain/models/audio-visual/video/entities/video.entity';
+import { getExtensionForMimeType } from '../../../domain/models/media-item/entities/get-extension-for-mime-type';
 import { MediaItemQueryService } from '../../../domain/services/query-services/media-management/media-item-query.service';
 import { ResourceType } from '../../../domain/types/ResourceType';
 import { InternalError, isInternalError } from '../../../lib/errors/InternalError';
@@ -48,13 +52,12 @@ export class MediaItemController {
             return sendInternalResultAsHttpResponse(res, NotFound);
         }
 
+        const name = new MultilingualText(searchResult.name).getOriginalTextItem().text;
+
         const options = {
             root: STATIC_DIR,
             dotfiles: 'deny',
-            headers: {
-                'x-timestamp': Date.now(),
-                'x-sent': true,
-            },
+            headers: this.buildHeaders({ mimeType: searchResult.mimeType, name }),
         };
 
         return res.sendFile(filePath, options);
@@ -103,10 +106,7 @@ export class MediaItemController {
         const options = {
             root: STATIC_DIR,
             dotfiles: 'deny',
-            headers: {
-                'x-timestamp': Date.now(),
-                'x-sent': true,
-            },
+            headers: this.buildHeaders({ mimeType: searchResult.mimeType, name }),
         };
 
         return res.sendFile(filePath, options);
@@ -146,5 +146,25 @@ export class MediaItemController {
         );
 
         return clonePlainObjectWithOverrides(result, { entities });
+    }
+
+    private buildHeaders({
+        mimeType,
+        name,
+    }: {
+        mimeType: MIMEType;
+        name: string;
+    }): Record<string, unknown> {
+        const disposition =
+            isAudioMimeType(mimeType) || isVideoMimeType(mimeType)
+                ? `inline`
+                : `attachment; filename="${name}.${getExtensionForMimeType(mimeType)}"`;
+
+        return {
+            'x-timestamp': Date.now(),
+            'x-sent': true,
+            'Content-Type': mimeType,
+            'Content-Disposition': disposition,
+        };
     }
 }
