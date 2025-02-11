5a0e5e1a7dbdcb32fa7537c56e8d8fe31f640c70
yaanahuu2 <gahorner@gmail.com>
2025-02-10 20:55:12 -0800

    add photograph query service

--- apps/api/src/queries/buildViewModelForResource/viewModels/photograph.view-model.ts
+++ apps/api/src/queries/buildViewModelForResource/viewModels/photograph.view-model.ts
@@ -1,34 +1,40 @@
-import { IPhotographViewModel } from '@coscrad/api-interfaces';
-import { FromDomainModel } from '@coscrad/data-types';
-import { ApiProperty } from '@nestjs/swagger';
-import { MediaItem } from '../../../domain/models/media-item/entities/media-item.entity';
-import { Photograph } from '../../../domain/models/photograph/entities/photograph.entity';
-import { BaseViewModel } from './base.view-model';
+import { AggregateType, IMultilingualText } from '@coscrad/api-interfaces';
+import { isNullOrUndefined } from '@coscrad/validation-constraints';
+import { DetailScopedCommandWriteContext } from '../../../app/controllers/command/services/command-info-service';
+import { ICoscradEvent } from '../../../domain/common';
+import { buildMultilingualTextWithSingleItem } from '../../../domain/common/build-multilingual-text-with-single-item';
+import { PhotographCreated } from '../../../domain/models/photograph';
+import { AccessControlList } from '../../../domain/models/shared/access-control/access-control-list.entity';
+import { AggregateId } from '../../../domain/types/AggregateId';
+import { HasAggregateId } from '../../../domain/types/HasAggregateId';
+import { DTO } from '../../../types/DTO';
 
-export class PhotographViewModel extends BaseViewModel implements IPhotographViewModel {
-    @ApiProperty({
-        example: 'https://www.myimages.com/mountains.png',
-        description: 'a url where the client can fetch a digital version of the photograph',
-    })
-    readonly imageUrl: string;
+export class PhotographViewModel implements HasAggregateId, DetailScopedCommandWriteContext {
+    contributions: { id: string; fullName: string }[];
 
-    @ApiProperty({
-        example: 'Justin Winters',
-        description: 'the name of the photographer who took the photograph',
-    })
-    @FromDomainModel(Photograph)
-    readonly photographer: string;
+    name: IMultilingualText;
 
-    /**
-     * The frontend will determine the dimensions from the actual
-     * image. Also, we may want to allow the frontend to request
-     * different resolutions. We can worry about this when we get
-     * there.
-     */
+    id: AggregateId;
+
+    photographer: string;
+
+    isPublished: boolean;
+
+    mediaItemId?: string;
+
+    actions: string[];
+
+    accessControlList: AccessControlList;
+
+    // notes
+
+    // tags
+
+    // events ?
+
+    // revision ?
 
     constructor(photograph: Photograph, _allMediaItems: MediaItem[]) {
-        super(photograph);
-
         const { photographer } = photograph;
 
         /**
@@ -38,7 +44,115 @@ export class PhotographViewModel extends BaseViewModel implements IPhotographVie
          * will reconcile upon rebase.
          */
         // this.imageUrl = searchResult?.url;
+    }
 
-        this.photographer = photographer;
+    static fromPhotographCreated({
+        payload: {
+            title,
+            languageCodeForTitle,
+            aggregateCompositeIdentifier: { id: photographId },
+        },
+        meta: { contributorIds },
+    }: PhotographCreated): PhotographViewModel {
+        const photograph = new PhotographViewModel();
+
+        photograph.name = buildMultilingualTextWithSingleItem(title, languageCodeForTitle);
+
+        photograph.id = photographId;
+
+        photograph.actions = [];
+
+        /**
+         * Note that this must be written in the DB by the event-handler, as
+         * we do not have access to the contributors in this scope.
+         */
+        photograph.contributions = [];
+
+        /**
+         * The contributor should have access.
+         */
+        photograph.accessControlList = new AccessControlList().allowUsers(contributorIds);
+
+        // photograph.notes = []; // there are no notes when the photograph is first created
+
+        // photograph.tags = []; // there are no tags with the photograph is first created
+
+        // set photograph.events here by applying the first event
+
+        photograph.isPublished = false;
+
+        photograph.actions = [
+            'PUBLISH_RESOURCE',
+            'TAG_RESOURCE',
+            'CONNECT_RESOURCES_WITH_NOTE',
+            'CREATE_NOTE_ABOUT_RESOURCE',
+        ];
+
+        return photograph;
+    }
+
+    static fromDto(dto: DTO<PhotographViewModel>): PhotographViewModel {
+        const photograph = new PhotographViewModel();
+
+        const { contributions, name, id, actions, accessControlList, mediaItemId, isPublished } =
+            dto;
+
+        photograph.contributions = Array.isArray(contributions) ? contributions : [];
+
+        photograph.name = name;
+
+        photograph.id = id;
+
+        photograph.actions = actions;
+
+        if (!isNullOrUndefined(mediaItemId)) {
+            photograph.mediaItemId = mediaItemId;
+        }
+
+        photograph.accessControlList = new AccessControlList(accessControlList);
+
+        photograph.actions = actions;
+
+        photograph.isPublished = isNullOrUndefined(isPublished) ? false : isPublished; // we want to be extra careful here
+
+        return photograph;
+    }
+
+    appendAction(action: string): PhotographViewModel {
+        this.actions.push(action);
+
+        return this;
+    }
+
+    appendActions(actions: string[]): PhotographViewModel {
+        for (const a of actions) {
+            this.actions.push(a);
+        }
+
+        return this;
+    }
+
+    apply(event: ICoscradEvent): PhotographViewModel {
+        if (
+            !event.isFor({
+                type: AggregateType.photograph,
+                id: this.id,
+            })
+        )
+            return this;
+
+        // there is no handler for this event
+        return this;
+    }
+
+    public getAvailableCommands() {
+        return this.actions;
+    }
+
+    public getCompositeIdentifier() {
+        return {
+            type: AggregateType.photograph,
+            id: this.id,
+        };
     }
 }
--- apps/api/src/queries/photograph/photograph-queries-fetch-by-id.e2e.spec.ts
+++ apps/api/src/queries/photograph/photograph-queries-fetch-by-id.e2e.spec.ts
@@ -1,18 +1,25 @@
-import { AggregateType, CoscradUserRole, LanguageCode } from '@coscrad/api-interfaces';
+import { AggregateType, IDetailQueryResult, LanguageCode } from '@coscrad/api-interfaces';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import httpStatusCodes from '../../app/constants/httpStatusCodes';
+import setUpIntegrationTest from '../../app/controllers/__tests__/setUpIntegrationTest';
 import getValidAggregateInstanceForTest from '../../domain/__tests__/utilities/getValidAggregateInstanceForTest';
 import buildDummyUuid from '../../domain/models/__tests__/utilities/buildDummyUuid';
 import { PhotographCreated } from '../../domain/models/photograph';
-import { AccessControlList } from '../../domain/models/shared/access-control/access-control-list.entity';
+import {
+    IPhotographQueryRepository,
+    PHOTOGRAPH_QUERY_REPOSITORY_TOKEN,
+} from '../../domain/models/photograph/queries';
 import { CoscradContributor } from '../../domain/models/user-management/contributor';
-import { CoscradUserGroup } from '../../domain/models/user-management/group/entities/coscrad-user-group.entity';
-import { CoscradUserWithGroups } from '../../domain/models/user-management/user/entities/user/coscrad-user-with-groups';
-import { CoscradUser } from '../../domain/models/user-management/user/entities/user/coscrad-user.entity';
 import { FullName } from '../../domain/models/user-management/user/entities/user/full-name.entity';
 import { AggregateId } from '../../domain/types/AggregateId';
 import { clonePlainObjectWithOverrides } from '../../lib/utilities/clonePlainObjectWithOverrides';
-import buildTestDataInFlatFormat from '../../test-data/buildTestDataInFlatFormat';
+import { ArangoDatabaseProvider } from '../../persistence/database/database.provider';
+import generateDatabaseNameForTestSuite from '../../persistence/repositories/__tests__/generateDatabaseNameForTestSuite';
+import TestRepositoryProvider from '../../persistence/repositories/__tests__/TestRepositoryProvider';
 import { TestEventStream } from '../../test-data/events';
 import { BaseResourceViewModel } from '../buildViewModelForResource/viewModels/base-resource.view-model';
+import { PhotographViewModel } from '../buildViewModelForResource/viewModels/photograph.view-model';
 
 // Set up endpoints: index endpoint, id endpoint
 const indexEndpoint = `/resources/photographs`;
@@ -20,19 +27,19 @@ const indexEndpoint = `/resources/photographs`;
 const buildDetailEndpoint = (id: AggregateId) => `${indexEndpoint}/${id}`;
 
 // We require an existing user for ACL tests
-const dummyQueryUserId = buildDummyUuid(4);
+// const dummyQueryUserId = buildDummyUuid(4);
 
-const { user: users, userGroup: userGroups } = buildTestDataInFlatFormat();
+// const { user: users, userGroup: userGroups } = buildTestDataInFlatFormat();
 
-const dummyUser = (users as CoscradUser[])[0].clone({
-    authProviderUserId: `auth0|${dummyQueryUserId}`,
-    id: dummyQueryUserId,
-    roles: [CoscradUserRole.viewer],
-});
+// const dummyUser = (users as CoscradUser[])[0].clone({
+//     authProviderUserId: `auth0|${dummyQueryUserId}`,
+//     id: dummyQueryUserId,
+//     roles: [CoscradUserRole.viewer],
+// });
 
-const dummyGroup = (userGroups as CoscradUserGroup[])[0].clone({ userIds: [dummyUser.id] });
+// const dummyGroup = (userGroups as CoscradUserGroup[])[0].clone({ userIds: [dummyUser.id] });
 
-const dummyUserWithGroups = new CoscradUserWithGroups(dummyUser, [dummyGroup]);
+// const dummyUserWithGroups = new CoscradUserWithGroups(dummyUser, [dummyGroup]);
 
 // Set up test data (use event sourcing to set up state)
 const photographTitle = `Photograph Title`;
@@ -45,6 +52,16 @@ const photographCompositeId = {
     type: AggregateType.photograph,
     id: photographId,
 };
+
+const existingMediaItem = getValidAggregateInstanceForTest(AggregateType.mediaItem).clone({
+    id: buildDummyUuid(55),
+});
+
+const testPhotographer = 'Jeff Thomas';
+
+const photograghHeightPx = 600;
+
+const photograghWidthPx = 1200;
 
 const dummyContributorFirstName = 'Dumb';
 
@@ -62,16 +79,21 @@ const photographCreated = new TestEventStream().andThen<PhotographCreated>({
     payload: {
         title: photographTitle,
         languageCodeForTitle: languageCodeForTitle,
-        mediaItemId: 
+        mediaItemId: existingMediaItem.id,
+        photographer: testPhotographer,
+        heightPx: photograghHeightPx,
+        widthPx: photograghWidthPx,
     },
     meta: {
         contributorIds: [dummyContributor.id],
     },
 });
 
-const dummyTerm = TermViewModel.fromTermCreated(photographCreated.as(photographCompositeId)[0] as TermCreated);
+const dummyPhotograph = PhotographViewModel.fromPhotographCreated(
+    photographCreated.as(photographCompositeId)[0] as PhotographCreated
+);
 
-const targetTermView = clonePlainObjectWithOverrides(dummyTerm, {
+const targetPhotographView = clonePlainObjectWithOverrides(dummyPhotograph, {
     isPublished: true,
     contributions: [
         {
@@ -81,13 +103,13 @@ const targetTermView = clonePlainObjectWithOverrides(dummyTerm, {
     ],
 });
 
-const privateTermThatUserCanAccess = clonePlainObjectWithOverrides(dummyTerm, {
-    accessControlList: new AccessControlList({
-        allowedUserIds: [dummyQueryUserId],
-        allowedGroupIds: [],
-    }),
-    isPublished: false,
-});
+// const privatePhotographThatUserCanAccess = clonePlainObjectWithOverrides(dummyPhotograph, {
+//     accessControlList: new AccessControlList({
+//         allowedUserIds: [dummyQueryUserId],
+//         allowedGroupIds: [],
+//     }),
+//     isPublished: false,
+// });
 
 // TODO Add happy path cases for a prompt photograph
 // const promptTermId = buildDummyUuid(2)
@@ -104,3 +126,93 @@ const assertResourceHasContributionFor = (
 
     expect(hasContribution).toBe(true);
 };
+
+describe(`when querying for a photograph: fetch by Id`, () => {
+    const testDatabaseName = generateDatabaseNameForTestSuite();
+
+    let app: INestApplication;
+
+    let testRepositoryProvider: TestRepositoryProvider;
+
+    let databaseProvider: ArangoDatabaseProvider;
+
+    let photographQueryRepository: IPhotographQueryRepository;
+
+    let seedPhotographs: (photographs: PhotographViewModel[]) => Promise<void>;
+
+    // let eventPublisher: ICoscradEventPublisher;
+    beforeEach(async () => {
+        await testRepositoryProvider.testSetup();
+
+        await databaseProvider.getDatabaseForCollection('photograph__VIEWS').clear();
+    });
+
+    afterAll(async () => {
+        await app.close();
+
+        databaseProvider.close();
+    });
+
+    describe(`when the user is unauthenticated`, () => {
+        beforeAll(async () => {
+            ({ app, testRepositoryProvider, databaseProvider } = await setUpIntegrationTest(
+                {
+                    ARANGO_DB_NAME: testDatabaseName,
+                }
+                // no authenticated user
+            ));
+
+            // eventPublisher = app.get(EVENT_PUBLISHER_TOKEN);
+
+            photographQueryRepository = app.get(PHOTOGRAPH_QUERY_REPOSITORY_TOKEN);
+
+            /**
+             * We need to use the proper publisher to make sure events only
+             * run if they match the pattern for the given handler.
+             */
+            seedPhotographs = async (photographs: PhotographViewModel[]) => {
+                await photographQueryRepository.createMany(photographs);
+            };
+        });
+
+        describe(`when there is a photograph with the given Id`, () => {
+            describe(`when a photograph is published`, () => {
+                beforeEach(async () => {
+                    /**
+                     * It is important that we do this before allowing the event
+                     * handlers to run, as they will only add contributions
+                     * if the corresponding contributor exists.
+                     */
+                    await testRepositoryProvider
+                        .getContributorRepository()
+                        .create(dummyContributor);
+
+                    await databaseProvider.getDatabaseForCollection('photograph__VIEWS').clear();
+
+                    await seedPhotographs([targetPhotographView]);
+                });
+
+                /**
+                 * TODO We need a contract test that will warn us if we potentially
+                 * break the client via breaking changes to the API.
+                 */
+                it('should return the expected result', async () => {
+                    const res = await request(app.getHttpServer()).get(
+                        buildDetailEndpoint(photographId)
+                    );
+
+                    expect(res.status).toBe(httpStatusCodes.ok);
+
+                    const result = res.body as IDetailQueryResult<PhotographViewModel>;
+
+                    expect(result.id).toBe(photographId);
+
+                    // We don't expose actions to non-admin users
+                    expect(result.actions).toEqual([]);
+
+                    assertResourceHasContributionFor(dummyContributor, result);
+                });
+            });
+        });
+    });
+});
