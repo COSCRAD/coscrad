36b638dc4f9e3e90e311bb5a10826c870201b557
yaanahuu2 <gahorner@gmail.com>
2025-02-10 20:55:12 -0800

    add photograph query service

--- libs/api-interfaces/src/lib/aggregate-views/view-models/resources/photograph.view-model.interface.ts
+++ libs/api-interfaces/src/lib/aggregate-views/view-models/resources/photograph.view-model.interface.ts
@@ -2,7 +2,7 @@ import { IBaseViewModel } from '../base.view-model.interface';
 
 export interface IPhotographViewModel extends IBaseViewModel {
     // Eventually, this should be a digital asset ID
-    imageUrl: string;
+    mediaItemId?: string;
 
     photographer: string;
 }
