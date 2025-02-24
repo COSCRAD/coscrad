9c1aa8070a8aea9bea82afb11ca00b69680c38b1
Aaron Plahn <aplahn85@gmail.com>
2025-02-24 14:56:27 -0800

    add unit test for CoscradDataExample decorator

--- apps/api/src/test-data/utilities/coscrad-data-example.ts
+++ apps/api/src/test-data/utilities/coscrad-data-example.ts
@@ -1,4 +1,4 @@
-import { isNonEmptyObject, isNullOrUndefined } from '@coscrad/validation-constraints';
+import { isNullOrUndefined } from '@coscrad/validation-constraints';
 import 'reflect-metadata';
 import { InternalError } from '../../lib/errors/InternalError';
 import { Ctor } from '../../lib/types/Ctor';
@@ -12,25 +12,34 @@ interface FromDto<T = unknown> {
 const isFromDto = <T = unknown>(input: unknown): input is FromDto<T> =>
     !isNullOrUndefined(input) && typeof (input as FromDto).fromDto === 'function';
 
-export const buildTestInstance = <T = unknown>(
-    Ctor: Ctor<T>,
-    overrides: Record<string, unknown>
-): T => {
-    const testMetadata = Reflect.getMetadata('SAMPLE', Ctor);
+export const getCoscradDataExamples = <T = unknown>(target: Ctor<T>): DTO<T>[] => {
+    const testMetadata = Reflect.getMetadata('SAMPLE', target);
 
-    if (!isNonEmptyObject(testMetadata)) {
-        throw new InternalError(`No metadata found for: ${Ctor}`);
+    if (isNullOrUndefined(testMetadata)) return [];
+
+    return testMetadata;
+};
+
+export const buildTestInstance = <T = unknown>(
+    target: Ctor<T>,
+    overrides: Partial<DTO<T>> = {}
+): T => {
+    const testMetadata = getCoscradDataExamples(target);
+
+    if (!Array.isArray(testMetadata) || testMetadata.length === 0) {
+        throw new InternalError(`No test data has been registered for: ${target}`);
     }
 
-    if (!isFromDto(Ctor)) {
+    if (!isFromDto(target)) {
         throw new InternalError(
             `In order to build a test instance of: ${
-                Object.getPrototypeOf(Ctor).name
+                Object.getPrototypeOf(target).name
             }, it must have a static factory method called "toDto"`
         );
     }
 
-    return Ctor.fromDto(clonePlainObjectWithOverrides(testMetadata, overrides)) as T;
+    // @ts-expect-error TODO fix the type issue with this utility
+    return target.fromDto(clonePlainObjectWithOverrides<DTO<T>>(testMetadata[0], overrides)) as T;
 };
 
 /**
@@ -39,7 +48,12 @@ export const buildTestInstance = <T = unknown>(
  * `OpenApi` schemas, including samples.
  */
 export const CoscradDataExample = ({ example }: { example: Object }): ClassDecorator => {
-    return function (target: Object) {
-        Reflect.defineMetadata('SAMPLE', example, target);
+    return function (target: any) {
+        const existingTestData = getCoscradDataExamples(target);
+
+        existingTestData.push(example);
+
+        // here we overwrite the original
+        Reflect.defineMetadata('SAMPLE', existingTestData, target);
     };
 };
