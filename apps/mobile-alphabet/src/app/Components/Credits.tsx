9cb7921915c11c950a2a1a62931373cad8dab95e
Justin Bambrick <jbambrick@tsilhqotin.ca>
2025-03-18 10:02:26 -0700

    feat(mobile-alphabet): support configurable credits screen

--- apps/mobile-alphabet/src/app/Components/Credits.tsx
+++ apps/mobile-alphabet/src/app/Components/Credits.tsx
@@ -1,12 +1,15 @@
-import { colors } from 'app/styles';
+import { creditsScreen } from 'app/styles';
 import { Text, View } from 'react-native';
 import Background from './Background';
+import config from './config.json';
 
 export function CreditsScreen() {
+    const contributions = config.credits;
+
     return (
         <Background>
-            <View>
-                <Text style={{ color: colors.text }}>Credits screen works!</Text>
+            <View style={[creditsScreen.page]}>
+                <Text style={[creditsScreen.text]}>{contributions}</Text>
             </View>
         </Background>
     );
