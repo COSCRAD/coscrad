71f272b263eb3f3ab6c4f0bb26f5b22d7a724ff2
Aaron Plahn <aplahn85@gmail.com>
2025-03-20 16:59:18 -0700

    refactor based on PR (#647)

--- apps/api/src/domain/models/term/commands/create-term/multilingual-text-auto-indexer.event-handler.ts
+++
@@ -1,7 +0,0 @@
-import { ICoscradEvent, ICoscradEventHandler } from '../../../../../domain/common';
-
-export class MultilingualTextAutoIndexer implements ICoscradEventHandler {
-    handle(event: ICoscradEvent): Promise<void> {
-        throw new Error('Method not implemented.');
-    }
-}
