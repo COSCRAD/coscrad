04c21072771c50ff8b0601b7385f19e7dfca9441
Aaron Plahn <aplahn85@gmail.com>
2025-03-20 19:15:27 -0700

    WIP implement tokenization for Chilcotin

---
+++ apps/api/src/domain/models/term/tokenization/tokenizer.interface.ts
@@ -0,0 +1,28 @@
+import { LanguageCode } from '@coscrad/api-interfaces';
+
+type Token = {
+    text: string;
+    languageCode: LanguageCode;
+    /**
+     * Note that if `isSpace` and `isPunct` are false, the `symbols` array will
+     * be a list of the atomic letters for the given alphabet, which may use
+     * multiple unicode symbols to indicate one letter.
+     */
+    symbols?: string[];
+    /**
+     * Eventually, we would like to move our NLP to spacy. We are staying
+     * close to their API for that reason.
+     */
+    isSpace: boolean;
+    isPunct: boolean;
+    isStop: boolean;
+};
+
+export interface ITokenizer {
+    /**
+     * Do we want this to be async in case we reach out to Spacy out of
+     * process in the future? Or will this be handled in a python
+     * event handler that receives publications from a messaging queue?
+     */
+    tokenize(document: string): Token[];
+}
