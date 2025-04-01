2a1ff60725a8237b04a25341a3042bb37f156664
yaanahuu2 <gahorner@gmail.com>
2025-04-01 10:16:46 -0700

    WIP photograph detail query test add notes test

--- apps/coscrad-frontend-e2e/src/e2e/resources/photographs/queries/photograph.detail.query-flow.e2e.cy.ts
+++ apps/coscrad-frontend-e2e/src/e2e/resources/photographs/queries/photograph.detail.query-flow.e2e.cy.ts
@@ -6,20 +6,34 @@ const aggregateCompositeIdentifier = buildDummyAggregateCompositeIdentifier(
     2
 );
 
+const aggregateCompositeIdentifierNoNotes = buildDummyAggregateCompositeIdentifier(
+    AggregateType.photograph,
+    3
+);
+
 const mediaItemCompositeIdentifier = buildDummyAggregateCompositeIdentifier(
     AggregateType.mediaItem,
     12
+);
+
+const mediaItemCompositeIdentifierNoNotes = buildDummyAggregateCompositeIdentifier(
+    AggregateType.mediaItem,
+    13
 );
 
 const buildRoute = (id: string) => `/Resources/Photographs/${id}`;
 
 const photographTitle = 'My Photo';
 
+const photographTitleNoNotes = 'My Photo without any notes';
+
 const dummyPhotographer = 'Slank Mortimer';
 
 const languageCodeForTitle = LanguageCode.Haida;
 
 const validPhotographDetailRoute = buildRoute(aggregateCompositeIdentifier.id);
+
+const noteText = 'This is a note about a photograph';
 
 describe(`the photograph detail page`, () => {
     describe(`when the photograph with the given ID 123 does not exist`, () => {
@@ -32,53 +46,112 @@ describe(`the photograph detail page`, () => {
         });
     });
 
+    before(() => {
+        cy.clearDatabase();
+
+        cy.seedTestUuids(200);
+
+        cy.seedDataWithCommand(`CREATE_MEDIA_ITEM`, {
+            aggregateCompositeIdentifier: mediaItemCompositeIdentifier,
+            title: 'My photo of Masset',
+            mimeType: MIMEType.jpg,
+            // Override default value to "remove" property from fsa for image media item
+            lengthMilliseconds: null,
+        });
+
+        cy.seedDataWithCommand(`PUBLISH_RESOURCE`, {
+            aggregateCompositeIdentifier: mediaItemCompositeIdentifier,
+        });
+
+        cy.seedDataWithCommand(`CREATE_PHOTOGRAPH`, {
+            aggregateCompositeIdentifier,
+            title: photographTitle,
+            languageCodeForTitle,
+            mediaItemId: mediaItemCompositeIdentifier.id,
+            photographer: dummyPhotographer,
+            heightPx: 800,
+            widthPx: 200,
+        });
+
+        cy.seedDataWithCommand(`PUBLISH_RESOURCE`, {
+            aggregateCompositeIdentifier,
+        });
+
+        cy.seedDataWithCommand(`CREATE_NOTE_ABOUT_RESOURCE`, {
+            aggregateCompositeIdentifier: buildDummyAggregateCompositeIdentifier(
+                AggregateType.note,
+                15
+            ),
+            resourceCompositeIdentifier: aggregateCompositeIdentifier,
+            text: noteText,
+        });
+
+        cy.seedDataWithCommand(`CREATE_MEDIA_ITEM`, {
+            aggregateCompositeIdentifier: mediaItemCompositeIdentifierNoNotes,
+            title: 'My photo of Skidegate with no notes',
+            mimeType: MIMEType.jpg,
+            // Override default value to "remove" property from fsa for image media item
+            lengthMilliseconds: null,
+        });
+
+        cy.seedDataWithCommand(`PUBLISH_RESOURCE`, {
+            aggregateCompositeIdentifier: mediaItemCompositeIdentifierNoNotes,
+        });
+
+        cy.seedDataWithCommand(`CREATE_PHOTOGRAPH`, {
+            aggregateCompositeIdentifier: aggregateCompositeIdentifierNoNotes,
+            title: photographTitleNoNotes,
+            languageCodeForTitle,
+            mediaItemId: mediaItemCompositeIdentifierNoNotes.id,
+            photographer: dummyPhotographer,
+            heightPx: 800,
+            widthPx: 200,
+        });
+
+        cy.seedDataWithCommand(`PUBLISH_RESOURCE`, {
+            aggregateCompositeIdentifier: aggregateCompositeIdentifierNoNotes,
+        });
+    });
+
     describe(`when the photograph exists`, () => {
         beforeEach(() => {
             cy.visit(validPhotographDetailRoute);
         });
 
-        describe(`when the photograph exists`, () => {
-            before(() => {
-                cy.clearDatabase();
+        it(`should contain the photograph name`, () => {
+            cy.contains(photographTitle);
 
-                cy.seedTestUuids(200);
+            cy.contains(dummyPhotographer);
 
-                cy.seedDataWithCommand(`CREATE_MEDIA_ITEM`, {
-                    aggregateCompositeIdentifier: mediaItemCompositeIdentifier,
-                    title: 'My photo of Masset',
-                    mimeType: MIMEType.jpg,
-                    // Override default value to "remove" property from fsa for image media item
-                    lengthMilliseconds: null,
-                });
+            cy.getByDataAttribute('LanguageIcon').first().trigger('mouseover');
 
-                cy.seedDataWithCommand(`PUBLISH_RESOURCE`, {
-                    aggregateCompositeIdentifier: mediaItemCompositeIdentifier,
-                });
+            cy.contains("Haida, 'original'");
+        });
+    });
 
-                cy.seedDataWithCommand(`CREATE_PHOTOGRAPH`, {
-                    aggregateCompositeIdentifier,
-                    title: photographTitle,
-                    languageCodeForTitle,
-                    mediaItemId: mediaItemCompositeIdentifier.id,
-                    photographer: dummyPhotographer,
-                    heightPx: 800,
-                    widthPx: 200,
-                });
+    describe('when there are notes for the Photograph (2)', () => {
+        beforeEach(() => {
+            cy.visit(validPhotographDetailRoute);
+        });
 
-                cy.seedDataWithCommand(`PUBLISH_RESOURCE`, {
-                    aggregateCompositeIdentifier,
-                });
-            });
+        it(`should display the note text:\n${noteText}`, () => {
+            cy.openPanel('notes');
 
-            it(`should contain the photograph name`, () => {
-                cy.contains(photographTitle);
+            cy.contains(noteText);
+        });
+    });
 
-                cy.contains(dummyPhotographer);
+    describe('when there are no notes for the Photograph (3)', () => {
+        beforeEach(() => {
+            cy.visit(`/Resources/Photographs/${aggregateCompositeIdentifierNoNotes.id}`);
+        });
 
-                cy.getByDataAttribute('LanguageIcon').first().trigger('mouseover');
+        it(`should display the no notes message`, () => {
+            cy.contains(photographTitleNoNotes);
 
-                cy.contains("Haida, 'original'");
-            });
+            cy.openPanel('notes');
+
+            cy.contains('No Notes Found');
         });
     });
 });
