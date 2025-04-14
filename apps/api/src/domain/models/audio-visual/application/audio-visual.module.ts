aa0fa9be285441c696147e19a08f489c70d11425
Blake Sellars <blakesellars02@gmail.com>
2025-04-14 14:52:57 -0700

    add handler to module

--- apps/api/src/domain/models/audio-visual/application/audio-visual.module.ts
+++ apps/api/src/domain/models/audio-visual/application/audio-visual.module.ts
@@ -23,6 +23,7 @@ import {
 } from '../audio-item/commands';
 import { AudioItemCreated } from '../audio-item/commands/create-audio-item/audio-item-created.event';
 import { AudioItemCreatedEventHandler } from '../audio-item/commands/create-audio-item/audio-item-created.event-handler';
+import { AudioItemNameTranslated } from '../audio-item/commands/translate-audio-item-name/audio-item-name-translated-event';
 import {
     ImportLineItemsToTranscript,
     ImportLineItemsToTranscriptCommandHandler,
@@ -64,7 +65,7 @@ import { VideoController } from './video.controller';
         ImportTranslationsForTranscript,
         ImportTranslationsForTranscriptCommandHandler,
         // events
-        ...[AudioItemCreated].map((ctor) => ({
+        ...[AudioItemCreated, AudioItemNameTranslated].map((ctor) => ({
             provide: ctor,
             useValue: ctor,
         })),
