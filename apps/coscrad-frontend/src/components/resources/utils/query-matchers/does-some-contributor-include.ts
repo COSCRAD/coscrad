54e351de6605fba2de7f49e9e9ce4a29b2d8b9a5
yaanahuu2 <gahorner@gmail.com>
2025-04-29 17:09:51 -0700

    WIP introduce doesSomeContributorInclude matcher

---
+++ apps/coscrad-frontend/src/components/resources/utils/query-matchers/does-some-contributor-include.ts
@@ -0,0 +1,21 @@
+import { ContributorWithId } from '@coscrad/api-interfaces';
+import { isNullOrUndefined } from '@coscrad/validation-constraints';
+import { parseLanguageCode } from './parse-language-code-from-query';
+
+export const doesSomeContributorInclude = (
+    contributiorsWithIds: ContributorWithId[],
+    query: string
+) => {
+    const languageCodeInQuery = parseLanguageCode(query);
+
+    const searchTerms = isNullOrUndefined(languageCodeInQuery)
+        ? query
+        : query.split(`{${languageCodeInQuery}}:`)[1];
+
+    if (isNullOrUndefined(searchTerms)) return undefined;
+
+    return contributiorsWithIds
+        .map(({ fullName }) => fullName.toLowerCase())
+        .join(' ')
+        .includes(searchTerms.toLowerCase());
+};
