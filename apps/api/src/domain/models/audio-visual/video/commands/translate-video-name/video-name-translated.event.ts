4bc6f998ef3d50825aba63d0d32aa4a6dd975b59
Blake Sellars <blakesellars02@gmail.com>
2025-05-20 09:43:15 -0700

    feat: introduce event consumer for VIDEO_NAME_TRANSLATED

--- apps/api/src/domain/models/audio-visual/video/commands/translate-video-name/video-name-translated.event.ts
+++ apps/api/src/domain/models/audio-visual/video/commands/translate-video-name/video-name-translated.event.ts
@@ -1,10 +1,41 @@
+import { AggregateType, LanguageCode } from '@coscrad/api-interfaces';
 import { CoscradEvent } from '../../../../../../domain/common';
+import { CoscradDataExample } from '../../../../../../test-data/utilities';
+import { DTO } from '../../../../../../types/DTO';
+import buildDummyUuid from '../../../../__tests__/utilities/buildDummyUuid';
+import { dummyDateNow } from '../../../../__tests__/utilities/dummyDateNow';
 import { BaseEvent } from '../../../../shared/events/base-event.entity';
 import { TranslateVideoName } from './translate-video-name.command';
 
 export type VideoNameTranslatedPayload = TranslateVideoName;
 
+const testEventId = buildDummyUuid(4);
+
+@CoscradDataExample<VideoNameTranslated>({
+    example: {
+        id: testEventId,
+        type: 'VIDEO_NAME_TRANSLATED',
+        payload: {
+            aggregateCompositeIdentifier: {
+                type: AggregateType.video,
+                id: buildDummyUuid(3),
+            },
+            text: 'text of translation for video name',
+            languageCode: LanguageCode.English,
+        },
+        meta: {
+            id: testEventId,
+            userId: buildDummyUuid(2),
+            contributorIds: [],
+            dateCreated: dummyDateNow,
+        },
+    },
+})
 @CoscradEvent('VIDEO_NAME_TRANSLATED')
 export class VideoNameTranslated extends BaseEvent<VideoNameTranslatedPayload> {
     readonly type = 'VIDEO_NAME_TRANSLATED';
+
+    public static fromDto(dto: DTO<VideoNameTranslated>) {
+        return new VideoNameTranslated(dto.payload, dto.meta);
+    }
 }
