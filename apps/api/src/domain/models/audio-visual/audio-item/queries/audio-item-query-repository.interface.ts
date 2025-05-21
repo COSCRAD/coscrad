a0c2df43c03527a52cdd7b1ca5965bfd14af47a9
Aaron Plahn <aplahn85@gmail.com>
2025-05-21 13:40:00 -0700

    refactor: leverage base transcript query builder in audio item query repo

--- apps/api/src/domain/models/audio-visual/audio-item/queries/audio-item-query-repository.interface.ts
+++ apps/api/src/domain/models/audio-visual/audio-item/queries/audio-item-query-repository.interface.ts
@@ -3,9 +3,7 @@ import { Maybe } from '../../../../../lib/types/maybe';
 import { AggregateId } from '../../../../types/AggregateId';
 import { IAccessible } from '../../../shared/common-commands/grant-resource-read-access-to-user/resource-read-access-granted-to-user.event-handler';
 import { IPublishable } from '../../../shared/common-commands/publish-resource/resource-published.event-handler';
-import { TranscriptLineItemDto } from '../../shared/commands/transcripts/import-line-items-to-transcript';
-import { TranscriptParticipant } from '../../shared/entities/transcript-participant';
-import { TranslationItem } from '../commands';
+import { ITranscriptRepository } from '../../shared/queries/transcript-repository.interface';
 import { EventSourcedAudioItemViewModel } from './audio-item.view-model.event-sourced';
 
 export const AUDIO_QUERY_REPOSITORY_TOKEN = 'AUDIO_QUERY_REPOSITORY_TOKEN';
@@ -17,7 +15,10 @@ export interface TranslationLineItemDto {
     languageCode: LanguageCode;
 }
 
-export interface IAudioItemQueryRepository extends IPublishable, IAccessible {
+export interface IAudioItemQueryRepository
+    extends IPublishable,
+        IAccessible,
+        ITranscriptRepository {
     create(view: EventSourcedAudioItemViewModel): Promise<void>;
 
     createMany(view: EventSourcedAudioItemViewModel[]): Promise<void>;
@@ -31,20 +32,4 @@ export interface IAudioItemQueryRepository extends IPublishable, IAccessible {
     translateName(id: AggregateId, translationItem: IMultilingualTextItem): Promise<void>;
 
     count(): Promise<number>;
-
-    createTranscript(id: AggregateId): Promise<void>;
-
-    addParticipant(id: AggregateId, participant: TranscriptParticipant): Promise<void>;
-
-    // should this be the DTO type?
-    addLineItem(id: AggregateId, lineItem: TranscriptLineItemDto): Promise<void>;
-
-    importLineItems(id: AggregateId, lineItems: TranscriptLineItemDto[]): Promise<void>;
-
-    translateLineItem(id: AggregateId, lineItem: TranslationLineItemDto): Promise<void>;
-
-    importTranslationsForTranscript(
-        id: AggregateId,
-        translations: TranslationItem[]
-    ): Promise<void>;
 }
