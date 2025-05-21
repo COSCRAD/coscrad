d3b62afa03f3ad079ffc66276cd9774dd56f9af6
Blake Sellars <blakesellars02@gmail.com>
2025-05-21 16:21:24 -0700

    WIP: test count in arango song query repo

--- apps/api/src/domain/models/song/queries/song-query-repository.interface.ts
+++ apps/api/src/domain/models/song/queries/song-query-repository.interface.ts
@@ -6,12 +6,18 @@ export const SONG_QUERY_REPOSITORY_TOKEN = 'SONG_QUERY_REPOSITORY_TOKEN';
 
 export interface ISongQueryRepository {
     create(view: EventSourcedSongViewModel): Promise<void>;
-    // createMany(view: EventSourcedAudioItemViewModel[]): Promise<void>;
+
+    createMany(view: EventSourcedSongViewModel[]): Promise<void>;
+
     // delete(id: AggregateId): Promise<void>;
+
     fetchById(id: AggregateId): Promise<Maybe<EventSourcedSongViewModel>>;
+
     // fetchMany(): Promise<EventSourcedAudioItemViewModel[]>;
+
     // translateName(id: AggregateId, translationItem: IMultilingualTextItem): Promise<void>;
-    // count(): Promise<number>;
+
+    count(): Promise<number>;
 
     // addLyrics(): Promise<void>;
 
