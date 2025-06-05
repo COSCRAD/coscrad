e702f4613b564d1911bdbaddf71e0fcef0229f9e
Aaron Plahn <aplahn85@gmail.com>
2025-06-05 15:15:52 -0700

    remove audio URL mapping layer from song slice in Redux

--- apps/coscrad-frontend/src/store/slices/resources/songs/thunks/fetch-songs.ts
+++ apps/coscrad-frontend/src/store/slices/resources/songs/thunks/fetch-songs.ts
@@ -1,6 +1,3 @@
-import { IIndexQueryResult, ISongViewModel, WithTags } from '@coscrad/api-interfaces';
-import { isNullOrUndefined } from '@coscrad/validation-constraints';
-import { getConfig } from '../../../../../config';
 import { buildResourceFetchActionPrefix } from '../../../utils/build-resource-fetch-action-prefix';
 import { createFetchThunk } from '../../../utils/create-fetch-thunk';
 import { getApiResourcesBaseRoute } from '../../shared';
@@ -9,26 +6,5 @@ import { SongIndexState } from '../types';
 
 export const fetchSongs = createFetchThunk<SongIndexState>(
     buildResourceFetchActionPrefix(SONGS),
-    `${getApiResourcesBaseRoute()}/songs`,
-    (
-        serverResponse: IIndexQueryResult<WithTags<ISongViewModel>>
-    ): IIndexQueryResult<WithTags<ISongViewModel>> => {
-        const { apiUrl } = getConfig();
-
-        /**
-         * TODO Phase the following mapping layer out in favour
-         * of doing this work on the server.
-         */
-        return {
-            ...serverResponse,
-            entities: serverResponse.entities.map((entity) => {
-                return {
-                    ...entity,
-                    audioURL: isNullOrUndefined(entity.audioURL)
-                        ? undefined
-                        : `${apiUrl}${entity.audioURL}`,
-                };
-            }),
-        };
-    }
+    `${getApiResourcesBaseRoute()}/songs`
 );
