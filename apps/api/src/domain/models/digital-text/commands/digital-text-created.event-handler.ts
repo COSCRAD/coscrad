e74b1ecec5c07b31c46c528632cbbaffd7730226
Aaron Plahn <aplahn85@gmail.com>
2025-06-23 10:31:43 -0700

    register DigitalTextCreatedEventHandler

--- apps/api/src/domain/models/digital-text/commands/digital-text-created.event-handler.ts
+++ apps/api/src/domain/models/digital-text/commands/digital-text-created.event-handler.ts
@@ -1,12 +1,13 @@
 import { Inject } from '@nestjs/common';
 import { DigitalTextViewModel } from '../../../../queries/digital-text';
-import { ICoscradEventHandler } from '../../../common';
+import { CoscradEventConsumer, ICoscradEventHandler } from '../../../common';
 import {
     DIGITAL_TEXT_QUERY_REPOSITORY_PROVIDER_TOKEN,
     IDigitalTextQueryRepository,
 } from '../queries/digital-text-query-repository.interface';
 import { DigitalTextCreated } from './digital-text-created.event';
 
+@CoscradEventConsumer('DIGITAL_TEXT_CREATED')
 export class DigitalTextCreatedEventHandler implements ICoscradEventHandler {
     constructor(
         @Inject(DIGITAL_TEXT_QUERY_REPOSITORY_PROVIDER_TOKEN)
