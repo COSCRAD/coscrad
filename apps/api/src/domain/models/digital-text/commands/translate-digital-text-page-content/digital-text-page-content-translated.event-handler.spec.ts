d5b24e067102b4bfc8a694b2ea7732f184f5199d
Blake Sellars <blakesellars02@gmail.com>
2025-07-03 14:29:45 -0700

    WIP: add test for digital text page content translated

---
+++ apps/api/src/domain/models/digital-text/commands/translate-digital-text-page-content/digital-text-page-content-translated.event-handler.spec.ts
@@ -0,0 +1,106 @@
+import { LanguageCode } from '@coscrad/api-interfaces';
+import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Test } from '@nestjs/testing';
+import buildMockConfigService from '../../../../../app/config/__tests__/utilities/buildMockConfigService';
+import buildConfigFilePath from '../../../../../app/config/buildConfigFilePath';
+import { Environment } from '../../../../../app/config/constants/environment';
+import { buildMultilingualTextWithSingleItem } from '../../../../../domain/common/build-multilingual-text-with-single-item';
+import { ArangoConnectionProvider } from '../../../../../persistence/database/arango-connection.provider';
+import { ArangoDatabaseProvider } from '../../../../../persistence/database/database.provider';
+import { PersistenceModule } from '../../../../../persistence/persistence.module';
+import generateDatabaseNameForTestSuite from '../../../../../persistence/repositories/__tests__/generateDatabaseNameForTestSuite';
+import { DigitalTextViewModel } from '../../../../../queries/digital-text';
+import { buildTestInstance } from '../../../../../test-data/utilities';
+import buildDummyUuid from '../../../__tests__/utilities/buildDummyUuid';
+import { ArangoDigitalTextQueryRepository } from '../../queries/arango-digital-text-query-repository';
+import { IDigitalTextQueryRepository } from '../../queries/digital-text-query-repository.interface';
+import { DigitalTextPageContentTranslated } from './digital-text-page-content-translated.event';
+import { DigitalTextPageContentTranslatedEventHandler } from './digital-text-page-content-translated.event-handler';
+
+const originalLanguageCode = LanguageCode.Chilcotin;
+
+const digitalTextId = buildDummyUuid(21);
+
+const translationLanguageCode = LanguageCode.English;
+
+const translatedContent = 'page content translated';
+
+const pageIdentifier = 'XV';
+
+const existingDigitalTextView = buildTestInstance(DigitalTextViewModel, {
+    id: digitalTextId,
+    name: buildMultilingualTextWithSingleItem('digital text page content', originalLanguageCode),
+});
+
+const digitalTextPageContentTranslated = buildTestInstance(DigitalTextPageContentTranslated, {
+    payload: {
+        aggregateCompositeIdentifier: { id: digitalTextId },
+        languageCode: translationLanguageCode,
+        translation: translatedContent,
+        pageIdentifier,
+    },
+});
+
+describe(`DigitalTextPageContentTranslated`, () => {
+    let testQueryRepository: IDigitalTextQueryRepository;
+
+    let databaseProvider: ArangoDatabaseProvider;
+
+    let app: INestApplication;
+
+    let digitalTextPageContentTranslatedEventHandler: DigitalTextPageContentTranslatedEventHandler;
+
+    beforeAll(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [PersistenceModule.forRootAsync()],
+        })
+            .overrideProvider(ConfigService)
+            .useValue(
+                buildMockConfigService(
+                    {
+                        ARANGO_DB_NAME: generateDatabaseNameForTestSuite(),
+                    },
+                    buildConfigFilePath(Environment.test)
+                )
+            )
+            .compile();
+
+        await moduleRef.init();
+
+        app = moduleRef.createNestApplication();
+
+        const connectionProvider = app.get(ArangoConnectionProvider);
+
+        databaseProvider = new ArangoDatabaseProvider(connectionProvider);
+
+        testQueryRepository = new ArangoDigitalTextQueryRepository(connectionProvider);
+
+        digitalTextPageContentTranslatedEventHandler =
+            new DigitalTextPageContentTranslatedEventHandler(testQueryRepository);
+    });
+
+    afterAll(async () => {
+        databaseProvider.close();
+    });
+
+    beforeEach(async () => {
+        await databaseProvider.clearViews();
+
+        await testQueryRepository.create(existingDigitalTextView);
+    });
+
+    describe(`when translating page content for digital text`, () => {
+        it('should tranlate the page content', async () => {
+            await digitalTextPageContentTranslatedEventHandler.handle(
+                digitalTextPageContentTranslated
+            );
+
+            const updatedView = (await testQueryRepository.fetchById(
+                digitalTextId
+            )) as DigitalTextViewModel;
+
+            expect(updatedView).toHaveLength(1);
+        });
+    });
+});
---
+++ apps/api/src/domain/models/digital-text/commands/translate-digital-text-page-content/digital-text-page-content-translated.event-handler.ts
@@ -0,0 +1,31 @@
+import { Inject } from '@nestjs/common';
+import { CoscradEventConsumer, ICoscradEventHandler } from '../../../../../domain/common';
+import {
+    DIGITAL_TEXT_QUERY_REPOSITORY_PROVIDER_TOKEN,
+    IDigitalTextQueryRepository,
+} from '../../queries/digital-text-query-repository.interface';
+import { DigitalTextPageContentTranslated } from './digital-text-page-content-translated.event';
+
+@CoscradEventConsumer('DIGITAL_TEXT_PAGE_CONTENT_TRANSLATED')
+export class DigitalTextPageContentTranslatedEventHandler implements ICoscradEventHandler {
+    constructor(
+        @Inject(DIGITAL_TEXT_QUERY_REPOSITORY_PROVIDER_TOKEN)
+        private readonly queryRepository: IDigitalTextQueryRepository
+    ) {}
+
+    async handle({
+        payload: {
+            translation,
+            languageCode,
+            aggregateCompositeIdentifier: { id: digitalTextId },
+            pageIdentifier,
+        },
+    }: DigitalTextPageContentTranslated): Promise<void> {
+        await this.queryRepository.translatePageContent(
+            digitalTextId,
+            translation,
+            pageIdentifier,
+            languageCode
+        );
+    }
+}
