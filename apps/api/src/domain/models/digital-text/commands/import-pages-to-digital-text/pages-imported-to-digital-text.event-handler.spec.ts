ad4a35e709ab3456fb118926f842558816a341f6
Aaron Plahn <aplahn85@gmail.com>
2025-07-15 13:42:46 -0700

    fix test setup and add more test coverage to import pages in query repo

--- apps/api/src/domain/models/digital-text/commands/import-pages-to-digital-text/pages-imported-to-digital-text.event-handler.spec.ts
+++ apps/api/src/domain/models/digital-text/commands/import-pages-to-digital-text/pages-imported-to-digital-text.event-handler.spec.ts
@@ -21,6 +21,7 @@ import { PagesImportedToDigitalTextEventHandler } from './pages-imported-to-digi
 const digitalTextId = buildDummyUuid(3);
 
 const existingDigitalTextView = buildTestInstance(DigitalTextViewModel, {
+    id: digitalTextId,
     pages: [],
 });
 
@@ -109,23 +110,50 @@ describe('PagesImportedToDigitalTextEventHandler', () => {
     });
 
     describe(`when the target digital text exists`, () => {
-        it(`should import the pages`, async () => {
-            await pagesImportedEventHandler.handle(pagesImported);
+        describe(`when there are no pages to begin with`, () => {
+            it(`should import the pages`, async () => {
+                await pagesImportedEventHandler.handle(pagesImported);
 
-            const updatedView = (await testQueryRepository.fetchById(
-                digitalTextId
-            )) as DigitalTextViewModel;
+                const updatedView = (await testQueryRepository.fetchById(
+                    digitalTextId
+                )) as DigitalTextViewModel;
 
-            const pageToCheckInDetail = updatedView.pages.find(
-                ({ identifier }) => identifier === targetPageIdentifier
-            );
+                const pageToCheckInDetail = updatedView.pages.find(
+                    ({ identifier }) => identifier === targetPageIdentifier
+                );
 
-            const { languageCode: foundOriginalLanguageCode, text: foundOriginalText } =
-                pageToCheckInDetail.content.getOriginalTextItem();
+                const { languageCode: foundOriginalLanguageCode, text: foundOriginalText } =
+                    pageToCheckInDetail.content.getOriginalTextItem();
 
-            expect(foundOriginalLanguageCode).toBe(originalLanguageCode);
+                expect(foundOriginalLanguageCode).toBe(originalLanguageCode);
 
-            expect(foundOriginalText).toBe(originalTextContentForTargetPage);
+                expect(foundOriginalText).toBe(originalTextContentForTargetPage);
+            });
+        });
+
+        describe(`when there are already pages`, () => {
+            /**
+             * This would be a system error. Our command validation flow
+             * prevents importing pages to a non-empty digital text.
+             */
+            it.todo(`should not update the existing view`);
+        });
+    });
+
+    describe(`when the target digital text does not exist`, () => {
+        it(`should fail gracefully`, async () => {
+            const tryIt = () =>
+                pagesImportedEventHandler.handle(
+                    buildTestInstance(PagesImportedToDigitalText, {
+                        payload: {
+                            aggregateCompositeIdentifier: { id: buildDummyUuid(404) },
+                        },
+                    })
+                );
+
+            expect(tryIt).resolves;
+
+            // TODO assert correct message is logged
         });
     });
 });
