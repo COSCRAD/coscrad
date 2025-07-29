c1f066b0e3ec0512f192ac6f39f5c6d562881d4b
Aaron Plahn <aplahn85@gmail.com>
2025-07-29 10:13:55 -0700

    update stale snapshots and remove type prop from vocabulary list entriy views

--- apps/api/src/queries/buildViewModelForResource/viewModels/term.view-model.ts
+++ apps/api/src/queries/buildViewModelForResource/viewModels/term.view-model.ts
@@ -36,8 +36,6 @@ import { NoteRecordForResourceViewModel } from './note-record-for-resource.view-
 import { EventSourcedTagViewModel } from './tag.view-model.event-sourced';
 
 export class VocabularyListRecordForTerm {
-    readonly type = ResourceType.term;
-
     @UUID({
         label: 'vocabulary list ID',
         description: 'system identifier for this vocabulary list',
