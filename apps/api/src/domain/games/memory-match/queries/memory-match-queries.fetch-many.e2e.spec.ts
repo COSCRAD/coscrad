1da3caa07dbacf0a3a4ec43305e10cdb8742b569
Aaron Plahn <aplahn85@gmail.com>
2025-09-08 11:14:35 -0700

    close app in after all

--- apps/api/src/domain/games/memory-match/queries/memory-match-queries.fetch-many.e2e.spec.ts
+++ apps/api/src/domain/games/memory-match/queries/memory-match-queries.fetch-many.e2e.spec.ts
@@ -115,6 +115,8 @@ describe(`when querying for a memory match round: fetch many`, () => {
         await app.close();
 
         databaseProvider.close();
+
+        await app.close();
     });
 
     /**
