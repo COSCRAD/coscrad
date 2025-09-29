c0852ee28619c46a8df137ce2ec9e6f47b4d390f
Aaron Plahn <aplahn85@gmail.com>
2025-09-29 14:57:54 -0700

    reword comment to test CI again- 2

--- apps/api/src/domain/games/memory-match/queries/memory-match-queries.fetch-many.e2e.spec.ts
+++ apps/api/src/domain/games/memory-match/queries/memory-match-queries.fetch-many.e2e.spec.ts
@@ -56,7 +56,7 @@ const unpublishedRound = buildTestInstance(MemoryMatchRound, {
     cards: [],
 });
 
-// TODO we need to opt-back in once we diagnose why this fails on the CI but not locally
+// TODO we will want to opt-back in once we diagnose why this fails on the CI but not locally
 describe.skip(`when querying for a memory match round: fetch many`, () => {
     let app: INestApplication;
 
