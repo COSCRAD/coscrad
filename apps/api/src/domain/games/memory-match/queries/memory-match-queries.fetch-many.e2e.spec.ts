f65e3579f4512c0e03092d4799c3c1bf746a38a3
Aaron Plahn <aplahn85@gmail.com>
2025-10-07 11:31:39 -0700

    mock unnecessary dependencies

--- apps/api/src/domain/games/memory-match/queries/memory-match-queries.fetch-many.e2e.spec.ts
+++ apps/api/src/domain/games/memory-match/queries/memory-match-queries.fetch-many.e2e.spec.ts
@@ -15,6 +15,7 @@ import { ArangoDatabaseProvider } from '../../../../persistence/database/databas
 import { PersistenceModule } from '../../../../persistence/persistence.module';
 import generateDatabaseNameForTestSuite from '../../../../persistence/repositories/__tests__/generateDatabaseNameForTestSuite';
 import { buildTestInstance } from '../../../../test-data/utilities';
+import { DynamicDataTypeFinderService } from '../../../../validation';
 import { buildMultilingualTextWithSingleItem } from '../../../common/build-multilingual-text-with-single-item';
 import buildDummyUuid from '../../../models/__tests__/utilities/buildDummyUuid';
 import { CoscradUserWithGroups } from '../../../models/user-management/user/entities/user/coscrad-user-with-groups';
@@ -56,6 +57,12 @@ const unpublishedRound = buildTestInstance(MemoryMatchRound, {
     cards: [],
 });
 
+const mockDynamicDataTypeFinderService = {
+    async bootstrapDynamicTypes() {
+        return Promise.resolve();
+    },
+};
+
 describe(`when querying for a memory match round: fetch many`, () => {
     let app: INestApplication;
 
@@ -92,6 +99,8 @@ describe(`when querying for a memory match round: fetch many`, () => {
             .useValue(new MockJwtAdminAuthGuard(testUserWithGroups))
             .overrideProvider(ConfigService)
             .useValue(mockConfigService)
+            .overrideProvider(DynamicDataTypeFinderService)
+            .useValue(mockDynamicDataTypeFinderService)
             .compile();
 
         app = testModuleRef.createNestApplication();
