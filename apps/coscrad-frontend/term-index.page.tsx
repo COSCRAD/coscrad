661535cb235e1ef21bdb5481dcbe19650c7e22b5
Aaron Plahn <aplahn85@gmail.com>
2026-01-09 15:50:11 -0800

    fix: use property labels for term index search scopes

--- apps/coscrad-frontend/term-index.page.tsx
+++ apps/coscrad-frontend/term-index.page.tsx
@@ -1,17 +1,25 @@
 import { Box, Stack } from '@mui/material';
 
+import { ITermViewModel } from '@coscrad/api-interfaces';
 import { TermPaginator } from './src/components/resources/terms/term-paginator';
 import { TermSearchBar } from './src/components/resources/terms/term-search-bar';
+import { HeadingLabel } from './src/utils/generic-components/presenters/tables';
 import { TermListContainer } from './term-list.container';
+
+// TODO share this with the `HeadingLabels`
+const searchableProps: HeadingLabel<ITermViewModel>[] = [
+    { propertyKey: 'name', headingLabel: 'Term' },
+    { propertyKey: 'contributions', headingLabel: 'Contributors' },
+    { propertyKey: 'vocabularyLists', headingLabel: 'Vocabulary Lists' },
+    { propertyKey: 'tokens', headingLabel: 'Letters' },
+];
 
 export const TermIndexPage = (): JSX.Element => {
     return (
         <div>
             <Stack>
                 <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                    <TermSearchBar
-                        scopes={['name', 'contributions', 'vocabularyLists', 'tokens']}
-                    />
+                    <TermSearchBar scopes={searchableProps} />
                 </Box>
                 <Box>
                     <TermListContainer />
