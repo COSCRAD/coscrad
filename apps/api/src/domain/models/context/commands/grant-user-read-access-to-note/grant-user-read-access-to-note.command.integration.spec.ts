eea42745b59e6b8a4360a3a7d7fc04c8b69633a9
Aaron Plahn <aplahn85@gmail.com>
2026-02-06 10:38:25 -0800

    WIP finalize integration test for GRANT_USER_READ_ACCESS_TO_NOTE

--- apps/api/src/domain/models/context/commands/grant-user-read-access-to-note/grant-user-read-access-to-note.command.integration.spec.ts
+++ apps/api/src/domain/models/context/commands/grant-user-read-access-to-note/grant-user-read-access-to-note.command.integration.spec.ts
@@ -5,37 +5,31 @@ import { Test } from '@nestjs/testing';
 import buildMockConfigService from '../../../../../app/config/__tests__/utilities/buildMockConfigService';
 import { EdgeConnectionModule } from '../../../../../app/domain-modules/edge-connection.module';
 import { CoscradEventFactory } from '../../../../../domain/common';
-import {
-    ID_MANAGER_TOKEN,
-    IIdManager,
-} from '../../../../../domain/interfaces/id-manager.interface';
+import { ID_MANAGER_TOKEN } from '../../../../../domain/interfaces/id-manager.interface';
 import { ArangoDatabaseProvider } from '../../../../../persistence/database/database.provider';
 import { PersistenceModule } from '../../../../../persistence/persistence.module';
 import generateDatabaseNameForTestSuite from '../../../../../persistence/repositories/__tests__/generateDatabaseNameForTestSuite';
 import TestRepositoryProvider from '../../../../../persistence/repositories/__tests__/TestRepositoryProvider';
+import { TestEventStream } from '../../../../../test-data/events';
 import { buildTestInstance } from '../../../../../test-data/utilities';
 import { DynamicDataTypeFinderService, DynamicDataTypeModule } from '../../../../../validation';
+import { assertCommandSuccess } from '../../../__tests__/command-helpers/assert-command-success';
+import { assertEventRecordPersisted } from '../../../__tests__/command-helpers/assert-event-record-persisted';
 import { CommandAssertionDependencies } from '../../../__tests__/command-helpers/types/CommandAssertionDependencies';
+import buildDummyUuid from '../../../__tests__/utilities/buildDummyUuid';
+import { dummySystemUserId } from '../../../__tests__/utilities/dummySystemUserId';
+import { EdgeConnection } from '../../edge-connection.entity';
+import { NoteAboutResourceCreated } from '../create-note-about-resource/note-about-resource-created.event';
 import { GrantUserReadAccessToNote } from './grant-user-read-access-to-note.command';
 
 const commandType = 'GRANT_USER_READ_ACCESS_TO_NOTE';
 
-const validCommandFsa = {
-    type: commandType,
-    payload: buildTestInstance(GrantUserReadAccessToNote,{
-        aggregateCompositeIdentifier: ,
-        
-    }),
-};
+const userId = buildDummyUuid(1);
 
 describe(commandType, () => {
     let app: INestApplication;
 
     let testRepositoryProvider: TestRepositoryProvider;
-
-    let commandHandlerService: CommandHandlerService;
-
-    let idManager: IIdManager;
 
     let commandAssertionDependencies: CommandAssertionDependencies;
 
@@ -96,6 +90,52 @@ describe(commandType, () => {
     });
 
     describe(`when the command is valid`, () => {
-        it(`should succeed`);
+        it(`should succeed`, async () => {
+            const validPayload = buildTestInstance(GrantUserReadAccessToNote, {
+                userId,
+            });
+
+            const existingNoteId = buildDummyUuid(123);
+
+            const eventHistoryForExistingNote = new TestEventStream()
+                // there will be an empty ACL at this point
+                .andThen<NoteAboutResourceCreated>({
+                    type: 'NOTE_ABOUT_RESOURCE_CREATED',
+                })
+                .as({
+                    id: existingNoteId,
+                });
+
+            const existingNote = EdgeConnection.fromEventHistory(
+                eventHistoryForExistingNote,
+                existingNoteId
+            ) as EdgeConnection;
+
+            await testRepositoryProvider.getEdgeConnectionRepository().create(existingNote);
+
+            await assertCommandSuccess(commandAssertionDependencies, {
+                systemUserId: dummySystemUserId,
+                seedInitialState: async () => {
+                    Promise.resolve();
+                },
+                buildValidCommandFSA: () => ({
+                    type: commandType,
+                    payload: validPayload,
+                }),
+                checkStateOnSuccess: async () => {
+                    const updatedNote = (await testRepositoryProvider
+                        .getEdgeConnectionRepository()
+                        .fetchById(existingNoteId)) as EdgeConnection;
+
+                    expect(updatedNote.queryAccessControlList.canUser(userId)).toBe(true);
+
+                    assertEventRecordPersisted(
+                        updatedNote,
+                        'NOTE_READ_ACCESS_GRANTED_TO_USER',
+                        dummySystemUserId
+                    );
+                },
+            });
+        });
     });
 });
--- apps/api/src/domain/models/context/commands/grant-user-read-access-to-note/grant-user-read-access-to-note.command.ts
+++ apps/api/src/domain/models/context/commands/grant-user-read-access-to-note/grant-user-read-access-to-note.command.ts
@@ -1,9 +1,20 @@
 import { ICommandBase } from '@coscrad/api-interfaces';
 import { Command } from '@coscrad/commands';
 import { NestedDataType, UUID } from '@coscrad/data-types';
+import { CoscradDataExample } from '../../../../../../src/test-data/utilities';
 import { AggregateId } from '../../../../../domain/types/AggregateId';
+import buildDummyUuid from '../../../__tests__/utilities/buildDummyUuid';
 import { EdgeConnectionCompositeIdentifier } from '../create-note-about-resource';
 
+@CoscradDataExample<GrantUserReadAccessToNote>({
+    example: {
+        aggregateCompositeIdentifier: {
+            type: 'note',
+            id: buildDummyUuid(1),
+        },
+        userId: buildDummyUuid(10),
+    },
+})
 @Command({
     type: 'GRANT_USER_READ_ACCESS_TO_NOTE',
     label: 'Grant Read Access to Note',
