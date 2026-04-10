761e264409dfaae0afc99af916a82e722f1e2bd9
Aaron Plahn <aplahn85@gmail.com>
2026-04-10 15:58:55 -0700

    update reducer exports

--- apps/coscrad-frontend/src/store/slices/memory-match/reducers/index.ts
+++ apps/coscrad-frontend/src/store/slices/memory-match/reducers/index.ts
@@ -1,5 +1,5 @@
-export * from './card-cleared.reducer.spec';
 export * from './card-fipped-up.reducer';
 export * from './card-flipped-down.reducer';
+export * from './matching-card-cleared.reducer.spec';
 export * from './matching-cards-cleared.reducer';
 export * from './round-reset.reducer';
