f62eed2fbb7caf89b5b29b7d8d8a43cfb4199b86
yaanahuu2 <gahorner@gmail.com>
2026-06-22 16:18:50 -0700

    basic router setup with terms index and detail

--- apps/admin-web-client/src/app/app.tsx
+++ apps/admin-web-client/src/app/app.tsx
@@ -1,8 +1,28 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Box, Divider } from '@mui/material';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { TermDetail } from '../components/resources/terms/term-detail.page';
 import { TermIndex } from '../components/resources/terms/term-index.page';
 
 export function App() {
-    return <TermIndex />;
+    return (
+        <BrowserRouter>
+            <Box sx={{ marginTop: '50px', marginLeft: '40px' }}>
+                <nav>
+                    <Link to="/">Home</Link>
+                    &nbsp;|&nbsp;
+                    <Link to="/terms">Terms</Link>
+                </nav>
+
+                <Divider sx={{ height: '20px', marginBottom: '10px' }} />
+
+                <Routes>
+                    <Route path="/" />
+                    <Route path="/terms" element={<TermIndex />} />
+                    <Route path="/terms/:id" element={<TermDetail />} />
+                </Routes>
+            </Box>
+        </BrowserRouter>
+    );
 }
 
 export default App;
