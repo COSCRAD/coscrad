daf4f496498959b4713cb011de1091116b25d085
yaanahuu2 <gahorner@gmail.com>
2026-06-26 00:03:16 -0700

    midway with create note form

---
+++ apps/admin-web-client/src/components/logout-button/logout-button.tsx
@@ -0,0 +1,31 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { IconButton, Stack, Tooltip } from '@mui/material';
+
+const LogoutButton = () => {
+    const { logout } = useAuth0();
+
+    // const dispatch = useAppDispatch();
+
+    return (
+        <Tooltip title="Log Out">
+            <Stack>
+                <IconButton
+                    data-testid="logout-button"
+                    color="secondary"
+                    onClick={() => {
+                        // dispatch(userLoggedOut());
+
+                        return logout({
+                            returnTo: window.location.origin,
+                        });
+                    }}
+                >
+                    <LogoutIcon />
+                </IconButton>
+            </Stack>
+        </Tooltip>
+    );
+};
+
+export default LogoutButton;
--- apps/admin-web-client/src/store/slices/command-status/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './command-status-slice';
-export * from './constants';
-export * from './hooks';
-export * from './selectors';
-export * from './thunks';
-export * from './types';
