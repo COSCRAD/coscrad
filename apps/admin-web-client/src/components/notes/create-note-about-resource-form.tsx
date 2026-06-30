8f89a177f8ae0c62f435fc93b38399bb05d8c110
yaanahuu2 <gahorner@gmail.com>
2026-06-29 23:09:42 -0700

    adding notes without term re-render

---
+++ apps/admin-web-client/src/components/notes/create-note-about-resource-form.tsx
@@ -0,0 +1,106 @@
+import { AggregateType, LanguageCode } from '@coscrad/api-interfaces';
+import { Box, Button, Stack, TextField } from '@mui/material';
+import { useState } from 'react';
+import { useExecuteCommandMutation } from '../command-status/store';
+import { LanguageSelect } from '../resources/language-select';
+
+type CreateNoteAboutResourceFormProps = {
+    generatedId: string;
+    resourceId: string;
+    resourceType: string;
+    onClose: () => void;
+};
+
+export const CreateNoteAboutResourceForm = ({
+    generatedId,
+    resourceId,
+    resourceType,
+    onClose,
+}: CreateNoteAboutResourceFormProps): JSX.Element => {
+    console.log(`${CreateNoteAboutResourceForm.name} rendered.`);
+
+    const [text, setText] = useState('');
+
+    const defaultLanguageCode = LanguageCode.English;
+
+    const [languageCode, setLanguageCode] = useState<LanguageCode>(defaultLanguageCode);
+
+    const [executeCommand, { isLoading: isRequestInProgress, error: commandError }] =
+        useExecuteCommandMutation();
+
+    if (isRequestInProgress) {
+        return <div>Processing Command Request...</div>;
+    }
+
+    if (commandError) {
+        if ('status' in commandError) {
+            // you can access all properties of `FetchBaseQueryError` here
+            const errMsg =
+                'error' in commandError ? commandError.error : JSON.stringify(commandError.data);
+
+            return (
+                <div>
+                    <div>An error with the command request has occurred:</div>
+                    <div>{errMsg}</div>
+                </div>
+            );
+        }
+        // you can access all properties of `SerializedError` here
+        return <div>Command Error: {commandError.message}</div>;
+    }
+
+    const isDisabled = text.length === 0 || !Object.values(LanguageCode).includes(languageCode);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault(); // Prevent browser refresh
+
+        console.log('Form sent to server:', text, languageCode, `${resourceType}/${resourceId}`);
+
+        executeCommand({
+            type: 'CREATE_NOTE_ABOUT_RESOURCE',
+            payload: {
+                aggregateCompositeIdentifier: {
+                    type: AggregateType.note,
+                    id: generatedId,
+                },
+                resourceCompositeIdentifier: {
+                    type: resourceType,
+                    id: resourceId,
+                },
+                text: text,
+                languageCode: languageCode,
+                resourceContext: { type: 'general' },
+            },
+        });
+
+        onClose();
+    };
+
+    return (
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ width: '450px' }}>
+            <div data-testid="create-note-for-resource-form" />
+            <Stack>
+                <TextField
+                    sx={{ width: '80%' }}
+                    data-testid={`text:note`}
+                    onChange={(e) => {
+                        setText(e.target.value);
+                    }}
+                ></TextField>
+                <LanguageSelect
+                    onSelectLanguage={(newLanguageCode: LanguageCode) => {
+                        setLanguageCode(newLanguageCode);
+                    }}
+                />
+                <Button
+                    data-testid={`submit-note`}
+                    variant="contained"
+                    disabled={isDisabled}
+                    type="submit"
+                >
+                    ADD NOTE
+                </Button>
+            </Stack>
+        </Box>
+    );
+};
