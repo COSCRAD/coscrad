dd16562eca097ef3a3b1e4909cb6d3e54d393954
Aaron Plahn <aplahn85@gmail.com>
2026-07-10 16:14:33 -0700

    remove unused ts directive

--- apps/api/src/domain/models/spatial-feature/repositories/arango-spatial-feature-query-repository.ts
+++ apps/api/src/domain/models/spatial-feature/repositories/arango-spatial-feature-query-repository.ts
@@ -56,7 +56,6 @@ export class ArangoSpatialFeatureQueryRepository implements ISpatialFeatureQuery
             name: view.properties.name,
         });
 
-        // @ts-expect-error fix this error related to serializing alternative names map to a record
         await this.database.create(document).catch((error) => {
             throw new InternalError(error);
         });
