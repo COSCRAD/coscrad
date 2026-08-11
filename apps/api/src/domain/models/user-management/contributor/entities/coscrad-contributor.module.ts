41d817b2a11ba60d30b3d8d9b8eed3cf507f9bd0
yaanahuu2 <gahorner@gmail.com>
2026-08-10 18:39:15 -0700

    add contributor export command and controller

--- apps/api/src/domain/models/user-management/contributor/entities/coscrad-contributor.module.ts
+++ apps/api/src/domain/models/user-management/contributor/entities/coscrad-contributor.module.ts
@@ -1,16 +1,23 @@
 import { CommandModule } from '@coscrad/commands';
 import { Module } from '@nestjs/common';
+import { CoscradContributorController } from '../../../../../app/controllers/coscrad-contributor.controller';
 import { IdGenerationModule } from '../../../../../lib/id-generation/id-generation.module';
 import { PersistenceModule } from '../../../../../persistence/persistence.module';
+import { CoscradContributorQueryService } from '../../../../services/query-services/coscrad-contributor-query.service';
 import { CreateContributor, CreateContributorCommandHandler } from '../commands/create-contributor';
 import { ContributorCreated } from '../commands/create-contributor/contributor-created.event';
 import { CoscradContributor } from './coscrad-contributor.entity';
 
+/**
+ * NOTE: this module is not in use, see UserManagementModule for contributors
+ */
+
 @Module({
     imports: [PersistenceModule, CommandModule, IdGenerationModule],
-    controllers: [],
+    controllers: [CoscradContributorController],
     providers: [
         CreateContributorCommandHandler,
+        CoscradContributorQueryService,
         ...[
             // domain model
             CoscradContributor,
@@ -24,4 +31,4 @@ import { CoscradContributor } from './coscrad-contributor.entity';
         })),
     ],
 })
-export class CoscradContributorModel {}
+export class CoscradContributorModule {}
--- apps/api/src/domain/services/query-services/coscrad-contributor-query.service.ts
+++ apps/api/src/domain/services/query-services/coscrad-contributor-query.service.ts
@@ -1,6 +1,11 @@
+import { ICoscradContributorViewModel, IIndexQueryResult } from '@coscrad/api-interfaces';
 import { Inject } from '@nestjs/common';
 import { CommandInfoService } from '../../../app/controllers/command/services/command-info-service';
+import { InternalError, isInternalError } from '../../../lib/errors/InternalError';
+import cloneToPlainObject from '../../../lib/utilities/cloneToPlainObject';
 import { REPOSITORY_PROVIDER_TOKEN } from '../../../persistence/constants/persistenceConstants';
+import { CoscradContributorViewModel } from '../../../queries/buildViewModelForResource/viewModels/coscrad-contributor.view-model';
+import { ResultOrError } from '../../../types/ResultOrError';
 import { CoscradContributor } from '../../models/user-management/contributor/entities/coscrad-contributor.entity';
 import { IRepositoryForAggregate } from '../../repositories/interfaces/repository-for-aggregate.interface';
 import { IRepositoryProvider } from '../../repositories/interfaces/repository-provider.interface';
@@ -12,7 +17,25 @@ export class CoscradContributorQueryService {
         @Inject(REPOSITORY_PROVIDER_TOKEN)
         protected readonly repositoryProvider: IRepositoryProvider,
         @Inject(CommandInfoService) protected readonly commandInfoService: CommandInfoService
-    ) {
-        this.coscradContributorRepository = repositoryProvider.getContributorRepository();
+    ) {}
+
+    async fetchMany(): Promise<ResultOrError<IIndexQueryResult<ICoscradContributorViewModel>>> {
+        const fetchResult = await this.repositoryProvider.getContributorRepository().fetchMany();
+
+        const allErrors = fetchResult.filter(isInternalError);
+
+        if (allErrors.length > 0)
+            return new InternalError(`Found one or more invalid contributors: \n`, allErrors);
+
+        const allContributors = fetchResult as CoscradContributor[];
+
+        return {
+            entities: allContributors.map((contributor) => ({
+                ...cloneToPlainObject(new CoscradContributorViewModel(contributor)),
+            })),
+            indexScopedActions: [],
+            page: 1,
+            count: allContributors.length,
+        };
     }
 }
