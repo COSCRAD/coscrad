16335aff697d0bdddcdeddb5009609f8e614df71
Blake Sellars <blakesellars02@gmail.com>
2026-08-24 13:06:58 -0700

    WIP: invalid test for create map

--- apps/api/src/domain/models/geospatial-map/commands/create-map.command.integration.spec.ts
+++ apps/api/src/domain/models/geospatial-map/commands/create-map.command.integration.spec.ts
@@ -8,6 +8,7 @@ import { Environment } from '../../../../app/config/constants/environment';
 import buildMockConfigService from '../../../../app/config/__tests__/utilities/buildMockConfigService';
 import { GeospatialMapModule } from '../../../../app/domain-modules/geospatial-map.module';
 import { clonePlainObjectWithOverrides } from '../../../../lib/utilities/clonePlainObjectWithOverrides';
+import assertErrorAsExpected from '../../../../lib/__tests__/assertErrorAsExpected';
 import { ArangoDatabaseProvider } from '../../../../persistence/database/database.provider';
 import { PersistenceModule } from '../../../../persistence/persistence.module';
 import generateDatabaseNameForTestSuite from '../../../../persistence/repositories/__tests__/generateDatabaseNameForTestSuite';
@@ -16,6 +17,9 @@ import { buildTestInstance } from '../../../../test-data/utilities';
 import { DynamicDataTypeFinderService } from '../../../../validation';
 import { CoscradEventFactory } from '../../../common';
 import { ID_MANAGER_TOKEN } from '../../../interfaces/id-manager.interface';
+import CommandExecutionError from '../../shared/common-command-errors/CommandExecutionError';
+import UuidNotGeneratedInternallyError from '../../shared/common-command-errors/UuidNotGeneratedInternallyError';
+import { assertCreateCommandError } from '../../__tests__/command-helpers/assert-create-command-error';
 import { assertCreateCommandSuccess } from '../../__tests__/command-helpers/assert-create-command-success';
 import { CommandAssertionDependencies } from '../../__tests__/command-helpers/types/CommandAssertionDependencies';
 import buildDummyUuid from '../../__tests__/utilities/buildDummyUuid';
@@ -27,11 +31,26 @@ const commandType = 'CREATE_MAP';
 
 const geospatialId = buildDummyUuid(3);
 
+const invalidGeoSpatialId = buildDummyUuid(4);
+
 const validFsa = {
     type: commandType,
     payload: buildTestInstance(CreateMap, {
         aggregateCompositeIdentifier: { id: geospatialId },
     }),
+};
+
+const invalidPayload = buildTestInstance(CreateMap, {
+    aggregateCompositeIdentifier: { id: invalidGeoSpatialId },
+});
+
+const invalidFsa = {
+    type: 'CREATE_MAP',
+    payload: invalidPayload,
+};
+
+const seedEmptyInitialState = async () => {
+    Promise.resolve();
 };
 
 describe(commandType, () => {
@@ -114,11 +133,27 @@ describe(commandType, () => {
                     expect(updatedGeospatialMap.name.getOriginalTextItem().text).toBe(
                         validFsa.payload.name
                     );
-
-                    expect(updatedGeospatialMap.description.getOriginalTextItem()).toBe(
-                        validFsa.payload.description
-                    );
                 },
+            });
+        });
+    });
+
+    describe(`when the command is invalid`, () => {
+        describe(`when the ID has not been generated via our system`, () => {
+            it(`should return with the expected error`, async () => {
+                await assertCreateCommandError(assertionHelperDependencies, {
+                    systemUserId: dummySystemUserId,
+                    buildCommandFSA: () => invalidFsa,
+                    seedInitialState: seedEmptyInitialState,
+                    checkError: (error) => {
+                        assertErrorAsExpected(
+                            error,
+                            new CommandExecutionError([
+                                new UuidNotGeneratedInternallyError(invalidGeoSpatialId),
+                            ])
+                        );
+                    },
+                });
             });
         });
     });
