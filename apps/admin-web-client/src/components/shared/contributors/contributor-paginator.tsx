2729bb1db8c2ac96e6157a85a2e563be79417267
yaanahuu2 <gahorner@gmail.com>
2026-08-25 09:35:36 -0700

    WIP add pagination for contributor index page

---
+++ apps/admin-web-client/src/components/shared/contributors/contributor-paginator.tsx
@@ -0,0 +1,116 @@
+import {
+    ArrowBackIosNew as ArrowBackIosNewIcon,
+    ArrowForwardIos as ArrowForwardIosIcon,
+} from '@mui/icons-material';
+import { FormControl, Grid, IconButton, MenuItem, Select, Typography } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../../store';
+import { setPage, setPageSize, useFetchTermsQuery } from '../../resources/terms/store';
+import { DEFAULT_PAGE_SIZE } from '../constants';
+import { insertNumberInSequence } from '../insert-in-sequence';
+import { cyclicDecrement } from '../math';
+
+const pageSizes: number[] = [5, 10, 50, 100];
+
+const pageSizeOptions: number[] = pageSizes.includes(DEFAULT_PAGE_SIZE)
+    ? insertNumberInSequence(pageSizes, DEFAULT_PAGE_SIZE)
+    : pageSizes;
+
+export const ContributorPaginator = (): JSX.Element => {
+    const dispatch = useDispatch();
+
+    const termQueryOptions = useSelector((state: RootState) => state.termQueryOptions);
+
+    const { data, isLoading, isError } = useFetchTermsQuery(termQueryOptions);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (isError) return <div>Error retrieving name.</div>;
+
+    const { entities: terms, count, page } = data;
+
+    const {
+        pagination: { size: pageSize },
+    } = termQueryOptions;
+
+    const pageCount = Math.ceil(count / pageSize);
+
+    const startingRecordNumberHumanReadable = pageSize * (page - 1) + 1;
+
+    const endingRecordNumberHumanReadable =
+        startingRecordNumberHumanReadable + (terms?.length || 0) - 1;
+
+    const totalNumberOfPages = Math.ceil(count / pageSize);
+
+    const updatePageSize = (newPageSize: number) => {
+        console.log({ newPageSize });
+
+        if (newPageSize > termQueryOptions.pagination.size) {
+            dispatch(setPageSize(newPageSize));
+            dispatch(setPage(1));
+        }
+
+        dispatch(setPageSize(newPageSize));
+    };
+
+    return (
+        <Grid container justifyContent="flex-end" spacing={3}>
+            <Grid item sx={{ display: 'flex', alignItems: 'center' }}>
+                <Typography component="span" sx={{ mr: 2, mt: 1 }}>
+                    Showing Records: {startingRecordNumberHumanReadable}-
+                    {endingRecordNumberHumanReadable}/{count} &nbsp; Filtered Records: {count}
+                </Typography>
+            </Grid>
+            <Grid item sx={{ display: 'flex', alignItems: 'center' }}>
+                <Typography component="span" sx={{ mr: 2 }}>
+                    Rows per page:
+                </Typography>
+                <FormControl variant="standard" sx={{ m: 1 }}>
+                    <Select
+                        name="pageSize"
+                        value={pageSize}
+                        onChange={(changeEvent) => {
+                            const {
+                                target: { value },
+                            } = changeEvent;
+
+                            const newPageSize =
+                                typeof value === 'string' ? Number.parseInt(value) : value;
+
+                            updatePageSize(newPageSize);
+                        }}
+                    >
+                        {pageSizeOptions.map((pageSize) => (
+                            <MenuItem key={pageSize} value={pageSize}>
+                                {pageSize}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            </Grid>
+            <Grid item sx={{ display: 'flex', alignItems: 'center' }}>
+                Page: {page}/{pageCount}
+            </Grid>
+            <Grid item sx={{ display: 'flex', alignItems: 'center' }}>
+                <IconButton
+                    onClick={() => {
+                        dispatch(setPage(cyclicDecrement(page - 1, totalNumberOfPages) + 1));
+                    }}
+                >
+                    <ArrowBackIosNewIcon />
+                </IconButton>
+            </Grid>
+            <Grid item sx={{ display: 'flex', alignItems: 'center' }}>
+                <IconButton
+                    onClick={() => {
+                        dispatch(setPage(cyclicIncrement(page - 1, totalNumberOfPages) + 1));
+                    }}
+                >
+                    <ArrowForwardIosIcon />
+                </IconButton>
+            </Grid>
+        </Grid>
+    );
+};
--- apps/admin-web-client/src/components/shared/contributors/store/contributor.api.ts
+++ apps/admin-web-client/src/components/shared/contributors/store/contributor.api.ts
@@ -1,6 +1,7 @@
 import { ICoscradContributorViewModel, IIndexQueryResult } from '@coscrad/api-interfaces';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { getConfig } from '../../../../config';
+import { TermQueryOptionsState } from '../../../resources/terms/store';
 
 export interface ContributorsForTerm {
     id: string;
@@ -12,9 +13,27 @@ export const contributorApi = createApi({
     tagTypes: ['contributors'],
     baseQuery: fetchBaseQuery({ baseUrl: `${getConfig().apiUrl}/` }),
     endpoints: (builder) => ({
-        fetchContributors: builder.query<IIndexQueryResult<ICoscradContributorViewModel>, void>({
-            query: () => `contributors`,
-            providesTags: ['contributors'],
+        fetchContributors: builder.query<
+            IIndexQueryResult<ICoscradContributorViewModel>,
+            TermQueryOptionsState
+        >({
+            query: (options) => ({
+                url: `contributors`,
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(options),
+            }),
+            providesTags: (result) =>
+                result
+                    ? [
+                          ...result.entities.map(({ id }) => ({
+                              type: 'contributors' as const,
+                              id,
+                          })),
+                          { type: 'contributors', id: 'LIST' },
+                      ]
+                    : [{ type: 'contributors', id: 'LIST' }],
+            keepUnusedDataFor: 300,
         }),
     }),
 });
