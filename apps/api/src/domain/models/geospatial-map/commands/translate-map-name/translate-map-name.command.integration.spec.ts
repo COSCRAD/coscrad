9e130bad3ab6b859ba37dfebd8a95d9c334bce28
Blake Sellars <blakesellars02@gmail.com>
2026-08-28 15:58:40 -0700

    WIP: add translate map name command

---
+++ apps/api/src/domain/models/geospatial-map/commands/translate-map-name/translate-map-name.command.integration.spec.ts
@@ -0,0 +1,73 @@
+import { CommandHandlerService } from '@coscrad/commands';
+import { INestApplication } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Test } from '@nestjs/testing';
+import buildConfigFilePath from '../../../../../app/config/buildConfigFilePath';
+import { Environment } from '../../../../../app/config/constants/environment';
+import buildMockConfigService from '../../../../../app/config/__tests__/utilities/buildMockConfigService';
+import { GeospatialMapModule } from '../../../../../app/domain-modules/geospatial-map.module';
+import { CoscradEventFactory } from '../../../../../domain/common';
+import { ID_MANAGER_TOKEN } from '../../../../../domain/interfaces/id-manager.interface';
+import { ArangoDatabaseProvider } from '../../../../../persistence/database/database.provider';
+import { PersistenceModule } from '../../../../../persistence/persistence.module';
+import generateDatabaseNameForTestSuite from '../../../../../persistence/repositories/__tests__/generateDatabaseNameForTestSuite';
+import TestRepositoryProvider from '../../../../../persistence/repositories/__tests__/TestRepositoryProvider';
+import { DynamicDataTypeFinderService } from '../../../../../validation';
+import { CommandAssertionDependencies } from '../../../__tests__/command-helpers/types/CommandAssertionDependencies';
+
+const commandType = 'TRANSLATE_MAP_NAME';
+
+describe(commandType, () => {
+    let app: INestApplication;
+
+    let testRepositoryProvider: TestRepositoryProvider;
+
+    let assertionHelperDependencies: CommandAssertionDependencies;
+
+    beforeAll(async () => {
+        const testModule = await Test.createTestingModule({
+            imports: [
+                ConfigModule.forRoot({
+                    isGlobal: true,
+                    envFilePath: buildConfigFilePath(Environment.test),
+                    cache: false,
+                }),
+                PersistenceModule.forRootAsync(),
+                GeospatialMapModule,
+            ],
+        })
+            .overrideProvider(ConfigService)
+            .useValue(
+                buildMockConfigService({
+                    ARANGO_DB_NAME: generateDatabaseNameForTestSuite(),
+                })
+            )
+            .compile();
+
+        app = testModule.createNestApplication();
+
+        await app.init();
+
+        testRepositoryProvider = new TestRepositoryProvider(
+            app.get(ArangoDatabaseProvider),
+            app.get(CoscradEventFactory),
+            app.get(DynamicDataTypeFinderService)
+        );
+
+        assertionHelperDependencies = {
+            testRepositoryProvider,
+            commandHandlerService: app.get(CommandHandlerService),
+            idManager: app.get(ID_MANAGER_TOKEN),
+        };
+    });
+
+    beforeEach(async () => {
+        await testRepositoryProvider.testSetup();
+    });
+
+    afterAll(async () => {
+        app.get(ArangoDatabaseProvider).close();
+
+        app.close();
+    });
+});
