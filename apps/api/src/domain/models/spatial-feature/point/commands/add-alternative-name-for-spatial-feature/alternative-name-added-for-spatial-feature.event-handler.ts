b6fbb25abb871dc408d04fcffafb99ed783a3284
Aaron Plahn <aplahn85@gmail.com>
2026-09-25 10:08:50 -0700

    fix: register event consumers for SPATIAL_FEATURES

--- apps/api/src/domain/models/spatial-feature/point/commands/add-alternative-name-for-spatial-feature/alternative-name-added-for-spatial-feature.event-handler.ts
+++ apps/api/src/domain/models/spatial-feature/point/commands/add-alternative-name-for-spatial-feature/alternative-name-added-for-spatial-feature.event-handler.ts
@@ -1,11 +1,12 @@
 import { Inject } from '@nestjs/common';
-import { ICoscradEventHandler } from '../../../../../../domain/common';
+import { CoscradEventConsumer, ICoscradEventHandler } from '../../../../../../domain/common';
 import {
     ISpatialFeatureQueryRepository,
     SPATIAL_FEATURE_QUERY_REPOSITORY_TOKEN,
 } from '../../../queries/spatial-feature-query-repository.interface';
 import { AlternativeNameAddedForSpatialFeature } from './alternative-name-added-for-spatial-feature.event';
 
+@CoscradEventConsumer('ALTERNATIVE_NAME_ADDED_FOR_SPATIAL_FEATURE')
 export class AlternativeNameAddedForSpatialFeatureEventHandler
     implements ICoscradEventHandler<AlternativeNameAddedForSpatialFeature>
 {
